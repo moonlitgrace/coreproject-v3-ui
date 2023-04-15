80e9387dd101113e04d28d3209f48099d2c7b741
sssuneeth <suneeth.since2005@gmail.com>
2023-04-15 19:55:51 +0530

    Add anime episodes options

---
+++ src/lib/components/pages/anime_info/AnimeEpisodesDetails.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	// icons
+	import SettingsOutline from '$icons/SettingsOutline.svelte';
+	import ChevronDown from '$icons/Chevron-Down.svelte';
+	import Search from '$icons/Search.svelte';
+</script>
+
+<div class="md:mt-[6vw] w-full">
+	<div class="flex items-center gap-3">
+		<span class="font-semibold md:text-[1.25vw] md:leading-[1.5vw]">Episodes</span>
+		<button class="btn btn-icon rounded p-0 bg-surface-400 md:w-[1.5vw] md:h-[1.5vw]">
+			<SettingsOutline width="11" height="11" class="opacity-75" />
+		</button>
+
+	</div>
+
+	<div class="md:mt-[3vw] flex justify-between w-full">
+		<div class="flex items-center gap-[1.25vw]">
+			<p class="flex items-end gap-[0.75vw]">
+				<span class="md:text-[2vw] md:leading-[1.9vw] leading-[0.9vw] font-bold">23</span>
+				<span class="md:text-[1vw] font-semibold">episodes</span>
+				<span class="md:text-[0.6vw]">▪</span>
+			</p>
+
+			<div>
+				<div class="w-max relative flex items-center">
+					<span class="pr-[4.5vw] text-[0.75vw] font-medium">available in</span>
+					<div class="absolute right-0 h-[0.08vw] w-[4vw] bg-white"></div>
+				</div>
+
+				<div>
+					<div class="h-[1.8vw] flex items-center gap-[0.75vw]">
+						<button class="btn btn-sm text-[0.8vw] leading-[0.9vw] rounded-[0.25vw] px-[0.9vw] h-full font-bold bg-surface-400 uppercase">
+							sub
+						</button>
+
+						<button class="btn btn-sm text-[0.8vw] leading-[0.9vw] rounded-[0.25vw] px-[0.9vw] h-full font-bold bg-surface-400 uppercase">
+							dub
+						</button>
+
+						<span class="md:text-[0.6vw]">▪</span>
+
+						<button class="btn btn-sm text-[0.8vw] leading-[0.9vw] rounded-[0.25vw] px-[0.9vw] h-full font-bold bg-surface-400">
+							1080p
+						</button>
+
+						<button class="btn btn-sm text-[0.8vw] leading-[0.9vw] rounded-[0.25vw] px-[0.9vw] h-full font-bold bg-surface-400">
+							720p
+						</button>
+						<button class="btn btn-sm text-[0.8vw] leading-[0.9vw] rounded-[0.25vw] px-[0.9vw] h-full font-bold bg-surface-400">
+							480p
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="flex items-end gap-[0.75vw]">
+			<div class="flex flex-col gap-[0.5vw]">
+				<span class="text-[0.75vw] text-surface-50 leading-[0.9vw]">Type</span>
+				<button class="btn text-[0.9vw] leading-[0.9vw] rounded-[0.5vw] px-[0.9vw] h-[2.4vw] font-semibold bg-surface-400">
+					<span>Subbed</span>
+					<ChevronDown width="15" color="lightgray" />
+				</button>
+			</div>
+			<div class="flex flex-col gap-[0.5vw]">
+				<span class="text-[0.75vw] text-surface-50 leading-[0.9vw]">Type</span>
+				<button class="btn text-[0.9vw] leading-[0.9vw] rounded-[0.5vw] px-[0.9vw] h-[2.4vw] font-semibold bg-surface-400">
+					<span>Subbed</span>
+					<ChevronDown width="15" color="lightgray" />
+				</button>
+			</div>
+			<button class="btn btn-icon h-[2.4vw] w-[2.4vw] p-0 leading-[0.9vw] rounded-[0.5vw] font-semibold bg-surface-400">
+				<Search width="15" color="lightgray" />
+			</button>
+		</div>
+	</div>
+</div>
--- src/lib/components/pages/anime_info/AnimeMainDetails.svelte
+++ src/lib/components/pages/anime_info/AnimeMainDetails.svelte
@@ -3,6 +3,7 @@
 
 	import ScrollArea from '$components/shared/ScrollArea.svelte';
 	import SidebarDetails from '$components/pages/anime_info/SidebarDetails.svelte';
+	import AnimeEpisodesDetails from '$components/pages/anime_info/AnimeEpisodesDetails.svelte';
 	import type { SvelteComponentDev } from 'svelte/internal';
 	// icons
 	import PlayCircle from '$icons/PlayCircle.svelte';
@@ -124,156 +125,160 @@
 </script>
 
 <div class="grid grid-cols-12 items-start">
-	<div class="col-span-10 grid grid-cols-12 items-end justify-between">
-		<div class="col-span-7 flex items-end md:gap-[3.125vw]">
-			<img
-				class="md:w-[12.5vw] md:rounded-[1vw]"
-				src={anime.cardBackgroundImage}
-				alt={anime.titles.eng}
-			/>
-			<div>
-				<span class="font-bold md:text-[2.5vw] md:leading-[3vw]">{anime.titles.eng}</span>
+	<div class="col-span-10 pr-[4vw]">
+		<div class="grid grid-cols-12 items-end justify-between">
+			<div class="col-span-7 flex items-end md:gap-[3.125vw]">
+				<img
+					class="md:w-[12.5vw] md:rounded-[1vw]"
+					src={anime.cardBackgroundImage}
+					alt={anime.titles.eng}
+				/>
+				<div>
+					<span class="font-bold md:text-[2.5vw] md:leading-[3vw]">{anime.titles.eng}</span>
 
-				<p class="flex items-center gap-2 text-surface-100 md:pt-[0.625vw]">
-					{#each Object.entries(anime.titles) as anime_title}
-						{#if anime_title[0] !== 'eng'}
+					<p class="flex items-center gap-2 text-surface-100 md:pt-[0.625vw]">
+						{#each Object.entries(anime.titles) as anime_title}
+							{#if anime_title[0] !== 'eng'}
+								<span
+									class="font-medium md:text-[0.75vw] md:leading-[0.9vw] [&:not(:last-child)]:after:ml-2 [&:not(:last-child)]:after:content-['▪']"
+									>{anime_title[1]}
+								</span>
+							{/if}
+						{/each}
+					</p>
+
+					<p class="flex flex-wrap items-center gap-2 md:pt-[1vw]">
+						{#each Object.entries(anime.basic_details) as anime_detail}
 							<span
-								class="font-medium md:text-[0.75vw] md:leading-[0.9vw] [&:not(:last-child)]:after:ml-2 [&:not(:last-child)]:after:content-['▪']"
-								>{anime_title[1]}</span
+								class="font-semibold md:text-[0.625vw] md:leading-[0.75vw] [&:not(:last-child)]:after:ml-1 [&:not(:last-child)]:after:content-['▪']"
 							>
-						{/if}
-					{/each}
-				</p>
+								{anime_detail[1]}
+								{anime_detail[0] === 'episodes' ? 'eps' : ''}
+							</span>
+						{/each}
+					</p>
 
-				<p class="flex flex-wrap items-center gap-2 md:pt-[1vw]">
-					{#each Object.entries(anime.basic_details) as anime_detail}
-						<span
-							class="font-semibold md:text-[0.625vw] md:leading-[0.75vw] [&:not(:last-child)]:after:ml-1 [&:not(:last-child)]:after:content-['▪']"
-						>
-							{anime_detail[1]}
-							{anime_detail[0] === 'episodes' ? 'eps' : ''}
-						</span>
-					{/each}
-				</p>
-
-				<div class="flex items-center md:mt-[2.25vw] md:gap-[1.15vw]">
-					<button
-						type="button"
-						class="btn bg-primary-500 font-bold text-white md:h-[4.3vw] md:w-[6.75vw] md:rounded-[0.625vw] md:text-[0.87vw]"
-					>
-						<div class="flex items-center justify-center md:gap-[0.7vw]">
-							<PlayCircle width="25" height="25" color="white" />
-							<div class="flex flex-col items-start">
-								<span class="md:leading-5">Watch</span>
-								<span class="font-normal leading-none text-surface-100 md:text-[0.625vw]"
-									>Ep 01</span
-								>
-							</div>
-						</div>
-					</button>
-
-					{#each Object.entries(icon_mapping.anime_options) as item}
-						{@const item_name = item[0]}
-						{@const item_icon = item[1].icon}
-
-						{@const component = item_icon.component}
-						{@const component_width = item_icon.width}
-						{@const component_height = item_icon.height}
-						{@const component_color = item_icon.color}
-
+					<div class="flex items-center md:mt-[2.25vw] md:gap-[1.15vw]">
 						<button
 							type="button"
-							class="btn bg-secondary-100 capitalize text-surface-500 md:h-[4.3vw] md:w-[4.3vw] md:rounded-[0.625vw]
-					       md:text-[0.87vw] md:font-semibold"
+							class="btn bg-primary-500 font-bold text-white md:h-[4.3vw] md:w-[6.75vw] md:rounded-[0.625vw] md:text-[0.87vw]"
 						>
-							<div class="flex flex-col items-center justify-center md:gap-[0.68vw]">
+							<div class="flex items-center justify-center md:gap-[0.7vw]">
+								<PlayCircle width="25" height="25" color="white" />
+								<div class="flex flex-col items-start">
+									<span class="md:leading-5">Watch</span>
+									<span class="font-normal leading-none text-surface-100 md:text-[0.625vw]"
+										>Ep 01</span
+									>
+								</div>
+							</div>
+						</button>
+
+						{#each Object.entries(icon_mapping.anime_options) as item}
+							{@const item_name = item[0]}
+							{@const item_icon = item[1].icon}
+
+							{@const component = item_icon.component}
+							{@const component_width = item_icon.width}
+							{@const component_height = item_icon.height}
+							{@const component_color = item_icon.color}
+
+							<button
+								type="button"
+								class="btn bg-secondary-100 capitalize text-surface-500 md:h-[4.3vw] md:w-[4.3vw] md:rounded-[0.625vw]
+						       md:text-[0.87vw] md:font-semibold"
+							>
+								<div class="flex flex-col items-center justify-center md:gap-[0.68vw]">
+									<svelte:component
+										this={component}
+										width={component_width}
+										height={component_height}
+										color={component_color}
+									/>
+									<span class="md:leading-[1vw]">{item_name}</span>
+								</div>
+							</button>
+						{/each}
+					</div>
+
+					<div class="flex md:mt-[1.25vw] md:gap-[0.625vw]">
+						{#each Object.entries(icon_mapping.user_options_icons) as item}
+							{@const item_icon = item[1].icon}
+
+							{@const component = item_icon.component}
+							{@const component_width = item_icon.width}
+							{@const component_height = item_icon.height}
+							{@const component_variant = item_icon.variant}
+
+							<button
+								type="button"
+								class="btn btn-icon bg-warning-400 p-0 text-surface-500 md:w-[1.875vw] md:rounded-[0.25vw]"
+							>
 								<svelte:component
 									this={component}
 									width={component_width}
 									height={component_height}
-									color={component_color}
+									variant={component_variant}
 								/>
-								<span class="md:leading-[1vw]">{item_name}</span>
-							</div>
-						</button>
-					{/each}
-				</div>
-
-				<div class="flex md:mt-[1.25vw] md:gap-[0.625vw]">
-					{#each Object.entries(icon_mapping.user_options_icons) as item}
-						{@const item_icon = item[1].icon}
-
-						{@const component = item_icon.component}
-						{@const component_width = item_icon.width}
-						{@const component_height = item_icon.height}
-						{@const component_variant = item_icon.variant}
-
-						<button
-							type="button"
-							class="btn btn-icon bg-warning-400 p-0 text-surface-500 md:w-[1.875vw] md:rounded-[0.25vw]"
-						>
-							<svelte:component
-								this={component}
-								width={component_width}
-								height={component_height}
-								variant={component_variant}
-							/>
-						</button>
-					{/each}
-				</div>
-			</div>
-		</div>
-
-		<div class="col-span-5 pr-8 md:w-[26.625vw]">
-			<div class="flex items-center gap-3">
-				<span class="font-semibold md:text-[1.25vw] md:leading-[1.5vw]"> Synopsis </span>
-				<button class="btn btn-icon rounded p-0 bg-surface-400 md:w-[1.5vw] md:h-[1.5vw]">
-					<SettingsOutline width="11" height="11" class="opacity-75" />
-				</button>
-			</div>
-
-			<ScrollArea
-				offsetScrollbar
-				gradientMask
-				parentClass="mt-5"
-				class="text-justify md:max-h-[9.5vw] md:text-[0.75vw] md:leading-[1vw]"
-			>
-				{anime.description}
-			</ScrollArea>
-
-			<div class="flex gap-2 md:mt-3">
-				{#each anime.generes as genere}
-					<div
-						class="rounded bg-surface-500 px-[0.95vw] text-white md:py-[0.375vw] md:text-[0.75vw] md:leading-[0.9vw]"
-					>
-						{genere}
+							</button>
+						{/each}
 					</div>
-				{/each}
+				</div>
 			</div>
 
-			<div
-				class="flex w-max items-center gap-2 rounded bg-white/10 backdrop-blur-lg md:mt-3 md:px-[0.75vw] md:py-[0.375vw] md:text-[0.625vw] md:leading-[0.75vw]"
-			>
-				<div class="flex items-center gap-1">
-					Score:
-					<span class="text-warning-400">{anime.score}</span>
+			<div class="col-span-5">
+				<div class="flex items-center gap-3">
+					<span class="font-semibold md:text-[1.25vw] md:leading-[1.5vw]"> Synopsis </span>
+					<button class="btn btn-icon rounded p-0 bg-surface-400 md:w-[1.5vw] md:h-[1.5vw]">
+						<SettingsOutline width="11" height="11" class="opacity-75" />
+					</button>
 				</div>
-				<div class="flex items-center gap-1">
-					Status:
-					<span class="text-warning-400">Watching</span>
-					<ChevronDown width="9" color="warning-400" />
+
+				<ScrollArea
+					offsetScrollbar
+					gradientMask
+					parentClass="mt-5"
+					class="text-justify md:max-h-[9.5vw] md:text-[0.75vw] md:leading-[1vw]"
+				>
+					{anime.description}
+				</ScrollArea>
+
+				<div class="flex gap-2 md:mt-3">
+					{#each anime.generes as genere}
+						<div
+							class="rounded bg-surface-500 px-[0.95vw] text-white md:py-[0.375vw] md:text-[0.75vw] md:leading-[0.9vw]"
+						>
+							{genere}
+						</div>
+					{/each}
 				</div>
-				<div class="flex items-center gap-1">
-					Episode:
-					<span class="text-warning-400">0/{anime.basic_details.episodes}</span>
-				</div>
-				<div class="flex items-center gap-1">
-					Your Score:
-					<span class="text-warning-400">Not Rated</span>
-					<ChevronDown width="9" color="warning-400" />
+
+				<div
+					class="flex w-max items-center gap-2 rounded bg-white/10 backdrop-blur-lg md:mt-3 md:px-[0.75vw] md:py-[0.375vw] md:text-[0.625vw] md:leading-[0.75vw]"
+				>
+					<div class="flex items-center gap-1">
+						Score:
+						<span class="text-warning-400">{anime.score}</span>
+					</div>
+					<div class="flex items-center gap-1">
+						Status:
+						<span class="text-warning-400">Watching</span>
+						<ChevronDown width="9" color="warning-400" />
+					</div>
+					<div class="flex items-center gap-1">
+						Episode:
+						<span class="text-warning-400">0/{anime.basic_details.episodes}</span>
+					</div>
+					<div class="flex items-center gap-1">
+						Your Score:
+						<span class="text-warning-400">Not Rated</span>
+						<ChevronDown width="9" color="warning-400" />
+					</div>
 				</div>
 			</div>
 		</div>
+
+		<AnimeEpisodesDetails />
 	</div>
 
 	<div class="col-span-2">
