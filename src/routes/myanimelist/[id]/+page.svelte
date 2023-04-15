1ffd6bcf1a1130f58fccca8233b72601a39d8e23
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-15 23:57:28 +0600

    Update +page.svelte

--- src/routes/myanimelist/[id]/+page.svelte
+++ src/routes/myanimelist/[id]/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import AnimeInfoPage from '$components/pages/anime_info/Index.svelte';
 	import { OpengraphGenerator } from '$functions/opengraph';
+	import { page } from '$app/stores';
 
 	const opengraph_html = new OpengraphGenerator({
 		title: 'Hello',
-		page_url: '',
+		page_url: $page.url.href,
 		description: 'random',
 		site_name: 'CoreProject',
 		locale: 'en_US'
