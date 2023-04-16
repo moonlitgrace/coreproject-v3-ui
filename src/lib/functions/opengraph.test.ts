cc8a112b6c51b75ad3c925fd095b48690777bfb8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-16 08:15:20 +0600

    [ `Enhancement` ] : Fix opengraph (#25)
    * ad
    
    * Update opengraph.ts
    
    * fix
    
    * Update +page.svelte
    
    * add test
    
    * Update opengraph.test.ts

---
+++ src/lib/functions/opengraph.test.ts
@@ -0,0 +1,20 @@
+import { test, expect } from 'vitest';
+import { OpengraphGenerator } from '$functions/opengraph';
+
+test('opengraph function', () => {
+	const opengraph_html = new OpengraphGenerator({
+		title: 'Hello',
+		page_url: 'https://example.com',
+		description: 'random',
+		site_name: 'CoreProject',
+		locale: 'en_US'
+	})
+		.generate_opengraph()
+		.trim()
+		// eslint-disable-next-line no-useless-escape
+		.replace(/(?<=\>)\s+(?=\<)/g, '');
+
+	expect(opengraph_html).toBe(
+		`<meta property="og:type" content="website"><meta property="og:site_name" content="CoreProject"><meta property="og:locale" content="en_US"><meta property="og:title" content="Hello"><meta property="og:url" content="https://example.com"><meta property="og:description" content="random">`
+	);
+});
--- src/routes/myanimelist/[id]/+page.svelte
+++ src/routes/myanimelist/[id]/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
 	import AnimeInfoPage from '$components/pages/anime_info/Index.svelte';
+	import { OpengraphGenerator } from '$functions/opengraph';
+	import { page } from '$app/stores';
+
+	const opengraph_html = new OpengraphGenerator({
+		title: 'Hello',
+		page_url: $page.url.href,
+		description: 'random',
+		site_name: 'CoreProject',
+		locale: 'en_US'
+	}).generate_opengraph();
 </script>
 
+<svelte:head>
+	{@html opengraph_html}
+</svelte:head>
 <AnimeInfoPage />
