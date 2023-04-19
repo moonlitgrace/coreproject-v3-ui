ecadd41bea1f1054268454c92379ebabf4962efb
sssuneeth <suneeth.since2005@gmail.com>
2023-04-19 16:14:41 +0530

    Update WelcomeNavigation card

--- src/lib/components/pages/home/MainHero/NavigationCard.svelte
+++ src/lib/components/pages/home/MainHero/NavigationCard.svelte
@@ -16,10 +16,10 @@
 <GradientCard
 	backgroundImage="https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDEzMzE1NzI3MzA0MzI2/animes-like-kono-subarashii-sekai-ni-shukufuku-wo.jpg"
 	rounded="0.875vw"
-	fromColor="from-surface-900/90"
+	fromColor="from-surface-900"
 	toColor="to-surface-900/10"
 	direction="bg-gradient-to-tr"
-	class="h-[24.3125vw] w-[16.625vw] mt-[3.4vw] "
+	class="h-[24.3125vw] w-[16.625vw] mt-[3.4vw] border-b-2 border-l-2 border-surface-300"
 >
 	<div class="px-[1.875vw] pt-[2vw]">
 		<div class="flex flex-col gap-[0.2w]">
