6c46733f657b71933f07d844ec433821d80a2fbd
sssuneeth <suneeth.since2005@gmail.com>
2023-04-19 11:29:39 +0530

    [ `Home` ] : Add Welcome section

--- src/lib/components/pages/home/MainHero/NavigationCard.svelte
+++ src/lib/components/pages/home/MainHero/NavigationCard.svelte
@@ -5,6 +5,11 @@
 	import Forum from "$icons/Forum.svelte";
 	import Recent from "$icons/Recent.svelte";
 	import Notifications from "$icons/Notifications.svelte";
+	import Language from "$icons/Language.svelte";
+	import Preference from "$icons/Preference.svelte";
+	import Moon from "$icons/Moon.svelte";
+	import SettingsOutline from "$icons/SettingsOutline.svelte";
+	import CoreProject from "$icons/CoreProject.svelte";
 </script>
 
 
@@ -14,15 +19,15 @@
 	fromColor="from-surface-900"
 	toColor="to-surface-900/10"
 	direction="bg-gradient-to-tr"
-	class="h-[24.3125vw] w-[16.625vw] mt-[3.4vw] ring-[0.13vw] ring-surface-50"
+	class="h-[24.3125vw] w-[16.625vw] mt-[3.4vw] ring-[0.13vw] ring-surface-50/75"
 >
 	<div class="px-[1.875vw] pt-[2vw]">
-		<div class="flex flex-col gap-[0.9375vw]">
+		<div class="flex flex-col gap-[0.2w]">
 			<span class="text-[1.5vw] font-bold">Welcome</span>
 			<span class="text-[0.875vw] font-semibold">Jump quickly into</span>
 		</div>
 
-		<div class="mt-[1.0625vw] flex flex-col gap-[0.625vw]">
+		<div class="mt-[1vw] flex flex-col gap-[0.625vw]">
 			<div class="flex items-center gap-[1vw]">
 				<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
 					<Forum width="1.25vw" height="1.25vw" class="text-surface-900" />
@@ -43,6 +48,28 @@
 				</button>
 				<span class="text-[1vw] font-bold">Notifications</span>
 			</div>
+		</div>
+
+		<div class="mt-[0.4vw]">
+			<span class="text-[0.9vw] font-semibold">More</span>
+			<div class="flex gap-[0.9375vw] mt-[0.75vw]">
+				<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
+					<Language width="1.25vw" height="1.25vw" class="text-surface-900" />
+				</button>
+				<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
+					<Preference width="1.25vw" height="1.25vw" class="text-surface-900" />
+				</button>
+				<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
+					<Moon width="1.25vw" height="1.25vw" class="text-surface-900" />
+				</button>
+				<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
+					<SettingsOutline width="1.25vw" height="1.25vw" class="text-surface-900" />
+				</button>
+			</div>
+		</div>
+
+		<div class="grid place-items-center mt-[1vw]">
+			<CoreProject width="6.375vw" />
 		</div>
 	</div>
 </GradientCard>
