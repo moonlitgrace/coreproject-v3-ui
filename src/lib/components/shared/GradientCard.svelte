91eb15c1cdc48560a8c74f8415fd8565387a33ea
sssuneeth <suneeth.since2005@gmail.com>
2023-04-19 10:04:30 +0530

    Update GradientCard module

--- src/lib/components/shared/GradientCard.svelte
+++ src/lib/components/shared/GradientCard.svelte
@@ -3,6 +3,7 @@
 	export let fromColor = 'from-surface-900/25';
 	export let toColor = 'to-transparent';
 	export let direction = 'bg-gradient-to-t';
+	export let directionTwo: string;
 	export let width = '100%';
 	export let height = '100%';
 	export let rounded = '10px';
@@ -16,7 +17,7 @@
 	class="{klass} w-[{width}] h-[{height}] rounded-[{rounded}] relative flex items-center overflow-hidden bg-cover bg-center"
 	style="background-image: url({backgroundImage})"
 >
-	<div class="gradient {direction} {fromColor} {toColor} absolute h-full w-full" />
+	<div class="gradient {direction} {directionTwo} {fromColor} {toColor} absolute h-full w-full" />
 	<div class="absolute w-full">
 		<slot />
 	</div>
