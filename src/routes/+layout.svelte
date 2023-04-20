fb56c40357aa007de47f2add0a95a52e8bfe9af6
sssuneeth <suneeth.since2005@gmail.com>
2023-04-20 18:39:13 +0530

    Add dropdown functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 	import { AppShell, Avatar } from '@skeletonlabs/skeleton';
 
 	import { blur } from 'svelte/transition';
+	import { onMount, onDestroy } from "svelte";
 
 	// import icons
 	import AnimeCore from '$icons/AnimeCore.svelte';
@@ -21,12 +22,17 @@
 	import Forum from '$icons/Forum.svelte';
 	import Settings from '$icons/Settings.svelte';
 	import Misc from '$icons/Misc.svelte';
+	import Person from '$icons/Person.svelte';
+	import Moon from '$icons/Moon.svelte';
+	import SettingsOutline from '$icons/SettingsOutline.svelte';
 
 	// Local
 	let active_button:
 		| keyof typeof icon_mapping.top
 		| keyof typeof icon_mapping.middle
 		| keyof typeof icon_mapping.bottom = 'home';
+
+	let active_profile_dropdown = false;
 
 	const icon_mapping = {
 		top: {
@@ -99,18 +105,66 @@
 					color: 'white'
 				}
 			}
+		},
+		profile_dropdown: {
+			profile: {
+				icon: {
+					component: Person
+				}
+			},
+			my_list: {
+				icon: {
+					component: List
+				}
+			},
+			theme: {
+				icon: {
+					component: Moon
+				}
+			},
+			settings: {
+				icon: {
+					component: SettingsOutline
+				}
+			}
 		}
 	};
 
 	async function middle_section_click(item: string) {
 		active_button = item as typeof active_button;
 	}
+
+	async function profile_button_click(event: MouseEvent) {
+		// check if dropdown is not clicked
+		if (typeof document !== 'undefined' && !(event.target as HTMLElement).closest('.dropdown') && !(event.target as HTMLElement).closest('.avatar')) {
+			console.log('click outside called')
+			active_profile_dropdown = false;
+		}
+	}
+
+	// adding and removing click event for dropdown
+	onMount(() => {
+	    if (typeof document !== 'undefined') {
+	      document.addEventListener('click', profile_button_click);
+	    }
+	    return () => {
+	      if (typeof document !== 'undefined') {
+	        document.removeEventListener('click', profile_button_click);
+	      }
+	    };
+  	});
+
+  	onDestroy(() => {
+	    if (typeof document !== 'undefined') {
+	      document.removeEventListener('click', profile_button_click);
+	    }
+  	});
 </script>
 
-<div class="h-screen">
+<div class="h-screen wrapper">
 	<AppShell>
 		<svelte:fragment slot="header">
-			<div class="h-18 flex items-center justify-between pl-[2.1vw] pr-[3.75vw] py-[0.9375vw]">
+			<div class="h-18 flex items-center justify-between pl-[2.1vw] pr-[3.75vw] py-[0.9375vw] relative">
 				<a href="/">
 					<Logo width="2vw" />
 				</a>
@@ -125,7 +179,43 @@
 					cursor="cursor-pointer"
 					src="https://play-lh.googleusercontent.com/hKZp5Jg2rmFMsD0O2wohA1NkQTHBokXwalRpoxmjYt94WyMu4nVN0rXtbcTmh7tIur8=w2560-h1440-rw"
 					initials="JD"
+					on:click={() => { active_profile_dropdown = !active_profile_dropdown }}
+					class="avatar"
 				/>
+
+				{#if active_profile_dropdown}
+					<div class="dropdown absolute w-[12vw] bg-surface-400 rounded-[0.375vw] shadow-lg shadow-surface-900/50 right-[3.5vw] top-[4.7vw] py-[1.125vw] px-[0.75vw]">
+						
+						<div class="flex gap-[0.8vw]">
+							<Avatar
+								rounded="rounded-[0.375vw]"
+								width="w-[2.5vw]"
+								cursor="cursor-pointer"
+								src="https://play-lh.googleusercontent.com/hKZp5Jg2rmFMsD0O2wohA1NkQTHBokXwalRpoxmjYt94WyMu4nVN0rXtbcTmh7tIur8=w2560-h1440-rw"
+								initials="JD"
+							/>
+							<div class="flex flex-col">
+								<span class="text-[0.9vw] font-semibold">Username</span>
+								<span class="text-[0.8vw] font-medium">email@domain.xyz</span>
+							</div>
+						</div>
+
+						<div class="mt-[1vw]">
+							{#each Object.entries(icon_mapping.profile_dropdown) as item}
+								{@const item_icon = item[1].icon}
+								{@const item_name = item[0]}
+
+								<div class="flex items-center gap-[0.7vw] cursor-pointer hover:bg-surface-300/20 p-[0.5vw] rounded-[0.2vw]">
+									<svelte:component 
+										this={item_icon.component}
+										width={"1.5vw"}
+									/>
+									<span class="text-[1vw] font-medium capitalize">{item_name}</span>
+								</div>
+							{/each}
+						</div>
+					</div>
+				{/if}
 			</div>
 		</svelte:fragment>
 		<svelte:fragment slot="sidebarLeft">
