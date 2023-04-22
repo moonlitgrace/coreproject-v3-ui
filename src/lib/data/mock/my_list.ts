22e5031d9d579a053ffa260a02e5cd0d7333f0dd
Suneeth <suneeth.since2005@gmail.com>
2023-04-22 14:03:58 +0530

    Create my_list.ts

---
+++ src/lib/data/mock/my_list.ts
@@ -0,0 +1,9 @@
+export const my_list = [
+	{
+		id: 1,
+		name: "Azur Lane",
+		cover: "https://i1.sndcdn.com/artworks-000640004677-8hkhau-t500x500.jpg",
+		current_episode: 4,
+		episodes_count: 12
+	}
+]
