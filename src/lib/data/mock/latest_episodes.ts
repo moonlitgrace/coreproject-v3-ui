18840f22859c7bd15394d50770191b4a78f68bbb
Suneeth <suneeth.since2005@gmail.com>
2023-04-22 14:31:36 +0530

    Create latest_episodes.ts

---
+++ src/lib/data/mock/latest_episodes.ts
@@ -0,0 +1,8 @@
+export const latest_episodes = [
+	{
+		id: 1,
+		name: "SpyxFamily",
+		cover: "https://dwgkfo5b3odmw.cloudfront.net/img/manga_series_header/632-SJ_SpyFamily_2000x800_jpg_wm",
+		episode_number: 06
+	}
+]
