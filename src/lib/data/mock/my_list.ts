5968c2926d82f0d57eb24ef96f1aa5a72f53be40
Suneeth <suneeth.since2005@gmail.com>
2023-04-22 16:34:33 +0530

    Update my_list.ts

--- src/lib/data/mock/my_list.ts
+++ src/lib/data/mock/my_list.ts
@@ -1,51 +1,53 @@
 export const my_list = [
 	{
 		id: 1,
-		name: "Azur Lane",
-		cover: "https://i1.sndcdn.com/artworks-000640004677-8hkhau-t500x500.jpg",
+		name: 'Azur Lane',
+		cover: 'https://i1.sndcdn.com/artworks-000640004677-8hkhau-t500x500.jpg',
 		current_episode: 4,
 		episodes_count: 12
 	},
 	{
 		id: 2,
-		name: "Akagami no Shirayuki-hime",
-		cover: "https://e0.pxfuel.com/wallpapers/822/772/desktop-wallpaper-akagami-no-shirayuki-hime-akagami-no-shirayukihime-thumbnail.jpg",
+		name: 'Akagami no Shirayuki-hime',
+		cover:
+			'https://e0.pxfuel.com/wallpapers/822/772/desktop-wallpaper-akagami-no-shirayuki-hime-akagami-no-shirayukihime-thumbnail.jpg',
 		current_episode: 11,
 		episodes_count: 12
 	},
 	{
 		id: 3,
-		name: "GOLDEN BOY: Sasurai no Obenkyou Yarou",
-		cover: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx268-O1hqy3Uea1ns.png",
+		name: 'GOLDEN BOY: Sasurai no Obenkyou Yarou',
+		cover: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx268-O1hqy3Uea1ns.png',
 		current_episode: 1,
 		episodes_count: 6
 	},
 	{
 		id: 4,
-		name: "7SEEDS",
-		cover: "https://socialanime.it/upload_serie/s_12775_small.jpg",
+		name: '7SEEDS',
+		cover: 'https://socialanime.it/upload_serie/s_12775_small.jpg',
 		current_episode: 5,
 		episodes_count: 12
 	},
 	{
 		id: 5,
-		name: "Shakugan no Shana",
-		cover: "https://imagecache.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/7ae19317-50ed-4a41-2cd6-8ae4424a4200/width=450/245470-1742259591-masterpiece,%20best%20quality,%20absurdres,%20perfect%20antomy,%20cinematic%20lighting,%20Shana,%20red%20hair,%20red%20eyes,%20kimono,%20flower-print,%20stand.jpeg",
+		name: 'Shakugan no Shana',
+		cover:
+			'https://imagecache.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/7ae19317-50ed-4a41-2cd6-8ae4424a4200/width=450/245470-1742259591-masterpiece,%20best%20quality,%20absurdres,%20perfect%20antomy,%20cinematic%20lighting,%20Shana,%20red%20hair,%20red%20eyes,%20kimono,%20flower-print,%20stand.jpeg',
 		current_episode: 19,
 		episodes_count: 24
 	},
 	{
 		id: 6,
-		name: "Death note",
-		cover: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx12445-hKIXQW3vA4iz.jpg",
+		name: 'Death note',
+		cover: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx12445-hKIXQW3vA4iz.jpg',
 		current_episode: 8,
 		episodes_count: 28
 	},
 	{
 		id: 7,
-		name: "To LOVE-Ru",
-		cover: "https://cdn.myanimelist.net/images/anime/9/79330.jpg",
+		name: 'To LOVE-Ru',
+		cover: 'https://cdn.myanimelist.net/images/anime/9/79330.jpg',
 		current_episode: 9,
 		episodes_count: 26
 	}
-]
+];
