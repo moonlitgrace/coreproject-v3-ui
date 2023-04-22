3bf9aa48c72d6ab7acc812ff3071235c3e1a19e6
Suneeth <suneeth.since2005@gmail.com>
2023-04-22 21:10:54 +0530

    Update home welcome card

--- src/lib/components/pages/home/MainHero/NavigationCard.svelte
+++ src/lib/components/pages/home/MainHero/NavigationCard.svelte
@@ -82,7 +82,7 @@
 	backgroundImage="https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDEzMzE1NzI3MzA0MzI2/animes-like-kono-subarashii-sekai-ni-shukufuku-wo.jpg"
 	rounded="0.875vw"
 	fromColor="from-surface-900"
-	toColor="to-surface-900/10"
+	toColor="to-surface-900/25"
 	direction="bg-gradient-to-tr"
 	class="mt-[3.4vw] h-[24.3125vw] w-[16.625vw] border-b-2 border-l-2 border-surface-300"
 >
