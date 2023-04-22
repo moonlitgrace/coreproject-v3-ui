27f4155c640733e52324350f46cc742cb89ff22c
Suneeth <suneeth.since2005@gmail.com>
2023-04-22 16:34:15 +0530

    Update latest_animes.ts

--- src/lib/data/mock/latest_animes.ts
+++ src/lib/data/mock/latest_animes.ts
@@ -1,97 +1,86 @@
 export const latest_animes = [
 	{
-	  "id": 1,
-	  "name": "Kimetsu no Yaiba",
-	  "type": "TV",
-	  "aired_from": "2023-04-20T15:38:51.162Z",
-	  "aired_to": "2023-04-20T15:38:51.162Z",
-	  "cover": "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
-	  "synopsis": `It is the Taisho Period in Japan. Tanjiro, a kindhearted boy who sells charcoal for a
+		id: 1,
+		name: 'Kimetsu no Yaiba',
+		type: 'TV',
+		aired_from: '2023-04-20T15:38:51.162Z',
+		aired_to: '2023-04-20T15:38:51.162Z',
+		cover:
+			'https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg',
+		synopsis: `It is the Taisho Period in Japan. Tanjiro, a kindhearted boy who sells charcoal for a
 				living, finds his family slaughtered by a demon. To make matters worse, his younger sister
 				Nezuko, the sole survivor, has been transformed into a demon herself. Though devastated by
 				this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his
 				sister back into a human, and kill the demon that massacred his family.`,
-	  "updated": "2023-04-20T15:38:51.162Z",
-	  "studios": [
-	  		"ufotable"
-	  ],
-	  "episodes_count": 26,
+		updated: '2023-04-20T15:38:51.162Z',
+		studios: ['ufotable'],
+		episodes_count: 26
 	},
 	{
 		id: 2,
-		"name": 'Hyouka',
-		type: "TV",
-		"aired_from": "2023-08-20T15:38:51.162Z",
-		"cover": 'https://raw.githubusercontent.com/baseplate-admin/CoreProject/19a84ff9c864e0cfb33cb6a4ce79f75af05cde6c/frontend/AnimeCore/static/images/Hyouka-poster.jpg',
-		"synopsis":
-			`High school freshman Houtarou Oreki has but one goal: to lead a gray life while conserving as much energy as he can. Unfortunately, his peaceful days come to an end when his older sister, Tomoe, forces him to save the memberless Classics Club from disbandment.
+		name: 'Hyouka',
+		type: 'TV',
+		aired_from: '2023-08-20T15:38:51.162Z',
+		cover:
+			'https://raw.githubusercontent.com/baseplate-admin/CoreProject/19a84ff9c864e0cfb33cb6a4ce79f75af05cde6c/frontend/AnimeCore/static/images/Hyouka-poster.jpg',
+		synopsis: `High school freshman Houtarou Oreki has but one goal: to lead a gray life while conserving as much energy as he can. Unfortunately, his peaceful days come to an end when his older sister, Tomoe, forces him to save the memberless Classics Club from disbandment.
 
 			Luckily, Oreki's predicament seems to be over when he heads to the clubroom and discovers that his fellow first-year, Eru Chitanda, has already become a member. However, despite his obligation being fulfilled, Oreki finds himself entangled by Chitanda's curious and bubbly personality, soon joining the club of his own volition.
 
 			Hyouka follows the four members of the Classics Club—including Oreki's friends Satoshi Fukube and Mayaka Ibara—as they, driven by Chitanda's insatiable curiosity, solve the trivial yet intriguing mysteries that permeate their daily lives.`,
 		updated: '2023-03-11T02:37:40.790Z',
-	  	"studios": [
-	  		"Kyoto animations",
-	  	],
-		episodes_count: 9,
+		studios: ['Kyoto animations'],
+		episodes_count: 9
 	},
 	{
 		id: 3,
-		"name": 'You Lie in April',
-		type: "TV",
-		"aired_from": "2014-11-20T15:38:51.162Z",
-		"cover": 'https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg',
-		"synopsis":
-			`Kousei Arima is a child prodigy known as the "Human Metronome" for playing the piano with precision and perfection. Guided by a strict mother and rigorous training, Kousei dominates every competition he enters, earning the admiration of his musical peers and praise from audiences. When his mother suddenly passes away, the subsequent trauma makes him unable to hear the sound of a piano, and he never takes the stage thereafter. \n
+		name: 'You Lie in April',
+		type: 'TV',
+		aired_from: '2014-11-20T15:38:51.162Z',
+		cover:
+			'https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg',
+		synopsis: `Kousei Arima is a child prodigy known as the "Human Metronome" for playing the piano with precision and perfection. Guided by a strict mother and rigorous training, Kousei dominates every competition he enters, earning the admiration of his musical peers and praise from audiences. When his mother suddenly passes away, the subsequent trauma makes him unable to hear the sound of a piano, and he never takes the stage thereafter. \n
 
 			Nowadays, Kousei lives a quiet and unassuming life as a junior high school student alongside his friends Tsubaki Sawabe and Ryouta Watari. While struggling to get over his mother's death, he continues to cling to music. His monochrome life turns upside down the day he encounters the eccentric violinist Kaori Miyazono, who thrusts him back into the spotlight as her accompanist. Through a little lie, these two young musicians grow closer together as Kaori tries to fill Kousei's world with color.`,
 		updated: '2023-03-11T02:37:40.790Z',
-	  	"studios": [
-	  		"A-1 Pictures",
-	  	],
-		episodes_count: 22,
+		studios: ['A-1 Pictures'],
+		episodes_count: 22
 	},
 	{
 		id: 4,
-		"name": 'Attack on Titan',
-		type: "TV",
-		"aired_from": "2014-11-20T15:38:51.162Z",
-		"cover": 'https://images.saymedia-content.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDQ0NTI1OTEyMDczODYy/attack-on-titan-season-1-review.jpg',
-		"synopsis":
-			`Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called Titans, forcing humans to hide in fear behind enormous concentric walls. What makes these giants truly terrifying is that their taste for human flesh is not born out of hunger but what appears to be out of pleasure. To ensure their survival, the remnants of humanity began living within defensive barriers, resulting in one hundred years without a single titan encounter. However, that fragile calm is soon shattered when a colossal Titan manages to breach the supposedly impregnable outer wall, reigniting the fight for survival against the man-eating abominations.`,
+		name: 'Attack on Titan',
+		type: 'TV',
+		aired_from: '2014-11-20T15:38:51.162Z',
+		cover:
+			'https://images.saymedia-content.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDQ0NTI1OTEyMDczODYy/attack-on-titan-season-1-review.jpg',
+		synopsis: `Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called Titans, forcing humans to hide in fear behind enormous concentric walls. What makes these giants truly terrifying is that their taste for human flesh is not born out of hunger but what appears to be out of pleasure. To ensure their survival, the remnants of humanity began living within defensive barriers, resulting in one hundred years without a single titan encounter. However, that fragile calm is soon shattered when a colossal Titan manages to breach the supposedly impregnable outer wall, reigniting the fight for survival against the man-eating abominations.`,
 		updated: '2013-04-7T02:37:40.790Z',
-	  	"studios": [
-	  		"Wit Studio",
-	  	],
-		episodes_count: 25,
+		studios: ['Wit Studio'],
+		episodes_count: 25
 	},
 	{
 		id: 5,
-		"name": 'Jujutsu Kaisen',
-		type: "TV",
-		"aired_from": "2014-11-20T15:38:51.162Z",
-		"cover": 'https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg',
-		"synopsis":
-			`Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
+		name: 'Jujutsu Kaisen',
+		type: 'TV',
+		aired_from: '2014-11-20T15:38:51.162Z',
+		cover:
+			'https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg',
+		synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
 		updated: '2020-10-3T02:37:40.790Z',
-	  	"studios": [
-	  		"MAPPA",
-	  	],
-		episodes_count: 24,
+		studios: ['MAPPA'],
+		episodes_count: 24
 	},
 	{
 		id: 6,
-		"name": 'Death Note',
-		type: "TV",
-		"aired_from": "2006-11-4T15:38:51.162Z",
-		"cover": 'https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/11/Death-Note-Cover.jpg',
-		"synopsis":
-			`Brutal murders, petty thefts, and senseless violence pollute the human world. In contrast, the realm of death gods is a humdrum, unchanging gambling den. The ingenious 17-year-old Japanese student Light Yagami and sadistic god of death Ryuk share one belief: their worlds are rotten.
+		name: 'Death Note',
+		type: 'TV',
+		aired_from: '2006-11-4T15:38:51.162Z',
+		cover:
+			'https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/11/Death-Note-Cover.jpg',
+		synopsis: `Brutal murders, petty thefts, and senseless violence pollute the human world. In contrast, the realm of death gods is a humdrum, unchanging gambling den. The ingenious 17-year-old Japanese student Light Yagami and sadistic god of death Ryuk share one belief: their worlds are rotten.
 			 For his own amusement, Ryuk drops his Death Note into the human world. Light stumbles upon it, deeming the first of its rules ridiculous: the human whose name is written in this note shall die. However, the temptation is too great, and Light experiments by writing a felon's name, which disturbingly enacts his first murder.`,
 		updated: '2020-10-3T02:37:40.790Z',
-	  	"studios": [
-	  		" Madhouse",
-	  	],
-		episodes_count: 24,
+		studios: [' Madhouse'],
+		episodes_count: 24
 	}
-]
+];
