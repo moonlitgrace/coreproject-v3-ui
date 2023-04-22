3330eb9fddab7a046f03d98edee40d319fae36e6
Suneeth <suneeth.since2005@gmail.com>
2023-04-22 16:27:15 +0530

    Add icon_mappings

--- src/lib/components/pages/home/MainHero/NavigationCard.svelte
+++ src/lib/components/pages/home/MainHero/NavigationCard.svelte
@@ -40,6 +40,40 @@
 					class: "text-surface-900",
 				}
 			},
+		},
+		bottom: {
+			language: {
+				icon: {
+					component: Language,
+					width: "1.25vw",
+					height: "1.25vw",
+					class: "text-surface-900",
+				}
+			},
+			preferences: {
+				icon: {
+					component: Preference,
+					width: "1.25vw",
+					height: "1.25vw",
+					class: "text-surface-900",
+				}
+			},
+			theme: {
+				icon: {
+					component: Moon,
+					width: "1.25vw",
+					height: "1.25vw",
+					class: "text-surface-900",
+				}
+			},
+			settings: {
+				icon: {
+					component: SettingsOutline,
+					width: "1.25vw",
+					height: "1.25vw",
+					class: "text-surface-900",
+				}
+			}
 		}
 	}
 </script>
@@ -60,26 +94,22 @@
 		</div>
 
 		<div class="mt-[1vw] flex flex-col gap-[0.625vw]">
-			<div class="flex items-center gap-[1vw]">
-				<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
-					<Forum width="1.25vw" height="1.25vw" class="text-surface-900" />
-				</button>
-				<span class="text-[1vw] font-bold">Forums</span>
-			</div>
+			{#each Object.entries(icon_mapping.top) as item}
+				{@const item_title = item[1].title}
+				{@const item_icon = item[1].icon}
 
-			<div class="flex items-center gap-[1vw]">
-				<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
-					<Recent width="1.25vw" height="1.25vw" class="text-surface-900" />
-				</button>
-				<span class="text-[1vw] font-bold">Last watched anime</span>
-			</div>
-
-			<div class="flex items-center gap-[1vw]">
-				<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
-					<Notifications width="1.25vw" height="1.25vw" class="text-surface-900" />
-				</button>
-				<span class="text-[1vw] font-bold">Notifications</span>
-			</div>
+				<div class="flex items-center gap-[1vw]">
+					<button class="btn w-[2.5vw] h-[2.5vw] p-0 bg-surface-50 rounded-[0.375vw]">
+						<svelte:component
+							this={item_icon.component}
+							width={item_icon.width}
+							height={item_icon.height}
+							class={item_icon.class}
+						/>
+					</button>
+					<span class="text-[1vw] font-bold">{item_title}</span>
+				</div>
+			{/each}
 		</div>
 
 		<div class="mt-[0.4vw]">
