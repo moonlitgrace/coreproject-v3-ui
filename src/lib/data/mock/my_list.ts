3fc3f2931f6c91f1cddc6a996f2ab2025f067b8c
Suneeth <suneeth.since2005@gmail.com>
2023-04-23 21:23:04 +0530

    Update my_list.ts

--- src/lib/data/mock/my_list.ts
+++ src/lib/data/mock/my_list.ts
@@ -3,6 +3,10 @@ export const my_list = [
 		id: 1,
 		name: 'Azur Lane',
 		cover: 'https://i1.sndcdn.com/artworks-000640004677-8hkhau-t500x500.jpg',
+		synopsis: `
+				Azur Lane, a combination of all the different Camps in the world, was once successful in repelling the underwater menace, the Siren. Now splintered, they must face a new threat in Red Axis, former allies who crave to wield this otherworldly Siren technology for their own nefarious desires! Who will be victorious in the never-ending war between these battleship girls!?
+				Akagami no Shirayuki-hime depicts Shirayuki's journey toward a new life at the royal palace of Clarines, as well as Zen's endeavor to become a prince worthy of his title. As loyal friendships are forged and deadly enemies formed, Shirayuki and Zen slowly learn to support each other as they walk their own paths.
+				`
 		current_episode: 4,
 		episodes_count: 12
 	},
@@ -11,6 +15,9 @@ export const my_list = [
 		name: 'Akagami no Shirayuki-hime',
 		cover:
 			'https://e0.pxfuel.com/wallpapers/822/772/desktop-wallpaper-akagami-no-shirayuki-hime-akagami-no-shirayukihime-thumbnail.jpg',
+		synopsis: `
+				Although her name means "snow white," Shirayuki is a cheerful, red-haired girl living in the country of Tanbarun who works diligently as an apothecary at her herbal shop. Her life changes drastically when she is noticed by the silly prince of Tanbarun, Prince Raji, who then tries to force her to become his concubine.
+				`
 		current_episode: 11,
 		episodes_count: 12
 	},
@@ -18,6 +25,9 @@ export const my_list = [
 		id: 3,
 		name: 'GOLDEN BOY: Sasurai no Obenkyou Yarou',
 		cover: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx268-O1hqy3Uea1ns.png',
+		synopsis: `
+				Kintarou Ooe is a specialist in part-time work, riding all over the highways and byways of Japan on his trusty steed, the Mikazuki 5, and finding employment wherever he can. His adventures bring him knowledge and experience that can't be taught in a classroom, from political corruption to the delicacy of a young woman's heart. With nothing but the open road before him—not to mention the many beautiful women along the way—Kintarou 
+				`
 		current_episode: 1,
 		episodes_count: 6
 	},
@@ -25,6 +35,9 @@ export const my_list = [
 		id: 4,
 		name: '7SEEDS',
 		cover: 'https://socialanime.it/upload_serie/s_12775_small.jpg',
+		synopsis: `
+				Imagine this: you are living a normal day in your life. Maybe you are out with friends, eating your family's home-cooked meal or spending time with your girlfriend. When you next wake up, you are suddenly thrust into a strange, new world, surrounded by five strangers on a rapidly sinking boat in the middle of a storm.
+				`
 		current_episode: 5,
 		episodes_count: 12
 	},
@@ -33,6 +46,9 @@ export const my_list = [
 		name: 'Shakugan no Shana',
 		cover:
 			'https://imagecache.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/7ae19317-50ed-4a41-2cd6-8ae4424a4200/width=450/245470-1742259591-masterpiece,%20best%20quality,%20absurdres,%20perfect%20antomy,%20cinematic%20lighting,%20Shana,%20red%20hair,%20red%20eyes,%20kimono,%20flower-print,%20stand.jpeg',
+			synopsis: `
+				The world has become a slaughtering ground for the Crimson Denizens, mysterious beings from a parallel universe who thrive on the life energy of humans. These merciless murderers only leave behind scant remainders of souls called "Torches," which are mere residues that will eventually be destroyed, along with the very fact of the victims' existence from the minds of the living. In an ambitious endeavor to put an end to this invisible, hungry massacre, warriors called Flame Hazes relentlessly fight these monsters.
+				`
 		current_episode: 19,
 		episodes_count: 24
 	},
@@ -40,6 +56,9 @@ export const my_list = [
 		id: 6,
 		name: 'Death note',
 		cover: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx12445-hKIXQW3vA4iz.jpg',
+		synopsis: `
+				Brutal murders, petty thefts, and senseless violence pollute the human world. In contrast, the realm of death gods is a humdrum, unchanging gambling den. The ingenious 17-year-old Japanese student Light Yagami and sadistic god of death Ryuk share one belief: their worlds are rotten.
+				`
 		current_episode: 8,
 		episodes_count: 28
 	},
@@ -47,6 +66,9 @@ export const my_list = [
 		id: 7,
 		name: 'To LOVE-Ru',
 		cover: 'https://cdn.myanimelist.net/images/anime/9/79330.jpg',
+		synopsis: `
+				Rito Yuuki never gets a break—he's always finding himself in lewd accidents with girls around him. Although his heart still yearns for Haruna, his childhood love, Rito can't help but question his feelings for Lala, the alien princess who appeared in front of him and declared she would marry him. But now, it's not just Lala he has to deal with: her younger twin sisters, Momo and Nana, have also traveled to Earth, wanting to meet their older sister's fiancé, and just as luck would have it, they end up staying at Rito's home.
+				`
 		current_episode: 9,
 		episodes_count: 26
 	}
