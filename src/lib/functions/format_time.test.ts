6e5d3bbaa661b0ee5d392598c9957b8a1ce34b7a
Suneeth <suneeth.since2005@gmail.com>
2023-04-25 22:51:21 +0530

    Update format_time.test.ts

--- src/lib/functions/format_time.test.ts
+++ src/lib/functions/format_time.test.ts
@@ -1,7 +1,7 @@
 import { formatTime } from '$functions/format_time';
 import { test, expect } from 'vitest';
 
-test('format date function', () => {
+test('format time function', () => {
 	const formated_time = new formatTime(1600);
 	expect(formated_time.formatSecondsToMinutes).toBe(26);
 	expect(formated_time.formatSecondsToTimeStampDuration).toBe('26:40');
