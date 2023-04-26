551f659789402b04eec8e567ee4d002ad771efca
Suneeth <suneeth.since2005@gmail.com>
2023-04-26 12:21:43 +0530

    Add test for date from now format
    Fixes #50

--- src/lib/functions/format_date.test.ts
+++ src/lib/functions/format_date.test.ts
@@ -6,4 +6,5 @@ test('format date function', () => {
 
 	expect(formated_date.format_to_human_readable_form).toBe('Mar 11, 2023');
 	expect(formated_date.format_to_season).toBe('spring 2023');
+	expect(formated_date.format_to_time_from_now).toBe('2 months ago');
 });
