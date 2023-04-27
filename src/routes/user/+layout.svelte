40a999cd4ad0452ec23e9700f5dbfba34c548cfc
Suneeth <suneeth.since2005@gmail.com>
2023-04-27 21:46:52 +0530

    Update +layout.svelte

--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -1,5 +1,46 @@
-<script>
-	
+<script lang="ts">
+	import { onDestroy, onMount } from "svelte";
+    import { blur } from "svelte/transition";
+
+    let choice_number: number;
+    let choices: Array<{
+        type: string;
+        name: string;
+        image: string;
+        credit?: string;
+    }> = [
+        {
+            type: "anime",
+            name: "Demon Slayer",
+            image: "/posters/demon-slayer.webp",
+            credit: "https://www.reddit.com/r/DemonSlayerAnime/comments/tpgpid/demon_slayer_4k_wallpaper/"
+        },
+        {
+            type: "anime",
+            name: "Attack on Titan",
+            image: "/posters/attack-on-titan.jpg"
+        },
+        {
+            type: "anime",
+            name: "Comic Girls",
+            image: "/posters/Comic-Girls-Image.png"
+        }
+    ];
+
+    const change_index = () => {
+        const index = Math.floor(Math.random() * choices.length);
+        choice_number = index;
+    };
+    let interval: NodeJS.Timer | undefined;
+    onMount(() => {
+        interval = setInterval(() => {
+            change_index();
+        }, 20000);
+        change_index();
+    });
+    onDestroy(() => {
+        clearInterval(interval);
+    });
 </script>
 
 <slot />
