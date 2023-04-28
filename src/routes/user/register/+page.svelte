0118dfaac811a7691f087e48b437add87289f5ba
Suneeth <suneeth.since2005@gmail.com>
2023-04-28 22:08:27 +0530

    Update +page.svelte

--- src/routes/user/register/+page.svelte
+++ src/routes/user/register/+page.svelte
@@ -5,4 +5,15 @@
 	<title>Register | AnimeCore</title>
 </svelte:head>
 
-<div>Register page form</div>
+<register-page>
+	<span class="text-[1.25vw] uppercase font-bold tracking-wider text-white/95 flex">
+		create your&nbsp
+		<div class="flex">
+			<span class="text-surface-50">c</span>
+			<span class="text-warning-400">o</span>
+			<span class="text-surface-50">r</span>
+			<span class="text-surface-50">e</span>
+		</div> 
+		&nbspaccount
+	</span>
+</register-page>
