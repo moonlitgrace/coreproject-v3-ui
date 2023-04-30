3e09736a70b6ee2d99085293cbb999d956f44e84
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-30 11:17:16 +0600

    add

--- src/routes/user/register/+page.svelte
+++ src/routes/user/register/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	import { superForm } from 'sveltekit-superforms/client';
 
+	import Info from '$icons/info.svelte';
+	import Cross from '$icons/cross.svelte';
+	import ArrowUpRight from '$icons/arrow_up_right.svelte';
+	import Tick from '$icons/tick.svelte';
+
+	import { zxcvbn, zxcvbnOptions } from '@zxcvbn-ts/core';
+	import zxcvbnCommonPackage from '@zxcvbn-ts/language-common';
+	import zxcvbnEnPackage from '@zxcvbn-ts/language-en';
+
 	export let data: any;
+
+	let password_strength = 0;
+	// Configure ZXCVBN
+	const zxcvbn_options = {
+		translations: zxcvbnEnPackage.translations,
+		graphs: zxcvbnCommonPackage.adjacencyGraphs,
+		dictionary: {
+			...zxcvbnCommonPackage.dictionary,
+			...zxcvbnEnPackage.dictionary
+		}
+	};
+	zxcvbnOptions.setOptions(zxcvbn_options);
 
 	let password_requirements = [
 		{ text: 'minimum 8 characters', valid: false },
@@ -19,6 +40,7 @@
 				return regex.test(email) ? null : 'Please enter a valid email address';
 			},
 			password: (password) => {
+				password_strength = zxcvbn(password).score;
 				return validate_password(password) ? null : 'Password error';
 			},
 			confirm_password: (confirm_password) => {
@@ -60,11 +82,6 @@
 		// Return true if all requirements are met, false otherwise
 		return password_requirements.every((requirement) => requirement.valid);
 	}
-
-	import Info from '$icons/info.svelte';
-	import Cross from '$icons/cross.svelte';
-	import ArrowUpRight from '$icons/arrow_up_right.svelte';
-	import Tick from '$icons/tick.svelte';
 </script>
 
 <svelte:head>
@@ -72,17 +89,6 @@
 </svelte:head>
 
 <register-page>
-	<span class="flex hidden text-[1.25vw] font-bold uppercase tracking-wider text-white/95">
-		create your&nbsp
-		<div class="flex">
-			<span class="text-surface-50">c</span>
-			<span class="text-warning-400">o</span>
-			<span class="text-surface-50">r</span>
-			<span class="text-surface-50">e</span>
-		</div>
-		&nbspaccount
-	</span>
-
 	<form class="flex h-full flex-col justify-between py-[1.5vw]" method="POST" use:enhance>
 		<form-fields>
 			<email-field>
@@ -124,7 +130,12 @@
 					</div>
 					<password-strength class="mt-[1vw] flex flex-col">
 						<div class="flex gap-[0.75vw]">
-							{#each Array(4) as _}
+							{#each Array(password_strength) as _}
+								<span
+									class="h-[0.625vw] w-[6.25vw] rounded-[0.1875vw] border-[0.2vw] border-surface-50/50 bg-green-400 transition duration-300"
+								/>
+							{/each}
+							{#each Array(4 - password_strength) as _}
 								<span
 									class="h-[0.625vw] w-[6.25vw] rounded-[0.1875vw] border-[0.2vw] border-surface-50/50 transition duration-300"
 								/>
@@ -139,13 +150,20 @@
 							<div class="ml-[0.75vw] mt-[0.4vw] flex w-3/5 flex-col gap-[0.1vw]">
 								{#each password_requirements as requirement}
 									<div class="grid grid-cols-12 items-center">
-										<svelte:component
-											this={requirement.valid ? Tick : Cross}
-											class="col-span-1"
-											style={requirement.valid
-												? 'width: 0.7vw; color: deepskyblue; opacity: 0.9;'
-												: 'width: 0.9vw; color: red; opacity: 0.8;'}
-										/>
+										{#if requirement.valid}
+											<svelte:component
+												this={Tick}
+												class="col-span-1"
+												style="width: 0.7vw; color: deepskyblue; opacity: 0.9;"
+											/>
+										{:else}
+											<svelte:component
+												this={Cross}
+												class="col-span-1"
+												style="width: 0.9vw; color: red; opacity: 0.8;"
+											/>
+										{/if}
+
 										<span class="col-span-11 text-[0.8vw] text-surface-300">{requirement.text}</span
 										>
 									</div>
