f310ffd6e14785ec36faa78de67376b48d87b19b
Suneeth <suneeth.since2005@gmail.com>
2023-05-01 20:24:44 +0530

    Create register_schema

---
+++ src/lib/data/schema/register_schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const schema = z
+    .object({
+        email: z
+            .string()
+            .min(10, "Email must be at least 10 characters long.")
+            .email("Please enter a valid email address"),
+
+        password: z
+            .string()
+            .min(8, "must_be_at_least_8")
+            .refine((val) => /(?=.*[!@#$%^&*()_+|~\-=?;:'",.<>{}[\]\\/])/.test(val), "missing_one_special_character")
+            .refine((val) => /(?=.*\d)/.test(val), "missing_one_number")
+            .refine((val) => /(?=.*[A-Z])/.test(val), "missing_one_uppercase")
+            .refine((val) => /(?=.*[a-z])/.test(val), "missing_one_lowercase"),
+
+        confirm_password: z
+            .string()
+            .refine(({ confirm_password, password }: any) => confirm_password === password, "Password does not match")
+    })
+    .refine((schema) => {
+        const password_and_confirm_password_match = schema.password === schema.confirm_password;
+
+        return [password_and_confirm_password_match].every((value) => value === true);
+    });
