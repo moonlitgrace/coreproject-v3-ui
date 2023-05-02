c39e3b6c896f84c0ecd1eba29745af3e7457d9eb
Suneeth <suneeth.since2005@gmail.com>
2023-05-02 18:31:49 +0530

    Fix homepage mobile responsive breakpoints

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -277,7 +277,7 @@
     <title>AnimeCore - A modern anime streaming site</title>
 </svelte:head>
 
-<div class="px-[5vw] md:p-[1.25vw] md:pr-[3.75vw]">
+<div class="px-[5vw] sm:p-[1.25vw] sm:pr-[3.75vw]">
     <div class="flex justify-between">
         <latest-animes class="h-[27.875vw] w-[42.1875vw]">
             <div
@@ -413,7 +413,7 @@
             </div>
         </latest-animes>
 
-        <latest-episodes class="w-[21.5625vw] hidden md:block">
+        <latest-episodes class="w-[21.5625vw] hidden sm:block">
             <div class="flex items-center gap-[0.625vw]">
                 <span class="text-[1.25vw] font-bold">Latest Episodes</span>
                 <button class="btn btn-icon h-[1.7vw] w-[1.7vw] rounded-[0.3vw] bg-surface-400">
@@ -468,7 +468,7 @@
         </latest-episodes>
 
         <navigation-card
-            class="relative hidden md:block mt-[3.4vw] h-[24.1325vw] w-[16.625vw] rounded-[0.875vw] bg-cover bg-center"
+            class="relative hidden sm:block mt-[3.4vw] h-[24.1325vw] w-[16.625vw] rounded-[0.875vw] bg-cover bg-center"
             style="background-image: url(https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDEzMzE1NzI3MzA0MzI2/animes-like-kono-subarashii-sekai-ni-shukufuku-wo.jpg)"
         >
             <div class="gradient absolute h-full w-full bg-gradient-to-t from-surface-900/95 to-surface-900/25" />
