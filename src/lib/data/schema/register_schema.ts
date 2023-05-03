c542199dc19fac59f6b7dc0a208fd26b515727ca
Suneeth <suneeth.since2005@gmail.com>
2023-05-03 22:24:57 +0530

    Update register schema

--- src/lib/data/schema/register_schema.ts
+++ src/lib/data/schema/register_schema.ts
@@ -12,13 +12,7 @@ export const schema = z
 			.min(8, "atleast_8")
 			.refine((val) => /(?=.*[!@#$%^&*()_+|~\-=?;:'",.<>{}[\]\\/])/.test(val), "missing_one_special_character")
 			.refine((val) => /(?=.*\d)/.test(val), "missing_one_number")
-			.refine((val) => /(?=.*[A-Z])/.test(val), "missing_one_uppercase")
-			.refine((val) => /(?=.*[a-z])/.test(val), "missing_one_lowercase"),
+			.refine((val) => /(?=.*[A-Z])|(?=.*[a-z])/.test(val), "missing_one_upper_or_lowercase"),
 
 		confirm_password: z.string()
 	})
-	.refine((schema) => {
-		const password_and_confirm_password_match = schema.password === schema.confirm_password;
-
-		return [password_and_confirm_password_match].every((value) => value === true);
-	});
