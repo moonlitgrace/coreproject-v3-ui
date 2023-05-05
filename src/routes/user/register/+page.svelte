e53511690dc2a70aad0500b9b321de2f4ea53772
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-05-05 12:29:28 +0600

    add

--- src/routes/user/register/+page.svelte
+++ src/routes/user/register/+page.svelte
@@ -1,96 +1,87 @@
 <script lang="ts">
-    import { superForm } from "sveltekit-superforms/client";
-    import { schema } from "$schemas/register";
     import _ from "lodash";
-
+    import { createForm, getValue } from "felte";
     import Info from "$icons/info.svelte";
     import Cross from "$icons/cross.svelte";
     import ArrowUpRight from "$icons/arrow_up_right.svelte";
     import Tick from "$icons/tick.svelte";
+    import { validator } from "@felte/validator-zod";
+    import { z } from "zod";
+    import { reporter, ValidationMessage } from "@felte/reporter-svelte";
+    import { onMount } from "svelte";
+
     import { zxcvbn, zxcvbnOptions } from "@zxcvbn-ts/core";
     import zxcvbnCommonPackage from "@zxcvbn-ts/language-common";
-    import type { OptionsType } from "@zxcvbn-ts/core/dist/types";
-    import type { PageServerData } from "./$types";
-    // Import languages
     import zxcvbnEnPackage from "@zxcvbn-ts/language-en";
 
-    export let data: PageServerData;
+    const options = {
+        dictionary: {
+            ...zxcvbnCommonPackage.dictionary,
+            ...zxcvbnEnPackage.dictionary
+        }
+    };
 
-    let password_strength = 0;
+    zxcvbnOptions.setOptions(options);
 
-    let password_requirements = [
-        { text: "minimum 8 characters", valid: false },
-        { text: "minimum 1 number", valid: false },
-        { text: "minimum 1 special character", valid: false },
-        { text: "minimum 1 lower-case or upper-case character", valid: false }
-    ];
+    // Broken
+    // See : https://github.com/pablo-abc/felte/issues/223#issuecomment-1510467575
+    let mounted = false;
+    onMount(() => {
+        mounted = true;
+    });
 
-    let email_input_element: HTMLInputElement;
-    let password_input_element: HTMLInputElement;
-    let confirm_password_input_element: HTMLInputElement;
+    export const schema = z.object({
+        email: z.string().email("Please enter a valid email address"),
 
-    // Client API:
-    const { form, errors, enhance } = superForm(data.form, {
-        validationMethod: "oninput",
-        validators: schema,
-        onSubmit: ({ cancel }) => {
-            if (!$form.email) {
-                email_input_element.focus();
-                $errors.email = ["Please enter a valid email address"];
-                cancel();
-            } else if (!$form.password) {
-                password_input_element.focus();
-                cancel();
-            } else if ($form.confirm_password !== $form.password) {
-                confirm_password_input_element.focus();
-                $errors.confirm_password = ["Passwords do not match"];
-                cancel();
-            } else if (password_strength < 4) {
-                cancel();
+        password: z
+            .string()
+            .min(8, "atleast_8")
+            .refine((val) => /(?=.*[!@#$%^&*()_+|~\-=?;:'",.<>{}[\]\\/])/.test(val), "missing_one_special_character")
+            .refine((val) => /(?=.*\d)/.test(val), "missing_one_number")
+            .refine((val) => /(?=.*[A-Z])|(?=.*[a-z])/.test(val), "missing_one_upper_or_lowercase"),
+
+        confirm_password: z.string()
+    });
+
+    const { form, errors, data } = createForm<z.infer<typeof schema>>({
+        extend: [reporter, validator({ schema })], // OR `extend: [validator],`
+        onSubmit: (values) => {
+            // ...
+        },
+        debounced: {
+            timeout: 100,
+            validate: (value) => {
+                // Configure ZXCVBN
+                if (value.password) {
+                    password_strength = zxcvbn(value.password).score;
+                } else {
+                    password_strength = 0;
+                }
+
+                return undefined;
             }
         }
     });
 
-    // Configure ZXCVBN
-    const zxcvbn_options: OptionsType = {
-        translations: zxcvbnEnPackage.translations,
-        graphs: zxcvbnCommonPackage.adjacencyGraphs,
-        dictionary: {
-            ...zxcvbnCommonPackage.dictionary,
-            ...zxcvbnEnPackage.dictionary,
-            userInputs: [...Object.values($form)]
-        }
+    let password_strength = 0;
+
+    const password_error_mapping: { [key: string]: string } = {
+        atleast_8: "minimum 8 characters",
+        missing_one_special_character: "minimum 1 special character",
+        missing_one_number: "minimum 1 number",
+        missing_one_upper_or_lowercase: "minimum 1 lower-case or upper-case character"
     };
-    zxcvbnOptions.setOptions(zxcvbn_options);
-
-    const password_validation = _.debounce(() => {
-        password_strength = zxcvbn($form.password).score;
-
-        let password_errors = $errors.password ?? [];
-        if (password_errors) {
-            password_requirements[0].valid = !password_errors.includes("atleast_8");
-            password_requirements[1].valid = !password_errors.includes("missing_one_number");
-            password_requirements[2].valid = !password_errors.includes("missing_one_special_character");
-            password_requirements[3].valid = !password_errors.includes("missing_one_upper_or_lowercase");
-        }
-    });
-
-    const confirm_password_validation = _.debounce((event) => {
-        if (event.target.value !== $form.password) {
-            $errors.confirm_password = ["Passwords do not match"];
-        }
-    });
 </script>
 
 <svelte:head>
-    <title>Register | AnimeCore</title>
+    <title>Register | CoreProject</title>
 </svelte:head>
 
 <register-page>
     <form
         class="flex h-full flex-col justify-between py-[1.5vw]"
         method="POST"
-        use:enhance
+        use:form
     >
         <form-fields>
             <email-field>
@@ -100,23 +91,26 @@
                 >
                     Email
                 </label>
-                <!-- svelte-ignore a11y-autofocus -->
                 <input
-                    bind:this={email_input_element}
-                    bind:value={$form.email}
                     name="email"
                     id="email"
-                    placeholder="username@mail"
-                    autofocus={true}
+                    placeholder="sora@coreproject.moe"
                     class="mt-[0.25vw] h-[3.125vw] w-full rounded-[0.75vw] border-[0.2vw] border-primary-500 bg-transparent pl-[1vw] text-[1.1vw] font-medium outline-none !ring-0 transition-all placeholder:text-white/50 focus:border-primary-400"
                 />
-                {#if $errors.email}
-                    <span class="text-[0.75vw] text-surface-300">{$errors.email[0]}</span>
-                {:else}
-                    <info class="mt-[0.5vw] flex items-center gap-[0.5vw]">
-                        <Info style="width: 0.9375vw; opacity: 0.7" />
-                        <span class="text-[0.75vw] text-surface-300">we’ll send you a verification email, so please ensure it’s active</span>
-                    </info>
+
+                {#if mounted}
+                    <ValidationMessage
+                        for="email"
+                        let:messages={message}
+                    >
+                        <span class="mt-[0.5vw] text-[0.75vw] text-surface-300">{message}</span>
+                        <div slot="placeholder">
+                            <info class="mt-[0.5vw] flex items-center gap-[0.5vw]">
+                                <Info style="width: 0.9375vw; opacity: 0.7" />
+                                <span class="text-[0.75vw] text-surface-300">we’ll send you a verification email, so please ensure it’s active</span>
+                            </info>
+                        </div>
+                    </ValidationMessage>
                 {/if}
             </email-field>
 
@@ -130,14 +124,11 @@
                 <div>
                     <div class="relative flex flex-col">
                         <input
-                            bind:this={password_input_element}
-                            bind:value={$form.password}
                             type="text"
                             id="password"
                             name="password"
                             placeholder="enter a strong password"
                             class="mt-[0.25vw] h-[3.125vw] w-full rounded-[0.75vw] border-[0.2vw] border-primary-500 bg-transparent pl-[1vw] text-[1.1vw] font-medium outline-none !ring-0 transition-all placeholder:text-white/50 focus:border-primary-400"
-                            on:input={password_validation}
                         />
                     </div>
                     <password-strength class="mt-[1vw] flex flex-col">
@@ -154,25 +145,62 @@
                         <div class="mt-[1vw]">
                             <span class="mt-[1vw] text-[1vw] font-semibold uppercase tracking-wider text-surface-50">must contain</span>
                             <div class="ml-[0.75vw] mt-[0.4vw] flex w-3/5 flex-col gap-[0.1vw]">
-                                {#each password_requirements as requirement}
+                                {#if mounted}
+                                    <ValidationMessage
+                                        for="password"
+                                        let:messages={message}
+                                    >
+                                        <!-- So we get an array of items  -->
+                                        {#if Array.isArray(message)}
+                                            <!-- Logics for cross -->
+                                            {#each message as crossed_item}
+                                                <div class="flex gap-2">
+                                                    <svelte:component
+                                                        this={Tick}
+                                                        class="col-span-1"
+                                                        style="width: 0.7vw; color: deepskyblue; opacity: 0.9"
+                                                    />
+                                                    <span class="mt-[0.5vw] text-[0.75vw] text-surface-300">{password_error_mapping[crossed_item]}</span>
+                                                </div>
+                                            {/each}
+
+                                            <!-- Logics for cross items  -->
+                                            {#each Object.keys(password_error_mapping).filter((key) => !message.includes(key)) as ticked_item}
+                                                <div class="flex gap-2">
+                                                    <svelte:component
+                                                        this={Cross}
+                                                        style="width: 0.9vw; color: red; opacity: 0.8"
+                                                    />
+                                                    <span class="text-[0.8vw] text-surface-300">{password_error_mapping[ticked_item]}</span>
+                                                </div>
+                                            {/each}
+                                        {/if}
+                                        <div slot="placeholder">
+                                            <div class="flex flex-col gap-1">
+                                                {#each Object.values(password_error_mapping) as item}
+                                                    <div class="flex gap-2">
+                                                        <svelte:component
+                                                            this={Cross}
+                                                            style="width: 0.9vw; color: red; opacity: 0.8"
+                                                        />
+                                                        <span class="text-[0.8vw] text-surface-300">{item}</span>
+                                                    </div>
+                                                {/each}
+                                            </div>
+                                        </div>
+                                    </ValidationMessage>
+                                {/if}
+                                <!-- {#each password_requirements as requirement}
                                     <div class="grid grid-cols-12 items-center">
                                         {#if requirement.valid}
-                                            <svelte:component
-                                                this={Tick}
-                                                class="col-span-1"
-                                                style="width: 0.7vw; color: deepskyblue; opacity: 0.9"
-                                            />
+                                          
                                         {:else}
-                                            <svelte:component
-                                                this={Cross}
-                                                class="col-span-1"
-                                                style="width: 0.9vw; color: red; opacity: 0.8"
-                                            />
+                                         
                                         {/if}
 
                                         <span class="col-span-11 text-[0.8vw] text-surface-300">{requirement.text}</span>
                                     </div>
-                                {/each}
+                                {/each} -->
                             </div>
                         </div>
                     </password-strength>
@@ -189,18 +217,15 @@
                 <div>
                     <div class="relative flex flex-col">
                         <input
-                            bind:this={confirm_password_input_element}
-                            bind:value={$form.confirm_password}
                             type="password"
                             id="confirm_password"
                             name="confirm_password"
                             placeholder="re-enter your password"
                             class="mt-[0.25vw] h-[3.125vw] w-full rounded-[0.75vw] border-[0.2vw] border-primary-500 bg-transparent pl-[1vw] text-[1.1vw] font-medium outline-none !ring-0 transition-all placeholder:text-white/50 focus:border-primary-400"
-                            on:input={confirm_password_validation}
                         />
-                        {#if $errors.confirm_password}
+                        <!-- {#if $errors.confirm_password}
                             <span class="mt-[0.5vw] text-[0.75vw] text-surface-300">{$errors.confirm_password}</span>
-                        {/if}
+                        {/if} -->
                     </div>
                 </div>
             </confirm-password-field>
