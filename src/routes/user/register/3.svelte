5c8f78a2428c51817e85da1f955cd567808fcc1e
Suneeth <suneeth.since2005@gmail.com>
2023-05-09 18:09:27 +0530

    :sparkles: Update register page final step mobile design

--- src/routes/user/register/3.svelte
+++ src/routes/user/register/3.svelte
@@ -35,12 +35,12 @@
 </script>
 
 <form
-    class="flex h-full flex-col justify-between"
+    class="flex h-max w-full flex-col bg-surface-900 px-10 py-14 md:h-full md:justify-between md:rounded-none md:p-0"
     use:form
     use:focusTrap={true}
 >
     <div class="flex flex-col items-start gap-[3vw]">
-        <span class="flex items-center pb-[1vw] text-[1.2vw] font-bold uppercase tracking-widest text-white">
+        <span class="flex items-center pb-3 text-base md:text-[1.2vw] font-bold uppercase tracking-widest text-white">
             welcome to&nbsp;
             <p class="inline-flex items-center text-surface-50">
                 {#each "core".split("") as item}
@@ -51,51 +51,51 @@
         </span>
 
         <user-info class="flex flex-col">
-            <span class="pb-[0.75vw] text-[1.5vw] font-semibold text-primary-500">Summary</span>
+            <span class="text-lg md:text-[1.5vw] font-semibold text-primary-500">Summary</span>
 
-            <username class="flex flex-col pt-[1vw]">
-                <span class="text-[1.1vw] font-medium">Username</span>
-                <span class="text-[1.1vw] font-medium text-surface-300">{combined_object.username}</span>
+            <username class="flex flex-col pt-5">
+                <span class="text-lg md:text-[1.1vw] font-medium">Username</span>
+                <span class="text-base md:text-[1.1vw] font-medium text-surface-300">{combined_object.username}</span>
             </username>
 
-            <email class="flex flex-col pt-[1.5vw]">
-                <span class="text-[1.1vw] font-medium">Email</span>
-                <span class="text-[1.1vw] font-medium text-surface-300">{combined_object.email}</span>
+            <email class="flex flex-col pt-3">
+                <span class="text-lg md:text-[1.1vw] font-medium">Email</span>
+                <span class="text-base md:text-[1.1vw] font-medium text-surface-300">{combined_object.email}</span>
             </email>
         </user-info>
 
-        <div class="flex flex-col items-start">
+        <div class="flex flex-col">
             <button
                 type="button"
-                class="btn p-0 text-[1vw] font-semibold text-primary-600 underline"
+                class="btn p-0 text-base md:text-[1vw] font-semibold text-primary-600 underline"
             >
-                {@html `< change username >`}
+                {@html `< resend code >`}
             </button>
             <button
                 type="button"
-                class="btn p-0 text-[1vw] font-semibold text-primary-600 underline"
+                class="btn p-0 text-base md:text-[1vw] font-semibold text-primary-600 underline"
             >
                 {@html `< change email >`}
             </button>
         </div>
     </div>
 
-    <div class="flex items-center justify-between">
+    <div class="mt-10 flex items-center justify-between md:mt-0">
         <div class="flex flex-col">
-            <span class="text-[0.75vw] text-surface-100">Already have an account?</span>
+            <span class="text-xs text-surface-100 md:text-[0.75vw]">Already have an account?</span>
             <a
                 href="./login"
-                class="text-[1.1vw]"
+                class="text-base md:text-[1.1vw]"
             >
                 Login
             </a>
         </div>
         <button
             type="submit"
-            class="btn h-[2.75vw] rounded-[0.5vw] bg-secondary-800 p-0 px-[1.25vw] text-[0.95vw] font-semibold"
+            class="btn h-12 rounded-lg bg-secondary-800 p-0 px-5 text-base font-semibold md:h-[2.75vw] md:rounded-[0.5vw] md:px-[1.25vw] md:text-[0.95vw]"
         >
             <span>Finish</span>
-            <Tick class="w-[0.75vw]" />
+            <Tick class="w-3 md:w-[0.75vw]" />
         </button>
     </div>
 </form>
