5b7422b24d0cbbcd3d18461cab90608ed88624f0
Suneeth <suneeth.since2005@gmail.com>
2023-05-13 11:34:49 +0530

    Update mock data

--- src/lib/data/mock/latest_episodes.ts
+++ src/lib/data/mock/latest_episodes.ts
@@ -2,28 +2,28 @@ export const latest_episodes = [
     {
         id: 1,
         name: "SpyxFamily",
-        cover: "https://dwgkfo5b3odmw.cloudfront.net/img/manga_series_header/632-SJ_SpyFamily_2000x800_jpg_wm",
+        cover: "https://www.cheatsheet.com/wp-content/uploads/2022/05/Spy-x-Family-Episode-7.jpg",
         episode_number: 6,
         release_date: "2023-04-22T10:30:00.000Z"
     },
     {
         id: 2,
         name: "Kaguya-sama: Love Is War",
-        cover: "https://f1.econotimes.com/2020/09/xz8zrddkwz_th_1024x0.jpg",
+        cover: "https://i.imgflip.com/2tb82r.png",
         episode_number: 5,
         release_date: "2023-04-22T10:30:00.000Z"
     },
     {
         id: 3,
         name: "Aharen-san wa Hakaraenai",
-        cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJCWBn4FOJWr0Ocg_2XjySBAaeOKk90ShR2ryoyGk5-izOUdA1N3hUKMWHKFdpVbf26Ij5kyWcDHw&usqp=CAU&ec=48665698",
+        cover: "https://i0.wp.com/www.theanimedaily.com/wp-content/uploads/2022/05/Aharen-San-Episode-9.jpg?resize=1024%2C586&ssl=1",
         episode_number: 9,
         release_date: "2023-04-22T11:30:00.000Z"
     },
     {
         id: 4,
         name: "Summer time Rendering",
-        cover: "https://images-geeknative-com.exactdn.com/wp-content/uploads/2021/10/18220337/Summer-Time-Rendering-banner-scaled.jpg?strip=all&lossy=1&sharp=1&ssl=1",
+        cover: "https://i0.wp.com/blog-media-cloud.nyc3.digitaloceanspaces.com/2022/07/Summertime-Rendering-12.mkv_20220703183414_0004.png?fit=1920%2C1080&ssl=1",
         episode_number: 12,
         release_date: "2023-04-22T12:30:00.000Z"
     },
