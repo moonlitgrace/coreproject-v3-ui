79ae166807210bba5059c8892ed4426f6c3c32f9
Suneeth <suneeth.since2005@gmail.com>
2023-05-13 22:22:28 +0530

    Add popular genres mock data

---
+++ src/lib/data/mock/popular_genres.ts
@@ -0,0 +1,17 @@
+export const popular_genres = [
+	{
+		title: "Action",
+		estimated_total_animes: 1000,
+		cover: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+	},
+	{
+		title: "Romantic",
+		estimated_total_animes: 700,
+		cover: "https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg",
+	},
+	{
+		title: "Shonen manga",
+		estimated_total_animes: 900,
+		cover: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg"
+	}
+]
