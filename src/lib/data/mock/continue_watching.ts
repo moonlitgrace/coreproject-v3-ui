a6c13567b8091bfc984f6747ba9fcf9da990b77c
Suneeth <suneeth.since2005@gmail.com>
2023-05-13 12:21:06 +0530

    Create continue_watching.ts

---
+++ src/lib/data/mock/continue_watching.ts
@@ -0,0 +1,9 @@
+export const continue_watching = [
+	{
+		name: "Oshi no ko",
+		banner: "https://i.redd.it/6ahm7k9gva5a1.jpg",
+		current_episode: 4,
+        episodes_count: 12,
+        time_watched: 1356
+	}
+]
