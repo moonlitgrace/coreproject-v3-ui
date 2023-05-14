0f64a205b93137f57285596d235711d3d0bbeb07
Suneeth <suneeth.since2005@gmail.com>
2023-05-14 19:44:16 +0530

    Add popular genres mock data

--- src/lib/data/mock/popular_genres.ts
+++ src/lib/data/mock/popular_genres.ts
@@ -1,17 +1,176 @@
 export const popular_genres = [
-	{
+	{	
+		id: 1,
 		title: "Action",
+		description: "It features high-stakes battles, dynamic fight scenes, and exciting chases. If you are a fan of thrilling and intense moments and enjoy watching shows with themes of courage, determination, and perseverance. Attack on Titans and Jujutsu Kaisen are some popular anime of this genre.",
 		estimated_total_animes: 1000,
 		cover: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+		animes: [
+			{
+				id: 1,
+				name: "Jujutsu Kaisen",
+				synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
+				banner: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+				episodes_count: 12,
+				rating: 4.7
+			},
+			{
+				id: 2,
+				name: "Kimetsu no Yaiba",
+				synopsis: `It is the Taisho Period in Japan.Tanjiro, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon.To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself.Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill the demon that massacred his family.`,
+				banner: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
+				episodes_count: 13,
+				rating: 4.5
+			},
+			{
+				id: 3,
+				name: "Hyouka",
+				banner: "https://imgbox.io/ib/JSf13DUhKz.jpg",
+				synopsis: `High school freshman Houtarou Oreki has but one goal: to lead a gray life while conserving as much energy as he can. Unfortunately, his peaceful days come to an end when his older sister, Tomoe, forces him to save the memberless Classics Club from disbandment.`,
+				episodes_count: 9,
+				rating: 4.5
+			},
+			{
+				id: 4,
+				name: "You Lie in April",
+				banner: "https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg",
+				synopsis: `Kousei Arima is a child prodigy known as the "Human Metronome" for playing the piano with precision and perfection. Guided by a strict mother and rigorous training, Kousei dominates every competition he enters, earning the admiration of his musical peers and praise from audiences. When his mother suddenly passes away, the subsequent trauma makes him unable to hear the sound of a piano, and he never takes the stage thereafter.`,
+				episodes_count: 22,
+				rating: 4.3
+			},
+			{
+				id: 5,
+				name: "Attack on Titan",
+				banner: "https://images.saymedia-content.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDQ0NTI1OTEyMDczODYy/attack-on-titan-season-1-review.jpg",
+				synopsis: `Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called Titans, forcing humans to hide in fear behind enormous concentric walls. What makes these giants truly terrifying is that their taste for human flesh is not born out of hunger but what appears to be out of pleasure. To ensure their survival, the remnants of humanity began living within defensive barriers, resulting in one hundred years without a single titan encounter. However, that fragile calm is soon shattered when a colossal Titan manages to breach the supposedly impregnable outer wall, reigniting the fight for survival against the man-eating abominations.`,
+				episodes_count: 25,
+				rating: 4.7
+			},
+			{
+				id: 6,
+				name: "Death Note",
+				banner: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/11/Death-Note-Cover.jpg",
+				synopsis: `Brutal murders, petty thefts, and senseless violence pollute the human world. In contrast, the realm of death gods is a humdrum, unchanging gambling den. The ingenious 17-year-old Japanese student Light Yagami and sadistic god of death Ryuk share one belief: their worlds are rotten.`,
+				episodes_count: 24,
+				rating: 4.8
+			}
+
+		]
 	},
 	{
-		title: "Romantic",
+		id: 2,
+		title: "Romance",
+		description: "Romance anime explores love and relationships between its characters. This genre often focuses on the development of romantic feelings between its characters, as well as the challenges they face in maintaining those relationships. These anime can range from lighthearted and playful to serious and heart-wrenching.",
 		estimated_total_animes: 700,
 		cover: "https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg",
+		animes: [
+			{
+				id: 1,
+				name: "Jujutsu Kaisen",
+				synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
+				banner: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+				episodes_count: 12,
+				rating: 4.7
+			},
+			{
+				id: 2,
+				name: "Kimetsu no Yaiba",
+				synopsis: `It is the Taisho Period in Japan.Tanjiro, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon.To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself.Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill the demon that massacred his family.`,
+				banner: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
+				episodes_count: 13,
+				rating: 4.5
+			},
+			{
+				id: 3,
+				name: "Hyouka",
+				banner: "https://imgbox.io/ib/JSf13DUhKz.jpg",
+				synopsis: `High school freshman Houtarou Oreki has but one goal: to lead a gray life while conserving as much energy as he can. Unfortunately, his peaceful days come to an end when his older sister, Tomoe, forces him to save the memberless Classics Club from disbandment.`,
+				episodes_count: 9,
+				rating: 4.5
+			},
+			{
+				id: 4,
+				name: "You Lie in April",
+				banner: "https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg",
+				synopsis: `Kousei Arima is a child prodigy known as the "Human Metronome" for playing the piano with precision and perfection. Guided by a strict mother and rigorous training, Kousei dominates every competition he enters, earning the admiration of his musical peers and praise from audiences. When his mother suddenly passes away, the subsequent trauma makes him unable to hear the sound of a piano, and he never takes the stage thereafter.`,
+				episodes_count: 22,
+				rating: 4.3
+			},
+			{
+				id: 5,
+				name: "Attack on Titan",
+				banner: "https://images.saymedia-content.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDQ0NTI1OTEyMDczODYy/attack-on-titan-season-1-review.jpg",
+				synopsis: `Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called Titans, forcing humans to hide in fear behind enormous concentric walls. What makes these giants truly terrifying is that their taste for human flesh is not born out of hunger but what appears to be out of pleasure. To ensure their survival, the remnants of humanity began living within defensive barriers, resulting in one hundred years without a single titan encounter. However, that fragile calm is soon shattered when a colossal Titan manages to breach the supposedly impregnable outer wall, reigniting the fight for survival against the man-eating abominations.`,
+				episodes_count: 25,
+				rating: 4.7
+			},
+			{
+				id: 6,
+				name: "Death Note",
+				banner: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/11/Death-Note-Cover.jpg",
+				synopsis: `Brutal murders, petty thefts, and senseless violence pollute the human world. In contrast, the realm of death gods is a humdrum, unchanging gambling den. The ingenious 17-year-old Japanese student Light Yagami and sadistic god of death Ryuk share one belief: their worlds are rotten.`,
+				episodes_count: 24,
+				rating: 4.8
+			}
+
+		]
 	},
 	{
+		id: 3,
 		title: "Shonen manga",
+		description: "Shonen: Manga targeted at tween and teen boys. Common themes: Action, Adventure, Friendship, Coming of Age. Shonen manga often feature lots of action and comedy, and some sort of coming-of-age camaraderie between characters.",
 		estimated_total_animes: 900,
-		cover: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg"
+		cover: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
+		animes: [
+			{
+				id: 1,
+				name: "Jujutsu Kaisen",
+				synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
+				banner: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+				episodes_count: 12,
+				rating: 4.7
+			},
+			{
+				id: 2,
+				name: "Kimetsu no Yaiba",
+				synopsis: `It is the Taisho Period in Japan.Tanjiro, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon.To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself.Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill the demon that massacred his family.`,
+				banner: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
+				episodes_count: 13,
+				rating: 4.5
+			},
+			{
+				id: 3,
+				name: "Hyouka",
+				banner: "https://imgbox.io/ib/JSf13DUhKz.jpg",
+				synopsis: `High school freshman Houtarou Oreki has but one goal: to lead a gray life while conserving as much energy as he can. Unfortunately, his peaceful days come to an end when his older sister, Tomoe, forces him to save the memberless Classics Club from disbandment.`,
+				episodes_count: 9,
+				rating: 4.5
+			},
+			{
+				id: 4,
+				name: "You Lie in April",
+				banner: "https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg",
+				synopsis: `Kousei Arima is a child prodigy known as the "Human Metronome" for playing the piano with precision and perfection. Guided by a strict mother and rigorous training, Kousei dominates every competition he enters, earning the admiration of his musical peers and praise from audiences. When his mother suddenly passes away, the subsequent trauma makes him unable to hear the sound of a piano, and he never takes the stage thereafter.`,
+				episodes_count: 22,
+				rating: 4.3
+			},
+			{
+				id: 5,
+				name: "Attack on Titan",
+				banner: "https://images.saymedia-content.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDQ0NTI1OTEyMDczODYy/attack-on-titan-season-1-review.jpg",
+				synopsis: `Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called Titans, forcing humans to hide in fear behind enormous concentric walls. What makes these giants truly terrifying is that their taste for human flesh is not born out of hunger but what appears to be out of pleasure. To ensure their survival, the remnants of humanity began living within defensive barriers, resulting in one hundred years without a single titan encounter. However, that fragile calm is soon shattered when a colossal Titan manages to breach the supposedly impregnable outer wall, reigniting the fight for survival against the man-eating abominations.`,
+				episodes_count: 25,
+				rating: 4.7
+			},
+			{
+				id: 6,
+				name: "Death Note",
+				banner: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/11/Death-Note-Cover.jpg",
+				synopsis: `Brutal murders, petty thefts, and senseless violence pollute the human world. In contrast, the realm of death gods is a humdrum, unchanging gambling den. The ingenious 17-year-old Japanese student Light Yagami and sadistic god of death Ryuk share one belief: their worlds are rotten.`,
+				episodes_count: 24,
+				rating: 4.8
+			}
+
+		]
 	}
 ]
