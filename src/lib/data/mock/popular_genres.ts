098bc7b52d75efc65130d414b8c0c4812d9e9995
Suneeth <suneeth.since2005@gmail.com>
2023-05-14 23:22:48 +0530

    Update popular_genres.ts

--- src/lib/data/mock/popular_genres.ts
+++ src/lib/data/mock/popular_genres.ts
@@ -41,14 +41,6 @@ export const popular_genres = [
 		animes: [
 			{
 				id: 1,
-				name: "Jujutsu Kaisen",
-				synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
-				banner: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
-				episodes_count: 12,
-				rating: 4.7
-			},
-			{
-				id: 2,
 				name: "Kimetsu no Yaiba",
 				synopsis: `It is the Taisho Period in Japan.Tanjiro, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon.To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself.Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill the demon that massacred his family.`,
 				banner: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
@@ -56,12 +48,20 @@ export const popular_genres = [
 				rating: 4.5
 			},
 			{
-				id: 3,
+				id: 2,
 				name: "Hyouka",
 				banner: "https://imgbox.io/ib/JSf13DUhKz.jpg",
 				synopsis: `High school freshman Houtarou Oreki has but one goal: to lead a gray life while conserving as much energy as he can. Unfortunately, his peaceful days come to an end when his older sister, Tomoe, forces him to save the memberless Classics Club from disbandment.`,
 				episodes_count: 9,
 				rating: 4.5
+			},
+			{
+				id: 3,
+				name: "Jujutsu Kaisen",
+				synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
+				banner: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+				episodes_count: 12,
+				rating: 4.7
 			},
 
 		]
@@ -75,19 +75,19 @@ export const popular_genres = [
 		animes: [
 			{
 				id: 1,
-				name: "Jujutsu Kaisen",
-				synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
-				banner: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
-				episodes_count: 12,
-				rating: 4.7
-			},
-			{
-				id: 2,
 				name: "Kimetsu no Yaiba",
 				synopsis: `It is the Taisho Period in Japan.Tanjiro, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon.To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself.Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill the demon that massacred his family.`,
 				banner: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
 				episodes_count: 13,
 				rating: 4.5
+			},
+			{
+				id: 2,
+				name: "Jujutsu Kaisen",
+				synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
+				banner: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+				episodes_count: 12,
+				rating: 4.7
 			},
 			{
 				id: 3,
