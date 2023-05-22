8199bc4ffe050dff2b68c6935a994c1d5dd002b3
sssuneeth <suneeth.since2005@gmail.com>
2023-05-22 23:09:43 +0530

    Add mock data for most views

---
+++ src/lib/data/mock/most_viewed.ts
@@ -0,0 +1,35 @@
+export const most_viewed = {
+	today: [
+		{
+			id: 1,
+			cover: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
+			name: "Kimetsu no Yaiba Demon",
+			views: 8129456
+		},{
+			id: 2,
+			cover: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+			name: "Jujutsu Kaisen",
+			views: 7451386
+		},{
+			id: 3,
+			cover: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/11/Death-Note-Cover.jpg",
+			name: "Death Note",
+			views: 6845136
+		},{
+			id: 4,
+			cover: "https://images.saymedia-content.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDQ0NTI1OTEyMDczODYy/attack-on-titan-season-1-review.jpg",
+			name: "Attack on Titan",
+			views: 6512898
+		},{
+			id: 5,
+			cover: "https://c4.wallpaperflare.com/wallpaper/470/72/942/hyouka-anime-chitanda-eru-fukube-satoshi-wallpaper-preview.jpg",
+			name: "Hyouka",
+			views: 6128659
+		},{
+			id: 6,
+			cover: "https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg",
+			name: "You Lie in April",
+			views: 5874132
+		},
+	]
+}
