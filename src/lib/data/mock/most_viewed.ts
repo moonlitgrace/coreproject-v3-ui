e02d1b5c25f2c145bcc08b011a8c0d5b32dee4fb
sssuneeth <suneeth.since2005@gmail.com>
2023-05-23 11:02:44 +0530

    Update mock data

--- src/lib/data/mock/most_viewed.ts
+++ src/lib/data/mock/most_viewed.ts
@@ -1,35 +1,109 @@
 export const most_viewed = {
 	today: [
 		{
-			id: 1,
-			cover: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
-			name: "Kimetsu no Yaiba Demon",
-			views: 8129456
-		},{
-			id: 2,
-			cover: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
-			name: "Jujutsu Kaisen",
-			views: 7451386
-		},{
-			id: 3,
-			cover: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/11/Death-Note-Cover.jpg",
-			name: "Death Note",
-			views: 6845136
-		},{
-			id: 4,
-			cover: "https://images.saymedia-content.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDQ0NTI1OTEyMDczODYy/attack-on-titan-season-1-review.jpg",
-			name: "Attack on Titan",
-			views: 6512898
-		},{
-			id: 5,
-			cover: "https://c4.wallpaperflare.com/wallpaper/470/72/942/hyouka-anime-chitanda-eru-fukube-satoshi-wallpaper-preview.jpg",
-			name: "Hyouka",
-			views: 6128659
-		},{
-			id: 6,
-			cover: "https://files.otakustudy.com/wp-content/uploads/2020/10/10153058/your-lie-in-april-cover.jpg",
-			name: "You Lie in April",
-			views: 5874132
-		},
+	        id: 1,
+	        name: "Kimetsu no Yaiba",
+	        cover: "https://cdn-cfmok.nitrocdn.com/juJsjTwdTqWjkJBaBrLnvLeovPsDevAD/assets/images/optimized/rev-37d38d6/wp-content/uploads/1587837891_336_Demon-Slayer-Kimetsu-No-Yaiba-4K-Wallpapers-2020.jpg",
+	        synopsis: `It is the Taisho Period in Japan. Tanjiro, a kindhearted boy who sells charcoal for a
+				living, finds his family slaughtered by a demon. To make matters worse, his younger sister
+				Nezuko, the sole survivor, has been transformed into a demon herself. Though devastated by
+				this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his
+				sister back into a human, and kill the demon that massacred his family.`,
+	        current_episode: 4,
+	        episodes_count: 12,
+	        genres: ["Action", "Ecchi", "sci-Fi"],
+	        type: "TV",
+	        release_date: "2023-04-22T10:30:00.000Z",
+	        studios: ["Bibury Animation Studios"],
+	        views: 987654
+	    },
+	    {
+	        id: 2,
+	        name: "Jujutsu Kaisen",
+	        cover: "https://images.ottplay.com/articles/2021q2/5_reasons_every_anim_OTTplay_news_cover_image_1_688.jpeg",
+	        synopsis: `Idly indulging in baseless paranormal activities with the Occult Club, high schooler Yuuji Itadori spends his days at either the clubroom or the hospital, where he visits his bedridden grandfather. However, this leisurely lifestyle soon takes a turn for the strange when he unknowingly encounters a cursed item. Triggering a chain of supernatural occurrences, Yuuji finds himself suddenly thrust into the world of Curses—dreadful beings formed from human malice and negativity—after swallowing the said item, revealed to be a finger belonging to the demon Sukuna Ryoumen, the "King of Curses."`,
+	        current_episode: 11,
+	        episodes_count: 12,
+	        genres: ["Love", "Drama", "Action"],
+	        type: "TV",
+	        release_date: "2013-08-22T10:30:00.000Z",
+	        studios: ["Kyoto Animations"],
+	        views: 876543
+	    },
+	    {
+	        id: 3,
+	        name: "Death Note",
+	        cover: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/11/Death-Note-Cover.jpg",
+	        synopsis: `Brutal murders, petty thefts, and senseless violence pollute the human world. In contrast, the realm of death gods is a humdrum, unchanging gambling den. The ingenious 17-year-old Japanese student Light Yagami and sadistic god of death Ryuk share one belief: their worlds are rotten.
+				For his own amusement, Ryuk drops his Death Note into the human world. Light stumbles upon it, deeming the first of its rules ridiculous: the human whose name is written in this note shall die. However, the temptation is too great, and Light experiments by writing a felon's name, which disturbingly enacts his first murder.`,
+		        updated: "2020-10-3T02:37:40.790Z",
+	        current_episode: 1,
+	        episodes_count: 6,
+	        genres: ["Action", "Ecchi", "sci-Fi"],
+	        type: "TV",
+	        release_date: "2020-12-22T10:30:00.000Z",
+	        studios: ["WI Studios"],
+	        views: 765432
+	    },
+	    {
+	        id: 4,
+	        name: "7SEEDS",
+	        cover: "https://socialanime.it/upload_serie/s_12775_small.jpg",
+	        synopsis: `
+					Imagine this: you are living a normal day in your life. Maybe you are out with friends, eating your family's home-cooked meal or spending time with your girlfriend. When you next wake up, you are suddenly thrust into a strange, new world, surrounded by five strangers on a rapidly sinking boat in the middle of a storm.
+					`,
+	        current_episode: 5,
+	        episodes_count: 12,
+	        genres: ["Action", "Horror", "sci-Fi"],
+	        type: "TV",
+	        release_date: "2010-01-22T10:30:00.000Z",
+	        studios: ["Bibury Animation Studios"],
+	        views: 654321
+	    },
+	    {
+	        id: 5,
+	        name: "Shakugan no Shana",
+	        cover: "https://imagecache.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/7ae19317-50ed-4a41-2cd6-8ae4424a4200/width=450/245470-1742259591-masterpiece,%20best%20quality,%20absurdres,%20perfect%20antomy,%20cinematic%20lighting,%20Shana,%20red%20hair,%20red%20eyes,%20kimono,%20flower-print,%20stand.jpeg",
+	        synopsis: `
+					The world has become a slaughtering ground for the Crimson Denizens, mysterious beings from a parallel universe who thrive on the life energy of humans. These merciless murderers only leave behind scant remainders of souls called "Torches," which are mere residues that will eventually be destroyed, along with the very fact of the victims' existence from the minds of the living. In an ambitious endeavor to put an end to this invisible, hungry massacre, warriors called Flame Hazes relentlessly fight these monsters.
+					`,
+	        current_episode: 19,
+	        episodes_count: 24,
+	        genres: ["Action", "Ecchi", "sci-Fi"],
+	        type: "TV",
+	        release_date: "2016-06-22T10:30:00.000Z",
+	        studios: ["Kyoto Studios"],
+	        views: 543219
+	    },
+	    {
+	        id: 6,
+	        name: "Death note",
+	        cover: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx12445-hKIXQW3vA4iz.jpg",
+	        synopsis: `
+					Brutal murders, petty thefts, and senseless violence pollute the human world. In contrast, the realm of death gods is a humdrum, unchanging gambling den. The ingenious 17-year-old Japanese student Light Yagami and sadistic god of death Ryuk share one belief: their worlds are rotten.
+					`,
+	        current_episode: 8,
+	        episodes_count: 28,
+	        genres: ["Action", "Drama", "Triller"],
+	        type: "TV",
+	        release_date: "2018-10-22T10:30:00.000Z",
+	        studios: ["Animation XXX"],
+	        views: 432198
+	    },
+	    {
+	        id: 7,
+	        name: "To LOVE-Ru",
+	        cover: "https://cdn.myanimelist.net/images/anime/9/79330.jpg",
+	        synopsis: `
+					Rito Yuuki never gets a break—he's always finding himself in lewd accidents with girls around him. Although his heart still yearns for Haruna, his childhood love, Rito can't help but question his feelings for Lala, the alien princess who appeared in front of him and declared she would marry him. But now, it's not just Lala he has to deal with: her younger twin sisters, Momo and Nana, have also traveled to Earth, wanting to meet their older sister's fiancé, and just as luck would have it, they end up staying at Rito's home.
+					`,
+	        current_episode: 9,
+	        episodes_count: 26,
+	        genres: ["Romantic", "Ecchi", "School"],
+	        type: "TV",
+	        release_date: "2021-7-22T10:30:00.000Z",
+	        studios: ["Bibury Animation Studios"],
+	        views: 321987
+	    }
 	]
 }
