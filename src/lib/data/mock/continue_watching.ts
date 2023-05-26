3ef675b15190ac0b7c5133d1e8e76129b98c2836
sssuneeth <suneeth.since2005@gmail.com>
2023-05-26 22:37:22 +0530

    Update continue_watching.ts

--- src/lib/data/mock/continue_watching.ts
+++ src/lib/data/mock/continue_watching.ts
@@ -1,14 +1,14 @@
 export const continue_watching = [
 	{
 		name: "Oshi no ko",
-		banner: "https://i.redd.it/6ahm7k9gva5a1.jpg",
+		banner: "/images/Oshinoko.jpg",
 		current_episode: 4,
 		episodes_count: 12,
 		time_watched: 1356
 	},
 	{
 		name: "Kono Subarashii Sekai ni Bakuen wo!",
-		banner: "https://animefagos.com/wp-content/uploads/2022/05/konosubaexplosion01-630x450.jpg",
+		banner: "/images/KonoSubarashii.jpg",
 		current_episode: 7,
 		episodes_count: 25,
 		time_watched: 1127
