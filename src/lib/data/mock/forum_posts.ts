295af73c0cd5119fbe6d126e11ebab7117c08419
sssuneeth <suneeth.since2005@gmail.com>
2023-05-28 20:16:19 +0530

    add: forum posts mock data

---
+++ src/lib/data/mock/forum_posts.ts
@@ -0,0 +1,10 @@
+export const forum_posts = [
+	{
+		title: "Celebrating 10 years of Hyouka! with all anime lovers.",
+		description: `Ousei Arima is a child prodigy known as the "Human Metronome" for playing the piano with precision and perfection. Guided by a strict mother and rigorous training, Kousei dominates every competition he enters`,
+		banner: "https://blog.sakugabooru.com/wp-content/uploads/2017/11/hk22-1038x576.jpg",
+		auther: "Eiennlaio",
+		posted_on: "2023-04-20T15:38:51.162Z",
+		responses: 69
+	}
+]
