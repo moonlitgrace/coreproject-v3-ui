719021e75a61e2f8d4e7e4768e7020e98bff8657
sssuneeth <suneeth.since2005@gmail.com>
2023-05-31 16:42:16 +0530

    style: Emoji picker custom styled

--- src/lib/components/shared/tippies/emoji_picker.svelte
+++ src/lib/components/shared/tippies/emoji_picker.svelte
@@ -6,14 +6,37 @@
 
     onMount(async () => {
         await import("emoji-picker-element");
+        const picker = document.querySelector("emoji-picker");
 
-        const emoji_picker = document.querySelector("emoji-picker");
-        if (emoji_picker) {
-            emoji_picker.addEventListener("emoji-click", event => {
-                insertTextAtCursor(text_element, event.detail.unicode ?? "");
-            });
-        }
+        picker?.addEventListener("emoji-click", (event) => insertTextAtCursor(text_element, event.detail.unicode ?? ""));
+
+        /* Shadow DOM styling */
+        const style = document.createElement("style");
+        style.textContent = `
+        	.picker {
+        		border-radius: 0.5vw;
+        	}
+        `;
+        picker?.shadowRoot?.appendChild(style);
     });
 </script>
 
 <emoji-picker />
+
+<style>
+    emoji-picker {
+        height: 25vw;
+        --num-columns: 7;
+        --emoji-size: 1.5vw;
+        --background: #1e2036;
+        --button-hover-background: #3e405b;
+        --button-active-background: #4d4f68;
+        --input-border-color: #72717c;
+        --input-border-radius: 0.5vw;
+        --input-border-size: 0.1vw;
+        --outline-size: 0.1vw;
+        --outline-color: #9c9ba3;
+        --input-font-size: 1vw;
+        --input-padding: 0.35vw 0.75vw;
+    }
+</style>
