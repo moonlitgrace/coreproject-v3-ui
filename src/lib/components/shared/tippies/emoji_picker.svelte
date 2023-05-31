ea877e81f043d383693c77685c327dfffcb3a3ce
sssuneeth <suneeth.since2005@gmail.com>
2023-05-31 10:07:16 +0530

    chore: add `emoji-picker-element`

---
+++ src/lib/components/shared/tippies/emoji_picker.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import insertTextAtCursor from "insert-text-at-cursor";
+    import { onMount } from "svelte";
+
+    export let text_element: HTMLTextAreaElement;
+
+    onMount(async () => {
+        await import("emoji-picker-element");
+
+        const emoji_picker = document.querySelector("emoji-picker");
+        if (emoji_picker) {
+            emoji_picker.addEventListener("emoji-click", event => {
+                insertTextAtCursor(text_element, event.detail.unicode ?? "");
+            });
+        }
+    });
+</script>
+
+<emoji-picker />
