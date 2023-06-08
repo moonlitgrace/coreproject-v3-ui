7c7e61dcf0b177f2430a1b12b69a9a4fd5cdeff2
Tokito <suneeth.since2005@gmail.com>
2023-06-08 21:10:32 +0530

    Feat: markdown emoji picker (#250)
    * add emojis json data
    
    * feat: add markdown emoji picker logic
    
    * refactor: rename variable
    
    * refactor: update codes
    
    * chore: add emojis locally
    
    * chore: add script for downloading emojis
    
    * Update emoji_downloader.js
    
    * Update emoji_downloader.js
    
    * fix: making popover floating with caret
    
    * fix: showing popover with no data
    
    * fix: not showing popover in new line
    
    * style: add styles for popover
    
    * format classes
    
    * Squashed commit of the following:
    
    commit 92748b7ff16ea24d57d20fa8addde4c0ca042058
    Merge: 2ba5319 a3e79e2
    Author: Tokito <suneeth.since2005@gmail.com>
    Date:   Wed Jun 7 22:15:43 2023 +0530
    
        Merge pull request #246 from tokitou-san/dependabot/npm_and_yarn/zxcvbn-ts/language-common-3.0.3
    
        chore(deps-dev): bump @zxcvbn-ts/language-common from 3.0.2 to 3.0.3
    
    commit a3e79e23e823c6dada700dd0ae71bd0eacb1ad9c
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed Jun 7 01:20:05 2023 +0000
    
        chore(deps-dev): bump @zxcvbn-ts/language-common from 3.0.2 to 3.0.3
    
        Bumps [@zxcvbn-ts/language-common](https://github.com/zxcvbn-ts/zxcvbn) from 3.0.2 to 3.0.3.
        - [Changelog](https://github.com/zxcvbn-ts/zxcvbn/blob/master/CHANGELOG.md)
        - [Commits](https://github.com/zxcvbn-ts/zxcvbn/compare/@zxcvbn-ts/language-common@3.0.2...@zxcvbn-ts/language-common@3.0.3)
    
        ---
        updated-dependencies:
        - dependency-name: "@zxcvbn-ts/language-common"
          dependency-type: direct:development
          update-type: version-update:semver-patch
        ...
    
        Signed-off-by: dependabot[bot] <support@github.com>
    
    * feat: can access emojis with keydown event
    
    * Fix the emoji image logics (#252)
    
    * add
    
    * Update emoji_downloader.js
    
    * add
    
    * Update text_editor.svelte
    
    * fix: emoji_matches updating
    
    * small type change lol
    
    * Squashed commit of the following:
    
    commit 67d28b36ea5a5d2398736d0ce2dce58909ce1c83
    Merge: 92748b7 078230c
    Author: Tokito <suneeth.since2005@gmail.com>
    Date:   Thu Jun 8 08:29:27 2023 +0530
    
        Merge pull request #251 from tokitou-san/dependabot/npm_and_yarn/sass-1.63.0
    
        chore(deps-dev): bump sass from 1.62.1 to 1.63.0
    
    commit 078230c1ee59da3cc47f274514e4aff8d9a02dd8
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Thu Jun 8 00:08:19 2023 +0000
    
        chore(deps-dev): bump sass from 1.62.1 to 1.63.0
    
        Bumps [sass](https://github.com/sass/dart-sass) from 1.62.1 to 1.63.0.
        - [Release notes](https://github.com/sass/dart-sass/releases)
        - [Changelog](https://github.com/sass/dart-sass/blob/main/CHANGELOG.md)
        - [Commits](https://github.com/sass/dart-sass/compare/1.62.1...1.63.0)
    
        ---
        updated-dependencies:
        - dependency-name: sass
          dependency-type: direct:development
          update-type: version-update:semver-minor
        ...
    
        Signed-off-by: dependabot[bot] <support@github.com>
    
    * Fix Some logics
    
    * fix: textarea caret position change on up down arrow selection
    
    * feat: add image loader for emojis and sorted
    
    * add: get selected emoji info
    
    * fix: replace emoji_code with emoji_name on select
    
    * fix: emoji loader
    
    * remove dead codes
    
    * refactor: update `tick()` function
    
    ---------
    
    Co-authored-by: baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>

---
+++ scripts/emoji_downloader.js
@@ -0,0 +1,53 @@
+import fs from "fs";
+import fetch from "node-fetch-retry";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Polyfill. Remove it later
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const emoji_data_path = path.join(__dirname, "..", "src", "lib", "data", "emojis.json");
+const emoji_input_path = path.join(__dirname, "emojis.json");
+const download_path = path.join(__dirname, "..", "static", "emojis");
+
+// Check if folder exists
+if (!fs.existsSync(download_path)) {
+    fs.mkdirSync(download_path);
+}
+
+fs.readFile(emoji_input_path, "utf8", async (err, data) => {
+    if (err) {
+        console.error("Error reading the file:", err);
+        return;
+    }
+    const emojis_data = JSON.parse(data);
+
+    const emoji_map = {};
+
+    for (const [emoji_name, emoji_link] of Object.entries(emojis_data)) {
+        const image_filename = `${download_path}/${emoji_name}.png`;
+
+        try {
+            const response = await fetch(emoji_link, { method: "GET", retry: 10, pause: 3000 });
+            if (!response.ok) {
+                throw new Error("Error while downloading image. HTTP status " + response.status);
+            }
+            const arrayBuffer = await response.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
+            fs.writeFileSync(image_filename, buffer);
+            console.log(`Downloaded: ${emoji_name}`);
+        } catch (error) {
+            console.error("Error while downloading image:", error);
+            return;
+        } finally {
+            // svelte specific code
+            emoji_map[emoji_name] = `/emojis/${emoji_name}.png`;
+        }
+    }
+
+    fs.writeFile(emoji_data_path, JSON.stringify(emoji_map), function (err) {
+        if (err) throw err;
+        console.log("wrote data");
+    });
+});
---
+++ src/lib/components/shared/text_editor.svelte
@@ -0,0 +1,161 @@
+<script lang="ts">
+    import ImageLoader from "$components/shared/image/image_loader.svelte";
+    import { emojis } from "$data/emojis";
+    import { offset } from "caret-pos";
+    import { afterUpdate, tick } from "svelte";
+
+    let textarea_element: HTMLTextAreaElement;
+    let textarea_value: string;
+
+    let emoji_matches: [{ emoji?: string; keyword?: string }?];
+    let show_emoji_picker = false;
+    let caret_offset: { top: number; left: number; height: number } | null = null;
+    let active_emoji_index: number;
+    const SHOW_EMOJI_LIMIT = 5;
+
+    const input_handler = (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        const input_text = target.value;
+        let last_typed_word: string | undefined;
+
+        // to get last typed word even its in middle
+        const selection_start = target.selectionStart;
+        if (typeof selection_start !== "number") return;
+
+        const words_before_caret = input_text.substring(0, selection_start);
+        const words_list = words_before_caret.split(/[\s\n]/);
+        last_typed_word = words_list.at(-1);
+
+        // check if last_typed_word starts with ":" that may or may not have subsequent word characters
+        const emoji_code = last_typed_word?.match(/^:(\S*)$/);
+        if (emoji_code) {
+            // Set first item active
+            active_emoji_index = 0;
+
+            show_emoji_picker = true;
+            emoji_matches = [];
+
+            for (const item of Object.entries(emojis).sort()) {
+                const keyword = item[0];
+                const emoji = item[1];
+                if (keyword.includes(emoji_code[1])) {
+                    emoji_matches.push({
+                        emoji: emoji,
+                        keyword: keyword
+                    });
+                }
+            }
+
+            // Popover settings
+            if (caret_offset === null) {
+                const textarea_position = textarea_element.getBoundingClientRect();
+                const scroll_top = textarea_element.scrollTop;
+
+                const caret_position = offset(textarea_element);
+                const caret_offset_top = caret_position.top - textarea_position.top;
+                const caret_offset_left = caret_position.left - textarea_position.left;
+                const caret_offset_height = caret_position.height + 5; // Add extra height
+
+                caret_offset = {
+                    top: caret_offset_top - scroll_top,
+                    left: caret_offset_left,
+                    height: caret_offset_height
+                };
+            }
+        } else {
+            emoji_matches = [];
+            caret_offset = null;
+            show_emoji_picker = false;
+        }
+    };
+
+    const handle_keydown = (event: KeyboardEvent) => {
+        if (!show_emoji_picker) return;
+
+        if (event.key === "ArrowUp") {
+            event.preventDefault();
+            active_emoji_index = (active_emoji_index - 1 + SHOW_EMOJI_LIMIT) % SHOW_EMOJI_LIMIT;
+        } else if (event.key === "ArrowDown") {
+            event.preventDefault();
+            active_emoji_index = (active_emoji_index + 1) % SHOW_EMOJI_LIMIT;
+        } else if (event.key === "Enter") {
+            event.preventDefault();
+            select_emoji(active_emoji_index);
+        }
+    };
+
+    const select_emoji = (emoji_index: number) => {
+        const emoji_keyword = emoji_matches[emoji_index]?.keyword;
+        const emoji_code = `:${emoji_keyword}:`;
+
+        const selection_start = textarea_element.selectionStart;
+        const selection_end = textarea_element.selectionEnd;
+
+        const text_before_selection = textarea_value.substring(0, selection_start);
+        const text_after_selection = textarea_value.substring(selection_end);
+
+        // replace last word before text selection with emoji code
+        const updated_text_before_selection = text_before_selection.replace(/\S+$/, emoji_code);
+        textarea_value = `${updated_text_before_selection} ${text_after_selection}`;
+
+        // set caret at the end of inserted emoji_code
+        tick().then(() => {
+            const caret_position = updated_text_before_selection.length + 1;
+            textarea_element.setSelectionRange(caret_position, caret_position);
+        });
+
+        // close emoji picker
+        show_emoji_picker = false;
+        caret_offset = null;
+        emoji_matches = [];
+    };
+
+    // close popover on "blur"
+    afterUpdate(() =>
+        textarea_element.addEventListener("blur", () => {
+            emoji_matches = [];
+            caret_offset = null;
+            show_emoji_picker = false;
+        })
+    );
+</script>
+
+<div class="relative">
+    <textarea
+        on:input={input_handler}
+        on:keydown={handle_keydown}
+        bind:this={textarea_element}
+        bind:value={textarea_value}
+        class="h-[8vw] w-full rounded-[0.75vw] border-none bg-surface-900 p-[1vw] text-[1vw] leading-[1.5vw] text-surface-50 outline-none ring-2 ring-white/25 duration-300 ease-in-out placeholder:text-surface-200 focus:ring-2 focus:ring-white/50"
+        placeholder="Leave a comment"
+    />
+    {#if show_emoji_picker && caret_offset && emoji_matches.length > 0}
+        <emoji-popover
+            class="emoji_picker absolute flex flex-col divide-y divide-surface-50/10 overflow-hidden rounded-[0.5vw] bg-surface-400 text-[1vw] text-surface-50"
+            style="top: {caret_offset?.top + caret_offset?.height}px; left: {caret_offset?.left}px; min-width: 12vw;"
+        >
+            {#each emoji_matches as item, index}
+                {#if index < SHOW_EMOJI_LIMIT}
+                    {@const emoji = item?.["emoji"] ?? ""}
+                    {@const keyword = item?.["keyword"] ?? ""}
+
+                    <div
+                        class="flex cursor-pointer items-center gap-[0.5vw] px-[0.75vw] py-[0.25vw] leading-[1.75vw] hover:bg-primary-500 hover:text-white"
+                        class:bg-primary-500={active_emoji_index === index}
+                        class:text-white={active_emoji_index === index}
+                        on:mousedown={() => select_emoji(index)}
+                    >
+                        <div class="h-[0.9vw] w-[0.9vw]">
+                            <ImageLoader
+                                src={emoji}
+                                alt={keyword}
+                                class="h-full w-full"
+                            />
+                        </div>
+                        <span>{keyword}</span>
+                    </div>
+                {/if}
+            {/each}
+        </emoji-popover>
+    {/if}
+</div>
