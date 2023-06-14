9d99b54a64f0e7f5b043c766abba657a1721ddf0
sssuneeth <suneeth.since2005@gmail.com>
2023-06-14 16:46:16 +0530

    style ( responsive ) : texteditor

--- src/lib/components/shared/text_editor.svelte
+++ src/lib/components/shared/text_editor.svelte
@@ -392,14 +392,14 @@
             bind:this={textarea_element}
             bind:value={textarea_value}
             spellcheck="true"
-            class="h-32 w-full resize-none border-none bg-surface-900 p-3 text-sm leading-tight md:leading-[1.5vw] text-surface-50 outline-none duration-300 ease-in-out placeholder:text-surface-200 focus:ring-0 md:h-[8vw] md:p-[1vw] md:text-[1vw]"
+            class="h-32 w-full resize-none border-none bg-surface-900 p-3 text-sm leading-tight text-surface-50 outline-none duration-300 ease-in-out placeholder:text-surface-200 focus:ring-0 md:h-[8vw] md:p-[1vw] md:text-[1vw] md:leading-[1.5vw]"
             placeholder="Leave a comment"
         />
     {:else if tab_type === "preview"}
-        <div class="h-[8.22rem] md:min-h-[8.25vw] p-3 md:p-[1vw]">
+        <div class="h-[8.22rem] p-3 md:h-full md:min-h-[8.25vw] md:p-[1vw]">
             <Markdown
                 markdown={textarea_value}
-                class="w-full border-none bg-surface-900 text-sm md:text-[1vw] leading-tight md:leading-[1.5vw] text-surface-50 outline-none"
+                class="w-full border-none bg-surface-900 text-sm leading-tight text-surface-50 outline-none md:text-[1vw] md:leading-[1.5vw]"
             />
         </div>
     {/if}
