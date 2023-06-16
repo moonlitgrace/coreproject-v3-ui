e3ed22b9aa9590e9a8ca5dcb3401862b38288330
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-06-16 10:14:25 +0600

    fix: tests

--- src/lib/functions/opengraph.test.ts
+++ src/lib/functions/opengraph.test.ts
@@ -14,5 +14,7 @@ test("opengraph function", () => {
         // eslint-disable-next-line no-useless-escape
         .replace(/(?<=\>)\s+(?=\<)/g, "");
 
-    expect(opengraph_html).toBe(`<meta property="og:type" content="website"><meta property="og:site_name" content="CoreProject"><meta property="og:locale" content="en_US"><meta property="og:title" content="Hello"><meta property="og:url" content="https://example.com"><meta property="og:description" content="random">`);
+    expect(opengraph_html).toBe(
+        `<meta property="og:type" content="website"><meta property="og:site_name" content="CoreProject"><meta property="og:locale" content="en_US"><title>Hello</title><meta property="og:title" content="Hello"><meta name="twitter:title" content="Hello" /><meta property="og:url" content="https://example.com"><meta property="og:description" content="random"><meta name="twitter:description" content="random" /><meta name="twitter:card" content="summary_large_image" />`
+    );
 });
