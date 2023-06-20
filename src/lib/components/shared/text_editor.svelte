97b7fdc1a2ebdb3f33fa9e8628e3b7ab589ba598
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-06-20 14:15:20 +0600

    Update text_editor.svelte

--- src/lib/components/shared/text_editor.svelte
+++ src/lib/components/shared/text_editor.svelte
@@ -425,7 +425,7 @@
                     type="button"
                     aria-label={item_label}
                     use:tippy={{
-                        content: `<div class='bg-surface-400/75 px-3 py-1 text-surface-50 text-sm leading-2 rounded-lg'>${description}</div>`,
+                        content: `<div class='bg-surface-400 px-3 py-1 text-surface-50 text-sm leading-2 rounded-lg'>${description}</div>`,
                         allowHTML: true,
                         arrow: false,
                         offset: [0, 11.5],
