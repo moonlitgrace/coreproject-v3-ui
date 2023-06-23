43f90ceeb58f5e61cd26d83c67a93aa042edcaf9
sssuneeth <suneeth.since2005@gmail.com>
2023-06-24 00:05:20 +0530

    fix desktop layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -158,7 +158,7 @@
 
     <AppShell>
         <svelte:fragment slot="header">
-            <navbar class="absolute top-0 flex h-[4.5rem] w-full items-center justify-between bg-surface-900/90 px-[3vw] backdrop-blur-3xl md:h-[10vh] md:py-[0.9375vw] md:pl-[2.1vw] md:pr-[3.75vw]">
+            <navbar class="absolute top-0 flex h-[4.5rem] w-full items-center justify-between bg-surface-900/90 px-[3vw] backdrop-blur-3xl md:static md:h-[10vh] md:py-[0.9375vw] md:pl-[2.1vw] md:pr-[3.75vw]">
                 {#if ["form", "logo"].includes($navbar_middle_section_variant)}
                     <a href="/">
                         <Logo class="w-9 md:w-[2vw]" />
