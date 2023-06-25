1a3417f4d19b2f704be8ebc1951c199eaebd7541
sssuneeth <suneeth.since2005@gmail.com>
2023-06-25 23:10:26 +0530

    restructure forum posts

--- src/lib/data/mock/forum_posts.ts
+++ src/lib/data/mock/forum_posts.ts
@@ -1,5 +1,6 @@
 export const forum_posts = [
-	{
+	{	
+		link: "/forum/",
 		title: "Celebrating 10 years of Hyouka!",
 		description: `Ousei Arima is a child prodigy known as the "Human Metronome" for playing the piano with precision and perfection. Guided by a strict mother and rigorous training, Kousei dominates every competition he enters`,
 		banner: "https://blog.sakugabooru.com/wp-content/uploads/2017/11/hk22-1038x576.jpg",
@@ -8,6 +9,7 @@ export const forum_posts = [
 		responses: 69
 	},
 	{
+		link: "/forum/",
 		title: "Can You See Hyouka as a Point-n-Click Adventure Game?",
 		description: `So, while watching the series, I can see it being adapted as a point-n-click game in which you ask people around, looking for clues and solve mysteries on the top given to you.
 					Player would be controlling Oreki in a 3D-esque environment and go around interrogate people around. You have choice of responses and see where that gets you.
