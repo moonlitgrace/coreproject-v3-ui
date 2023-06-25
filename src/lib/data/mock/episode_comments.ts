10a344e78afa494dae97e35cb441e57b02dbcbcf
sssuneeth <suneeth.since2005@gmail.com>
2023-06-25 22:24:57 +0530

    add comment section

---
+++ src/lib/data/mock/episode_comments.ts
@@ -0,0 +1,20 @@
+export const episode_comments = [
+    {
+        user: {
+            username: "Tokito",
+            profile_pic: "/images/DemonSlayer-bg.avif"
+        },
+        date: "2023-03-11T02:37:40.790Z",
+        content: `Hi **Tokito** here! `,
+        likes: 999
+    },
+    {
+        user: {
+            username: "Sora amamiya",
+            profile_pic: "/images/Avatar.avif"
+        },
+        date: "2023-01-11T02:37:40.790Z",
+        content: `This episode is damn good. **Tokito** is the best :P`,
+        likes: 99
+    },
+]
