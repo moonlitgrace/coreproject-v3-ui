06ba082166d1a9fd156d0afb0453b97a85141690
sssuneeth <suneeth.since2005@gmail.com>
2023-06-26 21:44:27 +0530

    ...

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -10,102 +10,109 @@
     import { ProgressBar } from "@skeletonlabs/skeleton";
 </script>
 
-<container class="block md:py-[2vw] md:pl-[5vw] md:pr-[3.75vw]">
-    <upload-area class="grid grid-cols-12 md:gap-[5vw] md:px-[10vw]">
-        <upload-progress class="flex items-end md:col-span-7 md:pb-[1.5vw]">
-            <div class="w-full">
+<container class="block p-5 md:py-[2vw] md:pl-[5vw] md:pr-[3.75vw]">
+    <upload-area class="grid grid-cols-12 gap-7 md:gap-[5vw] md:px-[10vw]">
+        <upload-progress class="col-span-12 mt-20 flex items-end md:col-span-7 md:pb-[1.5vw]">
+            <div class="w-full text-center md:text-left">
                 <ProgressBar
                     label="Progress Bar"
                     value={50}
                     max={100}
-                    height="md:h-[0.9vw]"
-                    rounded="md:rounded-[0.25vw]"
+                    height="h-3 md:h-[0.9vw]"
+                    rounded="rounded md:rounded-[0.25vw]"
                     track="bg-surface-400"
                     meter="bg-primary-500"
                 />
-                <progress-info class="flex flex-col leading-none md:mt-[1.5vw] md:gap-[0.5vw]">
+                <progress-info class="mt-5 flex flex-col gap-3 leading-none md:mt-[1.5vw] md:gap-[0.5vw]">
                     <span class="font-semibold md:text-[1vw]">292.8 GB</span>
                     <span class="text-surface-50 md:text-[1vw]">17 folders, 29 files</span>
                 </progress-info>
             </div>
         </upload-progress>
-        <upload-input class="md:col-span-5">
+        <upload-input class="col-span-12 md:col-span-5">
             <FileDropzone
                 name="files"
-                padding="md:p-[2vw] !bg-surface-400"
+                padding="md:p-[2vw] !bg-surface-400 h-48 md:h-full"
                 border="border-none"
-                rounded="md:rounded-[1vw]"
-                regionInterfaceText="flex flex-col place-items-center md:gap-[1vw]"
+                rounded="rounded-2xl md:rounded-[1vw]"
+                regionInterfaceText="flex flex-col place-items-center gap-2 md:gap-[1vw]"
                 slotLead="leading-none"
                 slotMessage="leading-none"
                 slotMeta="leading-none flex flex-col md:gap-[0.25vw]"
             >
                 <svelte:fragment slot="lead">
-                    <Upload class="md:w-[2vw]" />
+                    <Upload class="w-9 md:w-[2vw]" />
                 </svelte:fragment>
                 <svelte:fragment slot="message">
-                    <span class="font-semibold text-surface-50 md:text-[1.1vw]">Upload a file</span>
+                    <span class="text-base font-semibold text-surface-50 md:text-[1.1vw]">Upload a file</span>
                 </svelte:fragment>
                 <svelte:fragment slot="meta">
-                    <divider class="flex items-center md:gap-[0.5vw]">
-                        <left-border class="border-surface-300 md:w-[2vw] md:border-t-[0.1vw]" />
-                        <span class="font-semibold text-surface-300 md:text-[0.9vw]">Or</span>
-                        <right-border class="border-surface-300 md:w-[2vw] md:border-t-[0.1vw]" />
+                    <divider class="flex items-center justify-center gap-2 md:gap-[0.5vw]">
+                        <left-border class="w-5 border-t-2 border-surface-300 md:w-[2vw] md:border-t-[0.1vw]" />
+                        <span class="text-xs font-semibold text-surface-300 md:text-[0.9vw]">Or</span>
+                        <right-border class="w-5 border-t-2 border-surface-300 md:w-[2vw] md:border-t-[0.1vw]" />
                     </divider>
-                    <span class="text-surface-50 md:text-[1.1vw]">Browse</span>
+                    <span class="text-sm text-surface-50 md:text-[1.1vw]">Browse</span>
                 </svelte:fragment>
             </FileDropzone>
         </upload-input>
     </upload-area>
 
-    <hr class="!border-t-[0.2vw] !border-primary-200/25 md:mb-[1vw] md:mt-[3vw]" />
+    <hr class="!md:border-t-[0.2vw] mb-5 mt-10 !border-t-2 !border-primary-200/25 opacity-0 md:mb-[1vw] md:mt-[3vw] md:opacity-100" />
 
     <uploads>
-        <uploads-options class="flex justify-between">
-            <div class="flex items-center md:gap-[3vw]">
+        <uploads-options class="flex flex-col justify-between md:flex-row">
+            <div class="flex items-center justify-between md:justify-start md:gap-[3vw]">
                 <form class="relative flex items-center">
                     <button
-                        class="btn absolute left-[1vw] p-0"
+                        class="btn absolute left-2 p-0 md:left-[1vw]"
                         aria-label="Search"
                     >
-                        <Search style="width: 1vw; opacity: 0.75;" />
+                        <Search
+                            class="hidden md:flex"
+                            style="width: 1vw; opacity: 0.75;"
+                        />
+                        <Search
+                            class="flex md:hidden"
+                            style="width: 1rem; opacity: 0.75;"
+                        />
                     </button>
                     <input
                         type="text"
                         placeholder="Search"
-                        class="h-full w-full border-none bg-surface-400 leading-none text-white shadow-lg !ring-0 placeholder:font-medium placeholder:text-surface-50 md:rounded-[0.5vw] md:py-[0.5vw] md:pl-[3vw] md:text-[1vw]"
+                        class="h-full w-56 rounded-lg border-none bg-surface-400 pl-12 text-base leading-none text-white shadow-lg !ring-0 placeholder:font-medium placeholder:text-surface-50 md:w-full md:rounded-[0.5vw] md:py-[0.5vw] md:pl-[3vw] md:text-[1vw]"
                     />
                 </form>
-                <button class="btn flex p-0 leading-none text-surface-50 md:gap-[0.5vw] md:rounded-[0.25vw] md:text-[1vw]">
-                    <Cross class="rotate-45 md:w-[1vw]" />
+                <button class="btn flex gap-2 p-0 leading-none text-surface-50 md:gap-[0.5vw] md:rounded-[0.25vw] md:text-[1vw]">
+                    <Cross class="w-4 rotate-45 md:w-[1vw]" />
                     New folder
                 </button>
             </div>
 
-            <div class="flex md:gap-[3vw]">
-                <button class="btn flex p-0 leading-none text-surface-50 md:gap-[0.5vw] md:rounded-[0.25vw] md:text-[1vw]">
+            <div class="mt-5 flex justify-between md:mt-0 md:justify-start md:gap-[3vw]">
+                <button class="btn flex gap-3 p-0 text-base text-base font-semibold leading-none text-surface-50 md:gap-[0.5vw] md:rounded-[0.25vw] md:text-[1vw]">
                     <Edit
                         variant="without_underline_around_pencil"
-                        class="md:w-[1vw]"
+                        class="w-4 md:w-[1vw]"
                     />
                     Rename
                 </button>
-                <button class="btn flex p-0 leading-none text-surface-50 md:gap-[0.5vw] md:rounded-[0.25vw] md:text-[1vw]">
+                <button class="btn flex gap-3 p-0 text-base font-semibold leading-none text-surface-50 md:gap-[0.5vw] md:rounded-[0.25vw] md:text-[1vw]">
                     <Edit
                         variant="with_underline_around_pencil"
-                        class="md:w-[1vw]"
+                        class="w-4 md:w-[1vw]"
                     />
                     Edit Details
                 </button>
-                <button class="btn flex p-0 leading-none text-surface-50 md:gap-[0.5vw] md:rounded-[0.25vw] md:text-[1vw]">
-                    <Delete class="md:w-[1vw]" />
+                <button class="btn flex gap-3 p-0 text-base font-semibold leading-none text-surface-50 md:gap-[0.5vw] md:rounded-[0.25vw] md:text-[1vw]">
+                    <Delete class="w-4 md:w-[1vw]" />
                     Delete
                 </button>
             </div>
         </uploads-options>
 
-        <uploads-table class="block md:mt-[3vw]">
-            <table class="w-full">
+        <uploads-table class="mt-10 block md:mt-[3vw]">
+            <table class="w-full leading-none text-surface-50">
                 <thead>
                     <tr class="text-left md:text-[1vw]">
                         <th>
@@ -129,14 +136,14 @@
                             </div>
                         </th>
                         <th>
-                            <div class="flex items-center md:gap-[0.5vw]">
+                            <div class="hidden items-center md:flex md:gap-[0.5vw]">
                                 <span>Data modified</span>
                                 <button class="btn p-0"><Chevron class="md:w-[1vw]" /></button>
                                 <button class="btn p-0"><Chevron class="rotate-180 opacity-50 md:w-[1vw]" /></button>
                             </div>
                         </th>
                         <th>
-                            <div class="flex items-center md:gap-[0.5vw]">
+                            <div class="hidden items-center md:flex md:gap-[0.5vw]">
                                 <span>Data created</span>
                                 <button class="btn p-0"><Chevron class="md:w-[1vw]" /></button>
                                 <button class="btn p-0"><Chevron class="rotate-180 opacity-50 md:w-[1vw]" /></button>
@@ -154,11 +161,11 @@
                 <!-- spacing -->
                 <tbody>
                     <tr>
-                        <td class="md:h-[1vw]" />
+                        <td class="h-5 md:h-[1vw]" />
                     </tr>
                 </tbody>
                 <!-- spacing -->
-                <tbody class="">
+                <tbody class="border-separate border-spacing-10">
                     {#each Array(10) as _}
                         <tr>
                             <td class="flex items-center md:gap-[1vw]">
@@ -166,7 +173,7 @@
                                     type="checkbox"
                                     class="cursor-pointer rounded border-2 bg-transparent focus:ring-0 focus:ring-offset-0 md:h-[1.25vw] md:w-[1.25vw]"
                                 />
-                                <button class="btn p-0">
+                                <button class="btn hidden p-0 md:flex">
                                     <Star
                                         variant="empty"
                                         class="text-surface-50/50 md:w-[1.5vw]"
@@ -178,13 +185,14 @@
                                 <span class="md:text-[1vw]">Attack on Titan SO1</span>
                             </td>
                             <td>
-                                <span class="md:text-[1vw]">[DIRECTORY]</span>
+                                <span class="hidden text-[1vw] md:flex">[DIRECTORY]</span>
+                                <span class="flex text-base md:hidden">[DIR]</span>
                             </td>
-                            <td>
-                                <span class="md:text-[1vw]">2023-03-03</span>
+                            <td class="">
+                                <span class="hidden text-[1vw] md:flex">2023-03-03</span>
                             </td>
-                            <td>
-                                <span class="md:text-[1vw]">2023-03-03</span>
+                            <td class="">
+                                <span class="hidden text-[1vw] md:flex">2023-03-03</span>
                             </td>
                             <td>
                                 <span class="md:text-[1vw]">28.4 GB</span>
