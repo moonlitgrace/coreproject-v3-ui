a8fdce5d459bf97b70de192d39aca325a338ca3a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-06-26 10:15:57 +0600

    remove dolla
    Update +page.svelte

--- src/routes/[...slugs=myanimelist]/episode/[id=int]/+page.svelte
+++ src/routes/[...slugs=myanimelist]/episode/[id=int]/+page.svelte
@@ -2,8 +2,7 @@
     import { page } from "$app/stores";
     import EpisodePage from "$components/pages/episode/index.svelte";
 
-    let episode_number: number;
-    $: episode_number = Number($page.params.id);
+    let episode_number = Number($page.params.id);
 </script>
 
 <EpisodePage {episode_number} />
