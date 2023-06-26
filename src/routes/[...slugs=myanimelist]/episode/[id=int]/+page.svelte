46a39ee82e13357d22ddf11c74ab84692f516d52
sssuneeth <suneeth.since2005@gmail.com>
2023-06-26 07:12:48 +0530

    fix hoc/loc issue

--- src/routes/[...slugs=myanimelist]/episode/[id=int]/+page.svelte
+++ src/routes/[...slugs=myanimelist]/episode/[id=int]/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+    import { page } from "$app/stores";
     import EpisodePage from "$components/pages/episode/index.svelte";
+
+    let episode_number: number;
+    $: episode_number = Number($page.params.id);
 </script>
 
-<EpisodePage />
+<EpisodePage {episode_number} />
