446d2ef597e43a72a1e71ada5e08f37907646a78
sssuneeth <suneeth.since2005@gmail.com>
2023-07-02 19:23:05 +0530

    fix: error page button icon size

--- src/lib/components/pages/anime_info/error.svelte
+++ src/lib/components/pages/anime_info/error.svelte
@@ -37,10 +37,10 @@
         </context>
         <a
             href="/"
-            class="btn mt-5 w-max bg-primary-500 py-4 font-semibold leading-none md:mt-0 md:gap-[0.5vw] md:py-[1vw] md:text-[1.1vw]"
+            class="btn mt-5 w-max gap-2 bg-primary-500 py-4 font-semibold leading-none md:mt-0 md:gap-[0.5vw] md:py-[1vw] md:text-[1.1vw]"
         >
             Explore animes
-            <Chevron class="-rotate-90 md:w-[1.1vw]" />
+            <Chevron class="w-5 -rotate-90 md:w-[1.1vw]" />
         </a>
     </error-context>
     <character class="col-span-12 flex justify-center md:col-span-2">
