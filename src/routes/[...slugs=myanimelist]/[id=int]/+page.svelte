37d551d2fd745bfa50b686cdda7b6b0fbaefe0f5
Tokito <suneeth.since2005@gmail.com>
2023-07-02 06:44:47 +0530

    feat: add info no entry page design (#376)
    * feat: add info no entry page design
    
    * add link
    
    * fix text shifting
    
    * fix

--- src/routes/[...slugs=myanimelist]/[id=int]/+page.svelte
+++ src/routes/[...slugs=myanimelist]/[id=int]/+page.svelte
@@ -4,6 +4,8 @@
     import { anime_episodes } from "$data/mock/anime_episodes";
     import { anime_list } from "$data/mock/anime_list";
     import { OpengraphGenerator } from "$functions/opengraph";
+    import Chevron from "$icons/chevron.svelte";
+    import Ghost from "$icons/ghost.svelte";
     import TopRounded from "$icons/top_rounded.svelte";
 
     let anime_id = Number($page.params.id);
@@ -36,4 +38,44 @@
         anime_cover={anime.cover}
         {anime_episodes}
     />
+{:else}
+    <container class="relative flex h-[80vh] w-full flex-col items-center justify-center md:h-[90vh] md:gap-[1vw]">
+        <radial-gradient
+            class="pointer-events-none absolute inset-0 hidden h-[40vw] w-[40vw] translate-x-[27vw] md:flex"
+            style="
+                background-image: radial-gradient(circle at center, rgba(255, 255, 255, 0.05) 0%, transparent 75%);
+            "
+        />
+        <four-zero-four class="flex items-center gap-5 text-[5rem] font-bold md:gap-[2vw] md:text-[7vw]">
+            <span>4</span>
+            <Ghost class="w-20 animate-bounce md:w-[7vw]" />
+            <span>4</span>
+        </four-zero-four>
+
+        <context class="flex max-w-[90vw] flex-col items-center gap-5 md:max-w-[60vw] md:gap-[1.5vw] md:leading-[1.5vw]">
+            <status-code class="text-xl font-semibold text-primary-400 md:text-[1.5vw]">404 - Page not found!</status-code>
+            <status class="text-center text-sm font-semibold text-surface-50 md:text-[1.1vw] md:leading-[1.5vw]">
+                Uh-oh, looks like our
+                {#each "kokoro-chan".split("") as letter}
+                    <span class="inline-grid odd:text-warning-400 even:text-white">
+                        {letter}
+                    </span>
+                {/each}
+                is worked really hard for the past few days and now has fallen asleep. You can wait for her to wake up by looking at the status page, or come say hi to other fellow
+                {#each "kokoro-chan".split("") as letter}
+                    <span class="inline-grid odd:text-warning-400 even:text-white">
+                        {letter}
+                    </span>
+                {/each}
+                worksippers! ah- also let's wish her sweat dreams!
+            </status>
+            <a
+                href="/"
+                class="btn w-max gap-2 rounded-xl bg-primary-500 px-7 py-4 text-base font-semibold leading-none md:gap-[0.5vw] md:rounded-[0.75vw] md:px-[2vw] md:py-[1vw] md:text-[1.1vw]"
+            >
+                Go Home
+                <Chevron class="w-4 -rotate-90 md:w-[1.25vw]" />
+            </a>
+        </context>
+    </container>
 {/if}
