898de22b0c652aeedfea1a9b78bdbd9a36a1e869
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-18 22:45:22 +0600

    Update error.svelte

--- src/lib/components/pages/anime_info/error.svelte
+++ src/lib/components/pages/anime_info/error.svelte
@@ -9,6 +9,7 @@
         image: { src: string; alt: string; class?: string };
         class?: string;
         gradient: { mobile: string; desktop: string; class?: string };
+        position: Array<"left" | "right">;
     }> = [
         {
             image: {
@@ -19,7 +20,8 @@
                 class: "h-[50dvh] w-[100dvw] md:h-[40dvw] md:w-[calc(100%*2)]",
                 mobile: "radial-gradient(50dvh circle at center, rgba(218, 202, 207, 0.25) 0%, transparent 50%)",
                 desktop: "radial-gradient(40dvw circle at center, rgba(218, 202, 207, 0.25) 0%, transparent 50%)"
-            }
+            },
+            position: ["left", "right"]
         },
         {
             image: {
@@ -32,7 +34,8 @@
                 class: "h-[50dvh] w-[100dvw] md:h-[50dvw] md:w-[calc(100%*2)]",
                 mobile: "radial-gradient(45dvh circle at center, rgba(137, 155, 206, 0.25) 0%, transparent 50%)",
                 desktop: "radial-gradient(45dvw circle at center, rgba(137, 155, 206, 0.25) 0%, transparent 50%)"
-            }
+            },
+            position: ["right"]
         },
         {
             image: {
@@ -43,7 +46,8 @@
                 class: "h-[50dvh] w-[100dvw] md:h-[50dvw] md:w-[calc(100%*2)]",
                 mobile: "radial-gradient(50dvh circle at center, rgba(181, 124, 82, 0.25) 0%, transparent 50%)",
                 desktop: "radial-gradient(45dvw circle at center, rgba(181, 124, 82, 0.25) 0%, transparent 50%)"
-            }
+            },
+            position: ["left", "right"]
         },
         {
             image: {
@@ -54,7 +58,8 @@
                 class: "h-[50dvh] w-[100dvw] md:h-[50dvw] md:w-[calc(100%*2)]",
                 mobile: "radial-gradient(50dvh circle at center, rgba(243, 243, 243, 0.25) 0%, transparent 50%)",
                 desktop: "radial-gradient(45dvw circle at center, rgba(243, 243, 243, 0.25) 0%, transparent 50%)"
-            }
+            },
+            position: ["left", "right"]
         }
     ];
     let mapping:
