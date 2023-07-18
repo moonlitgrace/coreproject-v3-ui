358bf26a6953d47fe1617f4e9a6fa05b04a8dcf0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-18 15:50:15 +0600

    replace jsdom with `xss`

--- src/lib/functions/sanitize.test.ts
+++ src/lib/functions/sanitize.test.ts
@@ -3,8 +3,8 @@ import { expect, test } from "vitest";
 
 test("sanitize function", () => {
     const html = `<h1>hello world <script>console.log('sora amamiya is the best')</script></h1>`;
-    expect(sanitize(html)).toBe(`<h1>hello world </h1>`);
+    expect(sanitize(html)).toBe(`<h1>hello world &lt;script&gt;console.log('sora amamiya is the best')&lt;/script&gt;</h1>`);
 
     const image_src = `<img src='#' onerror=alert(1) />`;
-    expect(sanitize(image_src)).toBe(`<img src="#">`);
+    expect(sanitize(image_src)).toBe(`<img src="#" />`);
 });
