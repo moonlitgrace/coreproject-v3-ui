403f1e16759d4654d526d7ecccefd79f2b2f53f4
tokitou-san <suneeth.since2005@gmail.com>
2023-07-19 01:53:12 +0530

    add mapping to data dir.

--- src/routes/upload/login.svelte
+++ src/routes/upload/login.svelte
@@ -1,61 +1,10 @@
 <script lang="ts">
+    import { anime_girls_mapping } from "$data/characters/anime_girls_mapping";
     import Chevron from "$icons/chevron.svelte";
     import { sample } from "lodash";
     import { onMount } from "svelte";
     import { blur } from "svelte/transition";
 
-    const items: Array<{
-        image: { src: string; alt: string; class?: string };
-        class?: string;
-        gradient: { mobile: string; desktop: string; class?: string };
-    }> = [
-        {
-            image: {
-                src: "/images/characters/eliane/eliane.png",
-                alt: "Elaine"
-            },
-            gradient: {
-                class: "h-[50dvh] w-[100dvw] md:h-[40dvw] md:w-[calc(100%*2)]",
-                mobile: "radial-gradient(50dvh circle at center, rgba(218, 202, 207, 0.25) 0%, transparent 50%)",
-                desktop: "radial-gradient(40dvw circle at center, rgba(218, 202, 207, 0.25) 0%, transparent 50%)"
-            }
-        },
-        {
-            image: {
-                src: "/images/characters/ichigo/ichigo.png",
-                alt: "Ichigo",
-                class: "ml-auto"
-            },
-            class: "items-end",
-            gradient: {
-                class: "h-[50dvh] w-[100dvw] md:h-[40dvw] md:w-[calc(100%*2)]",
-                mobile: "radial-gradient(45dvh circle at center, rgba(137, 155, 206, 0.25) 0%, transparent 50%)",
-                desktop: "radial-gradient(45dvw circle at center, rgba(137, 155, 206, 0.25) 0%, transparent 50%)"
-            }
-        },
-        {
-            image: {
-                src: "/images/characters/sasha/sasha.png",
-                alt: "Sasha"
-            },
-            gradient: {
-                class: "h-[50dvh] w-[100dvw] md:h-[40dvw] md:w-[calc(100%*2)]",
-                mobile: "radial-gradient(50dvh circle at center, rgba(181, 124, 82, 0.25) 0%, transparent 50%)",
-                desktop: "radial-gradient(45dvw circle at center, rgba(181, 124, 82, 0.25) 0%, transparent 50%)"
-            }
-        },
-        {
-            image: {
-                src: "/images/characters/laura/laura.png",
-                alt: "Laura"
-            },
-            gradient: {
-                class: "h-[50dvh] w-[100dvw] md:h-[40dvw] md:w-[calc(100%*2)]",
-                mobile: "radial-gradient(50dvh circle at center, rgba(243, 243, 243, 0.25) 0%, transparent 50%)",
-                desktop: "radial-gradient(45dvw circle at center, rgba(243, 243, 243, 0.25) 0%, transparent 50%)"
-            }
-        }
-    ];
     let mapping:
         | {
               image: { src: string; alt: string; class?: string };
@@ -66,7 +15,7 @@
 
     // to prevent double mounting
     onMount(() => {
-        mapping = sample(items);
+        mapping = sample(anime_girls_mapping);
     });
 </script>
 
