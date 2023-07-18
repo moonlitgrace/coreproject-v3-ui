0162d5479bdfd5a92ee4421e2d0d2f35cf44d169
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-18 18:13:41 +0600

    better logics

--- src/lib/components/shared/text_editor.svelte
+++ src/lib/components/shared/text_editor.svelte
@@ -2,7 +2,7 @@
     import ImageLoader from "$components/shared/image/image_loader.svelte";
     import { emojis } from "$data/emojis";
     import { is_valid_url } from "$functions/is_valid_url";
-    import { sanitize } from "$functions/sanitize";
+    import { encode } from "html-entities";
     import Bold from "$icons/bold.svelte";
     import Code from "$icons/code.svelte";
     import Hyperlink from "$icons/hyperlink.svelte";
@@ -403,7 +403,7 @@
                     type="button"
                     aria-label={item_label}
                     use:tippy={{
-                        content: `<div class='leading-2 w-max whitespace-nowrap rounded-lg bg-surface-400 px-2 py-1 text-[0.65rem] text-surface-50 md:px-[0.75vw] md:py-[0.3vw] md:text-[1vw]'>${sanitize(description)}</div>`,
+                        content: `<div class='leading-2 w-max whitespace-nowrap rounded-lg bg-surface-400 px-2 py-1 text-[0.65rem] text-surface-50 md:px-[0.75vw] md:py-[0.3vw] md:text-[1vw]'>${encode(description)}</div>`,
                         allowHTML: true,
                         arrow: false,
                         offset: [0, 17],
