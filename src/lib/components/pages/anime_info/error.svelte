519f8e73d3b6aa72af9c05248c4668780330c8bd
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-19 05:18:21 +0600

    Revert "logic bomb"
    This reverts commit e773a07a341bbddc70a7625ab86c6e7444026e28.

--- src/lib/components/pages/anime_info/error.svelte
+++ src/lib/components/pages/anime_info/error.svelte
@@ -92,12 +92,10 @@
 
 {#if mapping}
     {@const image_left_or_right = mapping.position.length === 1 ? mapping.position[0] : mapping.position.filter((item) => item != sample(mapping?.position))[0]}
-    {@const left = image_left_or_right === "left"}
-    {@const right = image_left_or_right === "right"}
 
     <section
         transition:blur
-        class="{mapping.class} {right ?? 'md:flex-row'} {left ?? 'md:flex-row-reverse'} relative flex h-full flex-col justify-end gap-20 md:items-end md:gap-0"
+        class="{mapping.class} {image_left_or_right === 'left' ? 'md:flex-row-reverse' : 'md:flex-row'} relative flex h-full flex-col justify-end gap-20 md:items-end md:gap-0"
     >
         <error-context class="flex flex-col items-center leading-none md:mb-[13vw] md:w-[70dvw] md:items-start md:gap-[1vw] md:pl-[5vw]">
             <status-code class="text-7xl font-bold md:text-[7vw]">
@@ -125,7 +123,7 @@
             class="relative flex items-end justify-center md:w-[45dvw]"
             style="--mobile-gradient:{mapping.gradient.mobile}; --desktop-gradient:{mapping.gradient.desktop}"
         >
-            <gradient class="{mapping.gradient.class} {left ?? 'md:ml-[8vw]'} absolute [background:var(--mobile-gradient)] md:[background:var(--desktop-gradient)]" />
+            <gradient class="{mapping.gradient.class} {image_left_or_right === 'left' && 'md:ml-[8vw]'} absolute [background:var(--mobile-gradient)] md:[background:var(--desktop-gradient)]" />
 
             <img
                 src={mapping.image.src}
