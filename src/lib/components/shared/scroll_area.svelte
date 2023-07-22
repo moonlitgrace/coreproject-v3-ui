261dfb518df357f824b297b9549d9d7a89f81940
tokitou-san <suneeth.since2005@gmail.com>
2023-07-22 13:04:05 +0530

    re add js

--- src/lib/components/shared/scroll_area.svelte
+++ src/lib/components/shared/scroll_area.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     let klass = "";
     export { klass as class };
 
     export let parentClass = "";
     export let offsetScrollbar = false;
     export let gradientMask = false;
+
+    let scroll_percent = 0;
+    let element: HTMLDivElement;
+    onMount(() => {
+        if (gradientMask) {
+            element?.addEventListener("scroll", (event) => {
+                const el = event?.currentTarget as HTMLElement;
+                scroll_percent = globalThis.Math.round((el.scrollTop / (el.scrollHeight - el.clientHeight)) * 100);
+            });
+        }
+    });
 </script>
 
-<scroll-area class="{parentClass} {offsetScrollbar ? 'pr-3 md:pr-[0.75vw]' : 'pr-0'} mask-bottom block h-full w-full overflow-y-scroll overscroll-y-contain border-transparent scrollbar-thin">
+<scroll-area
+    bind:this={element}
+    class:mask-top={gradientMask && scroll_percent <= 100 && scroll_percent >= 90}
+    class:mask-middle={gradientMask && scroll_percent < 90 && scroll_percent >= 10}
+    class:mask-bottom={gradientMask && scroll_percent < 10 && scroll_percent >= 0}
+    class="{parentClass} {offsetScrollbar ? 'pr-3 md:pr-[0.75vw]' : 'pr-0'} block h-full w-full overflow-y-scroll overscroll-y-contain border-transparent scrollbar-thin"
+>
     <div class="{klass} whitespace-pre-line">
         <slot />
 
@@ -41,7 +60,14 @@
         &::-webkit-scrollbar-track {
             background: transparent !important;
         }
-
+        &.mask-top {
+            mask-image: linear-gradient(0deg, rgba(7, 5, 25, 0.95) 80%, rgba(0, 0, 0, 0) 100%);
+            mask-position: top;
+        }
+        &.mask-middle {
+            mask-image: linear-gradient(0deg, rgba(0, 0, 0, 0.35) 10%, rgba(7, 5, 25, 0.95) 20%, rgba(7, 5, 25, 0.95) 80%, rgba(7, 5, 25, 0.35) 90%);
+            mask-position: bottom;
+        }
         &.mask-bottom {
             mask-image: linear-gradient(180deg, rgba(7, 5, 25, 0.95) 80%, rgba(0, 0, 0, 0) 100%);
             mask-position: bottom;
