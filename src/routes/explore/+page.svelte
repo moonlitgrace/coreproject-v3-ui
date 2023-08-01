6df3e7797e9b3451afb6e1a4bdbfb733fd54294c
tokitou-san <tokitousan2005@gmail.com>
2023-08-01 21:50:44 +0530

    add search form

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { OpengraphGenerator } from "$functions/opengraph";
 	import { page } from "$app/stores";
+	import Search from "$icons/search.svelte";
 
 	const opengraph_html = new OpengraphGenerator({
         title: "Explore the Anime Universe: Your Gateway to Otaku Delights!",
@@ -21,4 +22,18 @@
 		<span class="md:text-[2vw] font-bold leading-none">Anime <span class="text-warning-400">Explore</span></span>
 		<span class="leading-none md:text-[1.1vw] font-semibold text-surface-50">Unleash your inner Otaku: Explore anime wonders</span>
 	</section-headings>
+
+	<filter-options class="md:mt-[2vw] block">
+		<search class="flex flex-col md:gap-[0.5vw]">
+			<span class="md:text-[1.1vw] leading-none font-semibold text-surface-50">Search Animes</span>
+			<div class="relative flex items-center">
+				<Search class="md:w-[1.25vw] text-surface-50 absolute md:ml-[1vw]" />
+				<input
+					type="text"
+					placeholder="Looking for specific anime? start here..."
+					class="md:pl-[3vw] focus:ring-0 bg-surface-400 placeholder:text-surface-50 md:rounded-[0.5vw] border-none md:py-[0.8vw] md:text-[1vw] leading-none"
+				/>
+			</div>
+		</search>
+	</filter-options>
 </section>
