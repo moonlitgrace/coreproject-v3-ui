64278a632023be1aae85205489ac01f5708c1fe3
tokitou-san <tokitousan2005@gmail.com>
2023-08-01 21:15:35 +0530

    add og

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
-	
+	import { OpengraphGenerator } from "$functions/opengraph";
+	import { page } from "$app/stores";
+
+	const opengraph_html = new OpengraphGenerator({
+        title: "Explore the Anime Universe: Your Gateway to Otaku Delights!",
+        site_name: "CoreProject",
+        image_url: "", // Use Opengraph later
+        url: $page.url.href,
+        locale: "en_US",
+        description: "The most modern anime streaming site"
+    }).generate_opengraph();
 </script>
+
+<svelte:head>
+    {@html opengraph_html}
+</svelte:head>
 
 <section>
 	<section-headings class="flex flex-col md:gap-[0.5vw]">
