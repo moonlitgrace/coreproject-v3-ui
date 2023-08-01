f7610d9cabe4d9b69743e3f40959153eb6ad088d
tokitou-san <tokitousan2005@gmail.com>
2023-08-02 05:29:39 +0530

    add scroll functionality

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -5,8 +5,20 @@
 	import Chevron from "$icons/chevron.svelte";
 	import Preference from "$icons/preference.svelte";
 	import Circle from "$icons/circle.svelte";
-	import { trending_animes } from "$data/mock/trending.ts";
+	import { trending_animes } from "$data/mock/trending";
 	import ImageLoader from "$components/shared/image/image_loader.svelte";
+
+	/* Anime cards scroll */
+	// no:of items to show on each scroll
+	const NO_SHOW_NEW_ITEMS = 2;
+	let trending_animes_scroll_element: HTMLElement;
+
+	function handle_scroll_right(element: HTMLElement) {
+		element.scrollLeft += NO_SHOW_NEW_ITEMS * 200;
+	}
+	function handle_scroll_left(element: HTMLElement) {
+		element.scrollLeft -= NO_SHOW_NEW_ITEMS * 200;
+	}
 
 	const opengraph_html = new OpengraphGenerator({
         title: "Explore the Anime Universe: Your Gateway to Otaku Delights!",
@@ -22,7 +34,7 @@
     {@html opengraph_html}
 </svelte:head>
 
-<section class="md:pr-[3.75vw]">
+<section class="md:pr-[3.75vw] md:pl-[1.5vw]">
 	<section-headings class="flex flex-col md:gap-[0.5vw]">
 		<span class="md:text-[2vw] font-bold leading-none">Anime <span class="text-warning-400">Explore</span></span>
 		<span class="leading-none md:text-[1.1vw] font-semibold text-surface-50">Unleash your inner Otaku: Explore anime wonders</span>
@@ -88,9 +100,12 @@
 			</headings>
 
 			<result-animes class="md:mt-[1.25vw] block relative">
-				<div class="flex md:gap-[1.25vw] overflow-x-scroll scroll-smooth scrollbar-none snap-x">
+				<div
+					class="flex md:gap-[1.25vw] overflow-x-scroll scroll-smooth scrollbar-none snap-x"
+					bind:this={trending_animes_scroll_element}
+				>
 					{#each trending_animes as anime}
-						<anime class="leading-none md:w-[13.95vw] snap-start flex-shrink-0 flex flex-col md:gap-[0.75vw]">
+						<anime class="leading-none md:w-[13.7vw] snap-start flex-shrink-0 flex flex-col md:gap-[0.75vw]">
 							<ImageLoader src={anime.cover} class="w-full md:h-[20vw] md:rounded-[0.75vw]" />
 							<div class="flex flex-col md:gap-[0.35vw]">
 								<anime_name class="line-clamp-1 md:text-[1.1vw] font-semibold">{anime.name}</anime_name>
@@ -107,8 +122,19 @@
 				</div>
 
 				<scroll-buttons>
+					<right-scroll class="absolute -left-[1.5vw] top-[8.5vw] z-10">
+						<button
+							class="btn rounded-full md:p-[1vw] bg-surface-400"
+							on:click={() => handle_scroll_left(trending_animes_scroll_element)}
+						>
+							<Chevron class="md:w-[1.5vw] rotate-90" />
+						</button>
+					</right-scroll>
 					<right-scroll class="absolute -right-[1.5vw] top-[8.5vw] z-10">
-						<button class="btn rounded-full md:p-[1vw] bg-surface-400">
+						<button
+							class="btn rounded-full md:p-[1vw] bg-surface-400"
+							on:click={() => handle_scroll_right(trending_animes_scroll_element)}
+						>
 							<Chevron class="md:w-[1.5vw] -rotate-90" />
 						</button>
 					</right-scroll>
