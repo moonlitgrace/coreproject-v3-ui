d91be7fffba0a9039efe2e0621df335fa3226ae9
tokitou-san <tokitousan2005@gmail.com>
2023-08-01 21:55:26 +0530

    add genres input

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -2,6 +2,7 @@
 	import { OpengraphGenerator } from "$functions/opengraph";
 	import { page } from "$app/stores";
 	import Search from "$icons/search.svelte";
+	import Chevron from "$icons/chevron.svelte";
 
 	const opengraph_html = new OpengraphGenerator({
         title: "Explore the Anime Universe: Your Gateway to Otaku Delights!",
@@ -17,17 +18,17 @@
     {@html opengraph_html}
 </svelte:head>
 
-<section>
+<section class="md:pr-[3.75vw]">
 	<section-headings class="flex flex-col md:gap-[0.5vw]">
 		<span class="md:text-[2vw] font-bold leading-none">Anime <span class="text-warning-400">Explore</span></span>
 		<span class="leading-none md:text-[1.1vw] font-semibold text-surface-50">Unleash your inner Otaku: Explore anime wonders</span>
 	</section-headings>
 
-	<filter-options class="md:mt-[2vw] block">
+	<filter-options class="md:mt-[2vw] block flex justify-between">
 		<search class="flex flex-col md:gap-[0.5vw]">
 			<span class="md:text-[1.1vw] leading-none font-semibold text-surface-50">Search Animes</span>
 			<div class="relative flex items-center">
-				<Search class="md:w-[1.25vw] text-surface-50 absolute md:ml-[1vw]" />
+				<Search class="md:w-[1.25vw] text-surface-50 absolute md:ml-[1vw] pointer-events-none" />
 				<input
 					type="text"
 					placeholder="Looking for specific anime? start here..."
@@ -35,5 +36,17 @@
 				/>
 			</div>
 		</search>
+		<genres class="flex flex-col md:gap-[0.5vw]">
+			<span class="md:text-[1.1vw] leading-none font-semibold text-surface-50">Genres</span>
+			<div class="relative flex items-center">
+				<Chevron class="md:w-[1.25vw] text-surface-50 absolute right-0 md:mr-[1vw] pointer-events-none" />
+				<input
+					type="text"
+					placeholder="Any"
+					class="focus:ring-0 bg-surface-400 placeholder:text-surface-50 md:rounded-[0.5vw] border-none md:py-[0.8vw] md:text-[1vw] leading-none md:w-[12.5vw]"
+				/>
+			</div>
+		</genres>
+
 	</filter-options>
 </section>
