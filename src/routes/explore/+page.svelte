ec2e27e141a55c4f8821b39705b83f4a991b8f06
tokitou-san <tokitousan2005@gmail.com>
2023-08-01 21:10:02 +0530

    add section headings

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,1 +1,10 @@
-Explore page
+<script lang="ts">
+	
+</script>
+
+<section>
+	<section-headings class="flex flex-col md:gap-[0.5vw]">
+		<span class="md:text-[2vw] font-bold leading-none">Anime <span class="text-warning-400">Explore</span></span>
+		<span class="leading-none md:text-[1.1vw] font-semibold text-surface-50">Unleash your inner Otaku: Explore anime wonders</span>
+	</section-headings>
+</section>
