aed69216f27c92d2c738405ba1a7ea946ba7c861
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-05 15:05:49 +0600

    add

--- src/lib/functions/sanitize.ts
+++ src/lib/functions/sanitize.ts
@@ -1,26 +1,31 @@
 import xss from "xss";
+import isNil from "lodash/isNil";
 
-export function sanitize(text: string) {
-    return xss(text, {
-        whiteList: {
-            blockquote: ["class"],
-            del: ["class"],
-            p: ["class"],
-            strong: ["class"],
-            em: ["class"],
-            b: ["class"],
-            a: ["href", "class"],
-            u: ["class"],
-            i: ["class"],
-            img: ["src", "alt", "class"],
-            code: ["class"],
-            pre: ["class"],
-            span: ["class"],
-            h1: ["class"],
-            h2: ["class"],
-            h3: ["class"],
-            h4: ["class"],
-            h5: ["class"]
-        }
-    });
+export async function sanitize(_text: string | undefined | Promise<string | undefined>): Promise<string> {
+    const text = await _text;
+
+    return isNil(text)
+        ? ""
+        : xss(text, {
+              whiteList: {
+                  blockquote: ["class"],
+                  del: ["class"],
+                  p: ["class"],
+                  strong: ["class"],
+                  em: ["class"],
+                  b: ["class"],
+                  a: ["href", "class"],
+                  u: ["class"],
+                  i: ["class"],
+                  img: ["src", "alt", "class"],
+                  code: ["class"],
+                  pre: ["class"],
+                  span: ["class"],
+                  h1: ["class"],
+                  h2: ["class"],
+                  h3: ["class"],
+                  h4: ["class"],
+                  h5: ["class"]
+              }
+          });
 }
