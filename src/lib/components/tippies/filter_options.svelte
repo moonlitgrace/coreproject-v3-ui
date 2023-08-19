ad1e1fa9362b3f58f572da8dc1f7e5039514f045
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-19 12:53:26 +0600

    add

--- src/lib/components/tippies/filter_options.svelte
+++ src/lib/components/tippies/filter_options.svelte
@@ -2,41 +2,45 @@
     import ScrollArea from "$components/shared/scroll_area.svelte";
     import Tick from "$icons/tick.svelte";
     import { createEventDispatcher } from "svelte";
+    import type { Writable } from "svelte/store";
+    export let input_store: Writable<string>;
+    export let items: Record<string, string> | undefined;
+    export let selected_items: Array<[string, string]>;
 
-	export let items: Record<string, string> | undefined;
-	export let selected_items: Array<[string, string]>;
+    const dispatch = createEventDispatcher();
 
-	const dispatch = createEventDispatcher();
+    function handle_select(item: [string, string]) {
+        dispatch("select", item);
+    }
 
-	function handle_select(item: [string, string]) {
-	  	dispatch("select", item);
-	}
+    $: console.log($input_store);
 </script>
 
-<div class="w-[8.5rem] rounded-lg md:w-[11vw] bg-surface-900 md:rounded-[0.5vw] overflow-x-hidden">
-	{#if items}
-		<ScrollArea
-			class="md:p-[0.5vw] flex flex-col"
-			parentClass="md:max-h-[30vw] bg-surface-400/75"
-		>
-			{#each Object.entries(items) as item}
-				{@const key = item[0]}
-				{@const value = item[1]}
+<div class="w-[8.5rem] overflow-x-hidden rounded-lg bg-surface-900 md:w-[11vw] md:rounded-[0.5vw]">
+    {#if items}
+        <ScrollArea
+            class="flex flex-col md:p-[0.5vw]"
+            parentClass="md:max-h-[30vw] bg-surface-400/75"
+        >
+            {#each Object.entries(items) as item}
+                {@const key = item[0]}
+                {@const value = item[1]}
 
-				{@const is_selected = selected_items.some(selected_item => selected_item[0] === key)}
+                {@const is_selected = selected_items.some((selected_item) => selected_item[0] === key)}
 
-				<button
-					on:click={() => handle_select(item)}
-					class="btn leading-none flex justify-between items-center hover:bg-surface-400 md:rounded-[0.35vw] p-3 text-sm md:px-[1vw] md:py-[0.75vw] md:text-[0.9vw] text-surface-50">
-					{value}
+                <button
+                    on:click={() => handle_select(item)}
+                    class="btn flex items-center justify-between p-3 text-sm leading-none text-surface-50 hover:bg-surface-400 md:rounded-[0.35vw] md:px-[1vw] md:py-[0.75vw] md:text-[0.9vw]"
+                >
+                    {value}
 
-					{#if is_selected}
-						<tick class="rounded-full bg-primary-500 text-white p-1 md:p-[0.3vw]">
-							<Tick class="w-2 md:w-[0.5vw]" />
-						</tick>
-					{/if}
-				</button>
-			{/each}
-		</ScrollArea>
-	{/if}
+                    {#if is_selected}
+                        <tick class="rounded-full bg-primary-500 p-1 text-white md:p-[0.3vw]">
+                            <Tick class="w-2 md:w-[0.5vw]" />
+                        </tick>
+                    {/if}
+                </button>
+            {/each}
+        </ScrollArea>
+    {/if}
 </div>
