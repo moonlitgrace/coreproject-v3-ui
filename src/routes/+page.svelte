be6ad981e184b5a4fefdf61a5b8e4b39ee403296
Tokito <tokitousan2005@gmail.com>
2023-08-25 15:33:28 +0530

    fix (#630)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -553,16 +553,18 @@
                     <ImageLoader
                         src={anime.cover}
                         alt={anime.name}
-                        class="h-52 w-full rounded-lg object-cover object-center md:h-[20vw] md:rounded-[0.5vw]"
+                        class="h-60 w-full rounded-lg object-cover object-center md:h-[20vw] md:rounded-[0.5vw]"
                     />
                     <anime-info class="absolute inset-x-0 bottom-0 rounded-b-lg backdrop-blur md:rounded-b-[0.5vw]">
-                        <div class="flex flex-col bg-surface-900/90 p-[1vw] md:gap-[0.35vw]">
-                            <ScrollArea class="flex md:max-h-[1.35vw] overflow-hidden font-semibold duration-300 ease-in-out scrollbar-none hover:max-h-[10vw] hover:overflow-y-scroll md:text-[1vw] md:leading-[1.35vw]">
-                                {anime.name}
+                        <div class="flex flex-col bg-surface-900/90 p-3 gap-1 md:p-[1vw] md:gap-[0.35vw]">
+                            <ScrollArea class="flex text-sm md:max-h-[1.35vw] overflow-hidden font-semibold duration-300 ease-in-out scrollbar-none hover:max-h-[10vw] hover:overflow-y-scroll md:text-[1vw] md:leading-[1.35vw]">
+                                <span class="line-clamp-1 md:line-clamp-none">
+                                    {anime.name}
+                                </span>
                             </ScrollArea>
                             <anime_info class="flex items-center gap-2 text-xs leading-none text-surface-50 md:gap-[0.5vw] md:text-[0.8vw]">
-                                <span>Watching</span>
-                                <Circle class="w-1 opacity-75 md:w-[0.25vw]" />
+                                <span class="hidden md:flex">Watching</span>
+                                <Circle class="hidden md:flex opacity-75 md:w-[0.25vw]" />
                                 <span>{anime.current_episode}/{anime.episodes_count} eps</span>
                             </anime_info>
                         </div>
