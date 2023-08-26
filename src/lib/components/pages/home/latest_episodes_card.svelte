4b75a5ac2525ea5a6f3a950a71fa02ed01a84f35
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-26 08:43:58 +0600

    Try to work on intersection observer (#635)

--- src/lib/components/pages/home/latest_episodes_card.svelte
+++ src/lib/components/pages/home/latest_episodes_card.svelte
@@ -3,7 +3,11 @@
     import ScrollArea from "$components/shared/scroll_area.svelte";
     import { FormatDate } from "$functions/format_date";
     import Play from "$icons/play.svelte";
+    import { onMount } from "svelte";
     import { slide } from "svelte/transition";
+
+    // Boolean flag to check if slide is last element
+    export let last_slide = false;
 
     export let anime: {
         id: number;
@@ -12,16 +16,49 @@
         episode_number: number;
         release_date: string;
         synopsis: string;
-    }
+    };
+    let scroll_area_element: HTMLElement | null = null;
 
+    onMount(() => {
+        scroll_area_element = anime_episode.parentElement?.parentElement?.parentElement! as HTMLElement;
+    });
     let show_more_info = false;
 
+    /** Bindings */
+    let anime_episode: HTMLElement;
+
+    function handle_mouseenter() {
+        show_more_info = true;
+    }
+    function handle_mouseleave() {
+        show_more_info = false;
+    }
+    function handle_animationstart() {
+        if (!scroll_area_element) {
+            return;
+        }
+
+        if (!last_slide) {
+            return;
+        }
+
+        const is_visible = anime_episode.getBoundingClientRect().top >= 0 && anime_episode.getBoundingClientRect().left >= 0 && anime_episode.getBoundingClientRect().bottom <= (window.innerHeight || document.documentElement.clientHeight) && anime_episode.getBoundingClientRect().right <= (window.innerWidth || document.documentElement.clientWidth);
+        if (!is_visible) {
+            return;
+        }
+
+        setTimeout(() => {
+            scroll_area_element!.scroll({ top: scroll_area_element!.scrollHeight, behavior: "smooth" });
+        }, 110);
+    }
 </script>
+
 <anime-episode
-    on:mouseenter={() => show_more_info = true}
-    on:mouseleave={() => show_more_info = false}
+    bind:this={anime_episode}
+    on:mouseenter={handle_mouseenter}
+    on:mouseleave={handle_mouseleave}
     role="group"
-    class="group relative h-[5vw] hover:h-[16vw] duration-300 ease-in-out delay-100"
+    class="group relative h-[5vw] delay-100 duration-300 ease-in-out hover:h-[16vw]"
 >
     <ImageLoader
         src={anime.cover}
@@ -46,7 +83,7 @@
         </div>
         <a
             href="./mal/{anime.id}/episode/{anime.episode_number}"
-            class="btn btn-icon h-[2.5vw] w-[2.5vw] rounded-full bg-warning-400 group-hover:bg-white text-surface-900 transition-colors duration-300"
+            class="btn btn-icon h-[2.5vw] w-[2.5vw] rounded-full bg-warning-400 text-surface-900 transition-colors duration-300 group-hover:bg-white"
         >
             <Play class="w-[1.25vw]" />
         </a>
@@ -56,7 +93,8 @@
         <more-anime-info
             in:slide={{ duration: 300, delay: 200 }}
             out:slide={{ duration: 200 }}
-            class="absolute inset-x-0 bottom-0 p-[1.3125vw] flex flex-col gap-[0.5vw]"
+            on:animationstart={handle_animationstart}
+            class="absolute inset-x-0 bottom-0 flex flex-col gap-[0.5vw] p-[1.3125vw]"
         >
             <genres class="flex items-center md:my-[0.35vw] md:gap-[0.5vw]">
                 {#each ["Action", "Romance", "Hentai"] as genre}
@@ -65,7 +103,7 @@
                     </genre>
                 {/each}
             </genres>
-            <ScrollArea class="h-[6vw] text-[0.8vw] text-surface-50 leading-[1.15vw]">
+            <ScrollArea class="h-[6vw] text-[0.8vw] leading-[1.15vw] text-surface-50">
                 {anime.synopsis}
             </ScrollArea>
         </more-anime-info>
