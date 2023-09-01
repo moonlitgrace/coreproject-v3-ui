93835da58489d4e045472f895518d347bd453f37
tokitou-san <tokitousan2005@gmail.com>
2023-09-01 21:17:27 +0530

    format: run prettier

--- src/lib/functions/classnames.ts
+++ src/lib/functions/classnames.ts
@@ -1,5 +1,5 @@
-import clsx from 'clsx';
-import type { ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import clsx from "clsx";
+import type { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
