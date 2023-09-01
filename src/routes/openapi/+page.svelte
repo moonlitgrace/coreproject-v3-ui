c48a1c63230aaf3876c63ce5bb78f77d3abba719
tokitou-san <tokitousan2005@gmail.com>
2023-09-01 12:05:12 +0530

    Add stoplight boilerplate-admin

---
+++ src/routes/openapi/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    
+    onMount(async () => {
+        await import("@stoplight/elements/styles.min.css");
+        // @ts-ignore
+        await import("@stoplight/elements/web-components.min.js");
+    });
+</script>
+
+<elements-api
+    class="w-screen"
+    apiDescriptionUrl="/openapi/schema.yaml"
+    router="hash"
+    layout="sidebar"
+/>
