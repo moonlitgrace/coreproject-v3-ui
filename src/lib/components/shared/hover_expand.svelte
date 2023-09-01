88081f1f3bae52f13159f57bb9140e533a15b8f1
tokitou-san <tokitousan2005@gmail.com>
2023-09-01 19:12:08 +0530

    feat: add classnames function

--- src/lib/components/shared/hover_expand.svelte
+++ src/lib/components/shared/hover_expand.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+    import { cn } from "$functions/classnames";
     import ScrollArea from "./scroll_area.svelte";
-    import clsx from "clsx";
 
     let klass = "";
     export { klass as class };
     export let height = "";
 </script>
 
-<ScrollArea class="{clsx(klass, height)} overflow-hidden duration-300 ease-in-out scrollbar-none hover:overflow-y-scroll">
+<ScrollArea class="{cn( klass, height )} overflow-hidden duration-300 ease-in-out scrollbar-none hover:overflow-y-scroll">
     <span class="line-clamp-1 md:line-clamp-none">
         <slot />
     </span>
