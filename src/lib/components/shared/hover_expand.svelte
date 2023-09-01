87781d65b742487363d65194c9470a53a5ecf790
tokitou-san <tokitousan2005@gmail.com>
2023-09-01 18:54:29 +0530

    used `clsx`

--- src/lib/components/shared/hover_expand.svelte
+++ src/lib/components/shared/hover_expand.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
     import ScrollArea from "./scroll_area.svelte";
+    import clsx from "clsx";
 
     let klass = "";
     export { klass as class };
     export let height = "";
-
-    let base_class: string;
-    $: base_class = `${klass} ${height}`;
 </script>
 
-<ScrollArea class="{base_class} overflow-hidden duration-300 ease-in-out scrollbar-none hover:overflow-y-scroll">
+<ScrollArea class="{clsx(klass, height)} overflow-hidden duration-300 ease-in-out scrollbar-none hover:overflow-y-scroll">
     <span class="line-clamp-1 md:line-clamp-none">
         <slot />
     </span>
