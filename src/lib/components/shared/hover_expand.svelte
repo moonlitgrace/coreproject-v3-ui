ffaa3007a433908c912644be96498bde086d12a8
tokitou-san <tokitousan2005@gmail.com>
2023-09-01 20:42:42 +0530

    add more options

--- src/lib/components/shared/hover_expand.svelte
+++ src/lib/components/shared/hover_expand.svelte
@@ -2,13 +2,15 @@
     import { cn } from "$functions/classnames";
     import ScrollArea from "./scroll_area.svelte";
 
-    let klass = "";
+    let klass: string = "";
     export { klass as class };
-    export let height = "";
+    export let height: string = "";
+    export let duration: number = 300;
+    export let line_clamp: number = 1;
 </script>
 
-<ScrollArea class="{cn( klass, height )} overflow-hidden duration-300 ease-in-out scrollbar-none hover:overflow-y-scroll">
-    <span class="line-clamp-1 md:line-clamp-none">
+<ScrollArea class="{cn( klass, height )} overflow-hidden duration-{duration} ease-in-out scrollbar-none hover:overflow-y-scroll">
+    <span class="line-clamp-{line_clamp} md:line-clamp-none">
         <slot />
     </span>
 </ScrollArea>
