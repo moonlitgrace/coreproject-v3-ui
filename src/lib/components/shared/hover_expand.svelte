50a61b807c4ddbe8d729c8d54d3b5c8f73344f85
tokitou-san <tokitousan2005@gmail.com>
2023-09-01 17:00:56 +0530

    create component

---
+++ src/lib/components/shared/hover_expand.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import ScrollArea from "./scroll_area.svelte";
+
+    let klass = "";
+    export { klass as class };
+    export let height = "";
+    export let hover_height = "";
+
+    $: base_class = `${klass} ${height} ${hover_height}`;
+</script>
+
+<ScrollArea class="{base_class} overflow-hidden duration-300 ease-in-out scrollbar-none hover:overflow-y-scroll">
+    <span class="line-clamp-1 md:line-clamp-none">
+        <slot />
+    </span>
+</ScrollArea>
