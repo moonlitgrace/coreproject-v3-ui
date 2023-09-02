527c0b858ab32c183a0c25782c03c7c633124269
tokitou-san <tokitousan2005@gmail.com>
2023-09-02 10:22:45 +0530

    Add OG

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { OpengraphGenerator } from "$functions/opengraph";
+
+	const opengraph_html = new OpengraphGenerator({
+        title: "CoreProject - A modern anime, manga, sound streaming site",
+        site_name: "CoreProject",
+        image_url: "", // Use Opengraph later
+        url: $page.url.href,
+        locale: "en_US",
+        description: "The most modern anime, manga, sound streaming site"
+    }).generate_opengraph()
+</script>
+
+<svelte:head>
+	{@html opengraph_html}
+</svelte:head>
