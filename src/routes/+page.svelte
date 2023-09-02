42b858ca4987664295e2f0921d78b2c588e2d29c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-02 22:07:05 +0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,26 @@
     import { page } from "$app/stores";
     import { OpengraphGenerator } from "$functions/opengraph";
 
-	const opengraph_html = new OpengraphGenerator({
+    const opengraph_html = new OpengraphGenerator({
         title: "CoreProject - A modern anime, manga, sound streaming site",
         site_name: "CoreProject",
         image_url: "", // Use Opengraph later
         url: $page.url.href,
         locale: "en_US",
         description: "The most modern anime, manga, sound streaming site"
-    }).generate_opengraph()
+    }).generate_opengraph();
 </script>
 
 <svelte:head>
-	{@html opengraph_html}
+    {@html opengraph_html}
 </svelte:head>
+
+<div class="flex h-screen w-full items-center justify-center text-3xl">
+    Visit &nbsp;
+    <a
+        href="/anime"
+        class="underline"
+    >
+        AnimeCore
+    </a>
+</div>
