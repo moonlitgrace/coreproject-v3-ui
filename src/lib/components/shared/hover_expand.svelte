3ed4df19eb031f8918b9e356d48bf114e9139bd4
Tokito <tokitousan2005@gmail.com>
2023-09-03 16:01:50 +0530

    refactor: rewrite `Hoverexpand` styles in tailwind (#686)

--- src/lib/components/shared/hover_expand.svelte
+++ src/lib/components/shared/hover_expand.svelte
@@ -9,18 +9,8 @@
     export let line_clamp: number = 1;
 </script>
 
-<ScrollArea class="{cn( klass, height, duration )} overflow-hidden ease-in-out scrollbar-none hover:overflow-y-scroll">
-    <span class="line-clamp-{line_clamp} md:line-clamp-none mask-right">
+<ScrollArea class={cn(klass, height, duration, "overflow-hidden ease-in-out scrollbar-none hover:overflow-y-scroll")}>
+    <span class="line-clamp-{line_clamp} md:line-clamp-none [&:not(:hover)]:[mask-image:_linear-gradient(90deg,_rgba(7,5,25)_75%,_rgba(0,0,0,0)_100%)]">
         <slot />
     </span>
 </ScrollArea>
-
-<style lang="scss">
-    .mask-right {
-        &:not(:hover) {
-            /* if we need to change the width, we should change the 75% to higher  */
-            mask-image: linear-gradient(90deg, rgba(7, 5, 25) 75%, rgba(0, 0, 0, 0) 100%);
-            mask-position: right;
-        }
-    }
-</style>
