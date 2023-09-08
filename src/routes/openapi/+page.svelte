58a3e5de333c9504b72c9eed1cde24ed337e3a8f
Tokito <tokitousan2005@gmail.com>
2023-09-08 06:18:20 +0530

    feat: Stoplight for openAPI (#669)
    Co-authored-by: baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>

---
+++ src/routes/openapi/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { beforeUpdate } from "svelte";
+
+    beforeUpdate(async () => {
+        // @ts-ignore
+        await import("@stoplight/elements/web-components.min.js");
+        await import("@stoplight/elements/styles.min.css");
+    });
+</script>
+
+<elements-api
+    data-theme="dark"
+    class="w-screen"
+    apiDescriptionUrl="/openapi/schema.yaml"
+    router="hash"
+    layout="sidebar"
+/>
