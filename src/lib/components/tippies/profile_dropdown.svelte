1a033cdf8b12fca672951da4f2b91fb3b8d9cbd6
tokitou-san <tokitousan2005@gmail.com>
2023-09-08 10:36:42 +0530

    add vercel hover effect

--- src/lib/components/tippies/profile_dropdown.svelte
+++ src/lib/components/tippies/profile_dropdown.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
+    import VercelHover from "$components/shared/vercel_hover.svelte";
     import List from "$icons/list.svelte";
     import Moon from "$icons/moon.svelte";
     import SettingsOutline from "$icons/settings_outline.svelte";
     import User from "$icons/user.svelte";
-    import { Avatar } from "@skeletonlabs/skeleton";
     import type { SvelteComponent } from "svelte";
 
     // Icons mapping
@@ -66,29 +66,39 @@
     <hr class="divider md:mb-[0.25vw] md:mt-[0.75vw] border-surface-50/20" />
 
     <div class="mt-3 md:mt-0 md:p-[0.5vw]">
-        {#each Object.entries(dropdown_icons) as item}
-            {@const item_icon = item[1].icon}
-            {@const item_href = item[1].url}
-            {@const item_name = item[1].name}
+        <VercelHover
+            direction="vertical"
+            glider_container_class="flex flex-col relative"
+            active_element_class="rounded-[0.35vw] bg-primary-500"
+            let:handle_mouseenter
+            let:handle_mouseleave
+        >
+            {#each Object.entries(dropdown_icons) as item}
+                {@const item_icon = item[1].icon}
+                {@const item_href = item[1].url}
+                {@const item_name = item[1].name}
 
-            <a
-                href={item_href}
-                
-            >
-                <div class="flex cursor-pointer items-center gap-2 rounded-[0.35vw] p-[0.4rem] transition duration-100 md:gap-[0.75vw] md:p-[0.5vw] md:py-[0.5vw] md:text-surface-50 md:hover:bg-white/10 md:hover:text-white">
-                    <svelte:component
-                        this={item_icon.component}
-                        class="hidden opacity-75 basis-[10%] md:flex {item_icon.class}"
-                    />
-                    <svelte:component
-                        this={item_icon.component}
-                        class="flex w-5 opacity-75 basis-[12%] md:hidden"
-                    />
-                    <span class=" text-xs font-medium md:text-[0.9vw]">
-                        {item_name}
-                    </span>
-                </div>
-            </a>
-        {/each}
+                <a
+                    on:mouseenter={handle_mouseenter}
+                    on:mouseleave={handle_mouseleave}
+                    href={item_href}
+                    class="z-10"
+                >
+                    <div class="flex cursor-pointer items-center gap-2 rounded-[0.35vw] p-[0.4rem] transition-colors md:gap-[0.75vw] md:p-[0.5vw] md:py-[0.5vw] md:text-surface-50 md:hover:text-white">
+                        <svelte:component
+                            this={item_icon.component}
+                            class="hidden opacity-75 basis-[10%] md:flex {item_icon.class}"
+                        />
+                        <svelte:component
+                            this={item_icon.component}
+                            class="flex w-5 opacity-75 basis-[12%] md:hidden"
+                        />
+                        <span class=" text-xs font-medium md:text-[0.9vw]">
+                            {item_name}
+                        </span>
+                    </div>
+                </a>
+            {/each}
+        </VercelHover>
     </div>
 </div>
