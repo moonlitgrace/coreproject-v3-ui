4f8803c25b5aea215115d2801fae9d62379abb0e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-09 06:14:27 -0700

    feat: remove clsx (#724)

--- src/lib/functions/classnames.ts
+++ src/lib/functions/classnames.ts
@@ -1,5 +1,21 @@
-import clsx from "clsx";
-import type { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+
+type ClassValue = ClassValue[] | Record<string, string | number | null | boolean | undefined> | string | number | null | boolean | undefined;
+
+// Code modified from
+// https://github.com/alexnault/classix/blob/0b36b65600ded771b15cac4b027bb32e8b9d74b7/src/index.ts#L9
+function clsx(...args: ClassValue[]): string {
+    let str = "",
+        i = 0,
+        arg: ClassValue;
+
+    for (; i < args.length; ) {
+        if ((arg = args[i++]) && typeof arg === "string") {
+            str && (str += " ");
+            str += arg;
+        }
+    }
+    return str;
+}
 
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
