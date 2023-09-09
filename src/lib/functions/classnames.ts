1239799fb05a9dc70142a13ff6dd9a3516b959ab
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-09 06:30:28 -0700

    refractor: `cn` to one line (#728)
    Update classnames.test.ts

--- src/lib/functions/classnames.ts
+++ src/lib/functions/classnames.ts
@@ -3,19 +3,6 @@ import { twMerge } from "tailwind-merge";
 type ClassValue = ClassValue[] | Record<string, string | number | null | boolean | undefined> | string | number | null | boolean | undefined;
 
 // Code modified from
-// https://github.com/alexnault/classix/blob/0b36b65600ded771b15cac4b027bb32e8b9d74b7/src/index.ts#L9
-function clsx(...args: ClassValue[]): string {
-    let str = "",
-        i = 0,
-        arg: ClassValue;
-
-    for (; i < args.length; ) {
-        if ((arg = args[i++]) && typeof arg === "string") {
-            str && (str += " ");
-            str += arg;
-        }
-    }
-    return str;
-}
-
+// https://chat.openai.com/share/58feb08e-0905-4860-a309-6d75687c2e6e
+const clsx = (...args: ClassValue[]) => args.filter((arg) => arg && typeof arg === "string").join(" ");
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
