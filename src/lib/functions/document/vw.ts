615e52f7691d300a3945ece4d6e74638de14a5c6
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-12 17:47:08 +0600

    add

--- src/lib/functions/document/vw.ts
+++ src/lib/functions/document/vw.ts
@@ -1,6 +1,3 @@
-import { browser } from "$app/environment";
-
 export function vw(vw: number) {
-    
-    return browser ? (vw * globalThis.window.innerWidth) / 100 : null;
+    return (vw * globalThis.window?.innerWidth) / 100;
 }
