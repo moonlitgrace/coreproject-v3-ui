9ba46fb2383cf33a2a3c23cc45dc277788679c67
tokitou-san <tokitousan2005@gmail.com>
2023-09-13 22:01:16 +0530

    rewrite in typed.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,30 +5,23 @@
     import CoreProject from "$icons/core_project.svelte";
     import Logo from "$icons/logo.svelte";
     import { onMount } from "svelte";
-    import { fly } from "svelte/transition";
-    // @ts-ignore
-    import Typewriter from "typewriter-effect/dist/core";
+    import Typed from "typed.js";
 
     let typewriter_el: HTMLElement;
 
     onMount(() => {
-        let typewriter = new Typewriter(typewriter_el, {
+        const typewriter = new Typed(typewriter_el, {
+            strings: [
+                '<a href="/anime" class="bg-gradient-to-r from-primary-500 to-primary-200 bg-clip-text text-transparent">Anime. </a>',
+                '<a href="/manga" class="bg-gradient-to-r from-primary-400 to-error-400 bg-clip-text text-transparent">Manga. </a>',
+                '<a href="/sound" class="bg-gradient-to-r from-warning-400 to-error-300 bg-clip-text text-transparent">Sound. </a>'
+            ],
             loop: true,
-            delay: 100,
-            cursorClassName: "md:text-[5vw] bg-gradient-to-b from-primary-400 to-error-400 bg-clip-text text-transparent animate-pulse"
+            typeSpeed: 100,
+            backSpeed: 100,
+            showCursor: false,
         });
-
-        typewriter
-            .typeString('<a href="/anime" class="bg-gradient-to-r from-primary-500 to-primary-200 bg-clip-text text-transparent">Anime. </a>')
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString('<a href="/manga" class="bg-gradient-to-r from-primary-400 to-error-400 bg-clip-text text-transparent">Manga. </a>')
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString('<a href="/sound" class="bg-gradient-to-r from-warning-400 to-error-300 bg-clip-text text-transparent">Sound. </a>')
-            .pauseFor(2000)
-            .start()
-    })
+    });
 
     const opengraph_html = new OpengraphGenerator({
         title: "CoreProject - A modern anime, manga, sound streaming site",
