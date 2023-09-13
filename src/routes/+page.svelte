b54f5fb24d9da88ea84b7fc8765de857270a032b
tokitou-san <tokitousan2005@gmail.com>
2023-09-13 21:05:06 +0530

    I like this

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,40 +9,26 @@
     // @ts-ignore
     import Typewriter from "typewriter-effect/dist/core";
 
-    let top_typewriter_el: HTMLElement,
-        typewriter_el: HTMLElement;
-
-    let show_buttons: boolean = false;
+    let typewriter_el: HTMLElement;
 
     onMount(() => {
-        let top_typewriter = new Typewriter(top_typewriter_el, {
+        let typewriter = new Typewriter(typewriter_el, {
+            loop: true,
             delay: 100,
-            cursorClassName: "hidden"
+            cursorClassName: "md:text-[5vw] bg-gradient-to-b from-primary-400 to-error-400 bg-clip-text text-transparent animate-pulse"
         });
 
-        top_typewriter
-            .typeString("Unlock the World of")
+        typewriter
+            .typeString('<a href="/anime" class="bg-gradient-to-r from-primary-500 to-primary-200 bg-clip-text text-transparent">Anime. </a>')
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString('<a href="/manga" class="bg-gradient-to-r from-primary-400 to-error-400 bg-clip-text text-transparent">Manga. </a>')
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString('<a href="/sound" class="bg-gradient-to-r from-warning-400 to-error-300 bg-clip-text text-transparent">Sound. </a>')
+            .pauseFor(2000)
             .start()
-
-        setTimeout(() => {
-            let typewriter = new Typewriter(typewriter_el, {
-                loop: true,
-                delay: 100,
-                cursorClassName: "md:text-[5vw] bg-gradient-to-b from-primary-400 to-error-400 bg-clip-text text-transparent animate-pulse"
-            });
-
-            typewriter
-                .typeString('<span class="bg-gradient-to-r from-primary-500 to-primary-200 bg-clip-text text-transparent">Anime. </span>')
-                .pauseFor(2000)
-                .deleteAll()
-                .typeString('<span class="bg-gradient-to-r from-primary-400 to-error-400 bg-clip-text text-transparent">Manga. </span>')
-                .pauseFor(2000)
-                .deleteAll()
-                .typeString('<span class="bg-gradient-to-r from-warning-400 to-error-300 bg-clip-text text-transparent">Sound. </span>')
-                .pauseFor(2000)
-                .start()
-            }, 2500)
-    });
+    })
 
     const opengraph_html = new OpengraphGenerator({
         title: "CoreProject - A modern anime, manga, sound streaming site",
@@ -81,50 +67,23 @@
         <content>
             <div class="flex flex-col md:gap-[2vw] relative">
                 <div class="flex flex-col md:text-[5vw] font-bold md:leading-[4.5vw]">
-                    <!-- <span>Unlock the World of</span> -->
-                    <main-typewriter bind:this={top_typewriter_el} />
+                    <span>Unlock the World of</span>
                     <typewriter bind:this={typewriter_el} />
                 </div>
-                <div class="w-max flex flex-col md:gap-[0.25vw] leading-none">
-                    <span class="md:text-[1.5vw] font-semibold text-surface-50">Welcome to CoreProject.</span>
-                    <gradient-border class="flex w-full md:h-[0.2vw] bg-gradient-to-r from-primary-500 to-error-500" />
+                <div class="flex flex-col md:gap-[1vw]">
+                    <div class="w-max flex flex-col md:gap-[0.25vw] leading-none">
+                        <span class="md:text-[1.5vw] font-semibold text-surface-50">Welcome to CoreProject</span>
+                        <gradient-border class="flex w-full md:h-[0.2vw] bg-gradient-to-r from-primary-500 to-error-500" />
+                    </div>
+                    <cta-buttons class="flex items-center md:gap-[1vw] w-max">
+                        <button
+                            class="btn rounded-[0.4rem] bg-gradient-to-r from-primary-500 to-error-500 px-5 py-4 text-base font-semibold leading-none md:rounded-[0.5vw] md:px-[1.25vw] md:py-[1vw] md:text-[1vw] shadow-2xl shadow-primary-800"
+                        >
+                            <span>Get Started</span>
+                            <Chevron class="md:w-[1.25vw] -rotate-90" />
+                        </button>
+                    </cta-buttons>
                 </div>
-                <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-                <cta-buttons
-                    on:mousedown={() => show_buttons = !show_buttons}
-                    role="main"
-                    class="flex items-center md:gap-[1vw] w-max"
-                >
-                    <button
-                        class="btn rounded-[0.4rem] bg-gradient-to-r from-primary-500 to-error-500 px-5 py-4 text-base font-semibold leading-none md:rounded-[0.5vw] md:px-[1.25vw] md:py-[1vw] md:text-[1vw] shadow-2xl shadow-primary-800"
-                    >
-                        <span>Get Started</span>
-                        <Chevron class="md:w-[1.25vw] -rotate-90" />
-                    </button>
-                    {#if show_buttons}
-                        <button
-                            in:fly={{x: -10}}
-                            out:fly={{x: -10, delay: 400, duration: 150}}
-                            class="btn rounded-[0.4rem] bg-white/5 md:border-[0.1vw] border-surface-50/20 px-5 py-4 text-base font-semibold leading-none md:rounded-[0.5vw] md:px-[1.25vw] md:py-[1vw] md:text-[1vw] hover:bg-white/10 transition-colors"
-                        >
-                            <span>Anime</span>
-                        </button>
-                        <button
-                            in:fly={{x: -10, delay: 100}}
-                            out:fly={{x: -10, delay: 200, duration: 150}}
-                            class="btn rounded-[0.4rem] bg-white/5 md:border-[0.1vw] border-surface-50/20 px-5 py-4 text-base font-semibold leading-none md:rounded-[0.5vw] md:px-[1.25vw] md:py-[1vw] md:text-[1vw] hover:bg-white/10 transition-colors"
-                        >
-                            <span>Manga</span>
-                        </button>
-                        <button
-                            in:fly={{x: -10, delay: 400}}
-                            out:fly={{x: -10, duration: 150}}
-                            class="btn rounded-[0.4rem] bg-white/5 md:border-[0.1vw] border-surface-50/20 px-5 py-4 text-base font-semibold leading-none md:rounded-[0.5vw] md:px-[1.25vw] md:py-[1vw] md:text-[1vw] hover:bg-white/10 transition-colors"
-                        >
-                            <span>Sound</span>
-                        </button>
-                    {/if}
-                </cta-buttons>
             </div>
         </content>
         <mascot class="relative flex flex-shrink-0 md:h-[40vw] self-end">
