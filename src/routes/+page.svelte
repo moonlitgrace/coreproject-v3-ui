38f2243df58e385893d0e7fcf0d751617affab02
Tokito <tokitousan2005@gmail.com>
2023-09-13 21:38:24 +0530

    feat: add CTA buttons (#750)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,34 @@
 <script lang="ts">
     import { page } from "$app/stores";
     import { OpengraphGenerator } from "$functions/opengraph";
+    import Chevron from "$icons/chevron.svelte";
     import CoreProject from "$icons/core_project.svelte";
     import Logo from "$icons/logo.svelte";
     import { onMount } from "svelte";
+    import { fly } from "svelte/transition";
     // @ts-ignore
     import Typewriter from "typewriter-effect/dist/core";
 
-    let top_typewriter_el: HTMLElement;
     let typewriter_el: HTMLElement;
 
     onMount(() => {
-        let top_typewriter = new Typewriter(top_typewriter_el, {
+        let typewriter = new Typewriter(typewriter_el, {
+            loop: true,
             delay: 100,
-            cursorClassName: "hidden"
+            cursorClassName: "md:text-[5vw] bg-gradient-to-b from-primary-400 to-error-400 bg-clip-text text-transparent animate-pulse"
         });
 
-        top_typewriter
-            .typeString("Unlock the World of")
+        typewriter
+            .typeString('<a href="/anime" class="bg-gradient-to-r from-primary-500 to-primary-200 bg-clip-text text-transparent">Anime. </a>')
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString('<a href="/manga" class="bg-gradient-to-r from-primary-400 to-error-400 bg-clip-text text-transparent">Manga. </a>')
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString('<a href="/sound" class="bg-gradient-to-r from-warning-400 to-error-300 bg-clip-text text-transparent">Sound. </a>')
+            .pauseFor(2000)
             .start()
-
-        setTimeout(() => {
-            let typewriter = new Typewriter(typewriter_el, {
-                loop: true,
-                delay: 100,
-                cursorClassName: "md:text-[5vw] bg-gradient-to-b from-primary-400 to-error-400 bg-clip-text text-transparent animate-pulse"
-            });
-
-            typewriter
-                .typeString('<span class="bg-gradient-to-r from-primary-500 to-primary-200 bg-clip-text text-transparent">Anime. </span>')
-                .pauseFor(2000)
-                .deleteAll()
-                .typeString('<span class="bg-gradient-to-r from-primary-400 to-error-400 bg-clip-text text-transparent">Manga. </span>')
-                .pauseFor(2000)
-                .deleteAll()
-                .typeString('<span class="bg-gradient-to-r from-warning-400 to-error-300 bg-clip-text text-transparent">Sound. </span>')
-                .pauseFor(2000)
-                .start()
-            }, 2500)
-    });
+    })
 
     const opengraph_html = new OpengraphGenerator({
         title: "CoreProject - A modern anime, manga, sound streaming site",
@@ -77,13 +67,22 @@
         <content>
             <div class="flex flex-col md:gap-[2vw] relative">
                 <div class="flex flex-col md:text-[5vw] font-bold md:leading-[4.5vw]">
-                    <!-- <span>Unlock the World of</span> -->
-                    <main-typewriter bind:this={top_typewriter_el} />
+                    <span>Unlock the World of</span>
                     <typewriter bind:this={typewriter_el} />
                 </div>
-                <div class="w-max flex flex-col md:gap-[0.25vw] leading-none">
-                    <span class="md:text-[1.5vw] font-semibold text-surface-50">Welcome to CoreProject.</span>
-                    <gradient-border class="flex w-full md:h-[0.2vw] bg-gradient-to-r from-primary-500 to-error-500" />
+                <div class="flex flex-col md:gap-[1vw]">
+                    <div class="w-max flex flex-col md:gap-[0.25vw] leading-none">
+                        <span class="md:text-[1.5vw] font-semibold text-surface-50">Welcome to CoreProject</span>
+                        <gradient-border class="flex w-full md:h-[0.2vw] bg-gradient-to-r from-primary-500 to-error-500" />
+                    </div>
+                    <cta-buttons class="flex items-center md:gap-[1vw] w-max">
+                        <button
+                            class="btn rounded-[0.4rem] bg-gradient-to-r from-primary-500 to-error-500 px-5 py-4 text-base font-semibold leading-none md:rounded-[0.5vw] md:px-[1.25vw] md:py-[1vw] md:text-[1vw] shadow-2xl shadow-primary-800"
+                        >
+                            <span>Get Started</span>
+                            <Chevron class="md:w-[1.25vw] -rotate-90" />
+                        </button>
+                    </cta-buttons>
                 </div>
             </div>
         </content>
