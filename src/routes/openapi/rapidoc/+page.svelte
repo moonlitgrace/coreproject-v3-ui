2ce6d0dad375af01b4f40d4925df1e4e1e5cbcde
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-17 02:57:46 +0600

    feat: add `rapidoc` (#763)

---
+++ src/routes/openapi/rapidoc/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        // @ts-ignore
+        await import("rapidoc");
+    });
+</script>
+
+<rapi-doc
+    spec-url="/openapi/schema.yaml"
+    render-style="read"
+    style={{ height: "100vh", width: "100%" }}
+></rapi-doc>
