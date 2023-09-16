e3be3773187d1408d24ba4a66a38ca1f51f1076a
tokitou-san <tokitousan2005@gmail.com>
2023-09-16 16:11:54 +0530

    fix spacing

--- src/lib/components/pages/anime_info/error.svelte
+++ src/lib/components/pages/anime_info/error.svelte
@@ -35,7 +35,7 @@
 
     <section
         transition:blur
-        class={cn(mapping.class, "relative grid h-full justify-end gap-20 md:grid-flow-col md:gap-0")}
+        class={cn(mapping.class, "relative grid h-full justify-end md:grid-flow-col")}
     >
         <error-context
             class:order-1={on_right}
@@ -48,7 +48,7 @@
                 {/each}
             </status-code>
             <status-text class="text-base font-semibold text-primary-300 md:text-[1.25vw]">Oops! Page not found...</status-text>
-            <span class="mt-5 text-base font-semibold italic md:mt-[1vw] md:text-[1.2vw]">
+            <span class="mt-2 text-base font-semibold italic md:mt-[1vw] md:text-[1.2vw]">
                 Hi <u>{mapping.image.alt}</u>
                 here!
             </span>
@@ -57,7 +57,7 @@
             </context>
             <a
                 href="../explore"
-                class="btn mt-5 w-max gap-2 bg-primary-500 py-4 font-semibold leading-none md:mt-0 md:gap-[0.5vw] md:py-[1vw] md:text-[1.1vw]"
+                class="btn mt-3 w-max gap-2 bg-primary-500 py-3 rounded-lg font-semibold leading-none md:mt-0 md:gap-[0.5vw] md:py-[1vw] md:text-[1.1vw]"
             >
                 Explore animes
                 <Chevron class="w-5 -rotate-90 md:w-[1.1vw]" />
