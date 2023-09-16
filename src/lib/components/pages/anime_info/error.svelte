25b889c7e7f372bf3ace9cee5a175d490911dec6
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-16 13:18:18 +0600

    Update error.svelte

--- src/lib/components/pages/anime_info/error.svelte
+++ src/lib/components/pages/anime_info/error.svelte
@@ -35,11 +35,13 @@
 
     <section
         transition:blur
-        class:md:flex-row-reverse={on_left}
-        class:md:flex-row={on_right}
-        class={cn(mapping.class, "relative grid h-full justify-end gap-20 md:col-span-2 md:grid-flow-col md:gap-0")}
+        class={cn(mapping.class, "relative grid h-full grid-flow-col justify-end gap-20 md:col-span-2 md:gap-0")}
     >
-        <error-context class="row-span-5 flex flex-col items-center self-end leading-none md:mb-[13vw] md:items-start md:gap-[1vw] md:pl-[5vw]">
+        <error-context
+            class:order-1={on_right}
+            class:order-2={on_left}
+            class="row-span-5 flex flex-col items-center self-end leading-none md:mb-[13vw] md:items-start md:gap-[1vw] md:pl-[5vw]"
+        >
             <status-code class="text-7xl font-bold md:text-[7vw]">
                 {#each "404".split("") as number}
                     <span class="odd:text-warning-400">{number}</span>
@@ -62,13 +64,12 @@
             </a>
         </error-context>
         <character-image
+            class:order-1={on_left}
+            class:order-2={on_right}
             class="pointer-events-none relative flex items-end justify-center md:col-span-4"
             style="--mobile-gradient:{mapping.gradient.mobile}; --desktop-gradient:{mapping.gradient.desktop}"
         >
-            <gradient
-                class:md:ml-[8vw]={on_left}
-                class={cn(mapping.gradient.class, "absolute [background:var(--mobile-gradient)] md:[background:var(--desktop-gradient)]")}
-            />
+            <gradient class={cn(mapping.gradient.class, "absolute [background:var(--mobile-gradient)] md:[background:var(--desktop-gradient)]")} />
 
             <img
                 src={mapping.image.src}
