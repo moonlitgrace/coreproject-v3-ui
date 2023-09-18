02c8550eabfdde6e12880200d27239a1811b6ab5
tokitou-san <tokitousan2005@gmail.com>
2023-09-18 06:03:23 +0530

    revert: add basic `Home` page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,4 +17,9 @@
     {@html opengraph_html}
 </svelte:head>
 
-<div class="flex h-full w-full items-center justify-center gap-3 md:gap-[0.75vw]"></div>
+<div class="flex h-screen items-center justify-center gap-3 md:gap-[0.75vw]">
+    <a href="/anime" class="flex items-center gap-3 md:gap-[1vw]">
+        <span class="text-3xl md:text-[2vw] font-semibold text-surface-50">Explore</span>
+        <AnimeCore class="w-40 md:w-[12vw] border-b-[0.15vw] border-surface-300" />
+    </a>
+</div>
