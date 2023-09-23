f2982d122a4155a416f639d002e47ca35e08d135
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-23 08:51:54 +0600

    Create +page.svelte

---
+++ src/routes/openapi/redoc/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount } from "svelte";
+    import React from "react";
+    import { RedocStandalone } from "redoc";
+    import { createRoot } from "react-dom/client";
+
+    const e = React.createElement;
+    class Documentation extends React.Component {
+        render() {
+            return e(RedocStandalone, {
+                specUrl: "http://petstore.swagger.io/v2/swagger.json",
+                options: {
+                    nativeScrollbars: true,
+                    theme: { colors: { primary: { main: "#dd5522" } } }
+                }
+            });
+        }
+    }
+    /**
+     * @type {HTMLDivElement}
+     */
+    let container;
+    onMount(function () {
+        createRoot(container).render(e(Documentation));
+    });
+</script>
+
+<main>
+    <div bind:this={container} />
+</main>
