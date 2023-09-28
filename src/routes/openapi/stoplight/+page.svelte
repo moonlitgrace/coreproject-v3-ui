013c04bca69ce68406abbbfba02687f4014b78dd
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-28 12:12:24 +0600

    Update +page.svelte

--- src/routes/openapi/stoplight/+page.svelte
+++ src/routes/openapi/stoplight/+page.svelte
@@ -1,42 +1,32 @@
 <script lang="ts">
+    import { beforeUpdate } from "svelte";
     import { OpengraphGenerator } from "$functions/opengraph";
     import { page } from "$app/stores";
-    import "@stoplight/elements/styles.min.css";
 
-    import { beforeUpdate } from "svelte";
-    import React from "react";
-    import { API } from "@stoplight/elements";
-    import { createRoot } from "react-dom/client";
-
-    const e = React.createElement;
-    class Documentation extends React.Component {
-        render() {
-            return e(
-                "div",
-                { className: "w-screen" },
-                e(API, {
-                    apiDescriptionUrl: "/openapi/schema.yaml"
-                })
-            );
-        }
-    }
-    let container: HTMLDivElement;
-    beforeUpdate(() => {
-        createRoot(container).render(e(Documentation));
+    beforeUpdate(async () => {
+        // @ts-ignore
+        await import("@stoplight/elements/web-components.min.js");
+        await import("@stoplight/elements/styles.min.css");
     });
-</script>
 
-<svelte:head>
-    {@html new OpengraphGenerator({
+    const opengraph = new OpengraphGenerator({
         title: "Stoplight | CoreProject",
         url: $page.url.href,
         description: "Explore backend of coreproject",
         site_name: "CoreProject",
         locale: "en_US",
         image_url: ""
-    }).generate_opengraph()}
+    }).generate_opengraph();
+</script>
+
+<svelte:head>
+    {@html opengraph}
 </svelte:head>
 
-<main>
-    <div bind:this={container} />
-</main>
+<elements-api
+    data-theme="dark"
+    class="w-screen"
+    apiDescriptionUrl="/openapi/schema.yaml"
+    router="hash"
+    layout="sidebar"
+/>
