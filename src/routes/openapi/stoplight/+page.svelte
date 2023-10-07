827caeec6be480960e96e89ea1e40392609a37a3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-07 19:38:47 +0600

    feat: move logics around

--- src/routes/openapi/stoplight/+page.svelte
+++ src/routes/openapi/stoplight/+page.svelte
@@ -1,3 +1,36 @@
+<!-- <script lang="ts">
+    import { beforeUpdate } from "svelte";
+    import { OpengraphGenerator } from "$functions/opengraph";
+    import { page } from "$app/stores";
+
+    beforeUpdate(async () => {
+        // @ts-ignore
+        await import("@stoplight/elements/web-components.min.js");
+        await import("@stoplight/elements/styles.min.css");
+    });
+
+    const opengraph = new OpengraphGenerator({
+        title: "Stoplight | CoreProject",
+        url: $page.url.href,
+        description: "Explore backend of coreproject",
+        site_name: "CoreProject",
+        locale: "en_US",
+        image_url: ""
+    }).generate_opengraph();
+</script>
+
+<svelte:head>
+    {@html opengraph}
+</svelte:head>
+
+<elements-api
+    data-theme="dark"
+    class="w-screen"
+    apiDescriptionUrl="/openapi/schema.yaml"
+    router="hash"
+    layout="sidebar"
+/> -->
+
 <script lang="ts">
     import { OpengraphGenerator } from "$functions/opengraph";
     import { page } from "$app/stores";
@@ -42,36 +75,3 @@
 <main>
     <div bind:this={container} />
 </main>
-
-<!-- <script lang="ts">
-    import { beforeUpdate } from "svelte";
-    import { OpengraphGenerator } from "$functions/opengraph";
-    import { page } from "$app/stores";
-
-    beforeUpdate(async () => {
-        // @ts-ignore
-        await import("@stoplight/elements/web-components.min.js");
-        await import("@stoplight/elements/styles.min.css");
-    });
-
-    const opengraph = new OpengraphGenerator({
-        title: "Stoplight | CoreProject",
-        url: $page.url.href,
-        description: "Explore backend of coreproject",
-        site_name: "CoreProject",
-        locale: "en_US",
-        image_url: ""
-    }).generate_opengraph();
-</script>
-
-<svelte:head>
-    {@html opengraph}
-</svelte:head>
-
-<elements-api
-    data-theme="dark"
-    class="w-screen"
-    apiDescriptionUrl="/openapi/schema.yaml"
-    router="hash"
-    layout="sidebar"
-/> -->
