cec6dd236d304077370020a9426908578db55acf
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-23 02:37:54 +0600

    Update json_to_tree.ts

--- src/lib/functions/json_to_tree.ts
+++ src/lib/functions/json_to_tree.ts
@@ -1,21 +1,64 @@
-import set from "lodash/set";
-
-type JSON_Element = { [key: string]: string };
-
+type Comment = {
+    pk: number;
+    user: {
+        username: string;
+        first_name: string;
+        last_name: string;
+        avatar: null | string;
+        avatar_url: string;
+    };
+    ratio: number;
+    text: string;
+    path: string;
+    created_at: string;
+    childrens: number;
+    user_reaction: "upvoted" | "downvoted" | null;
+    child: Comment[];
+};
 export class JSONToTree {
-    #json: { [key: string]: JSON_Element } = {};
+    #json: Comment[] = new Array<Comment>();
 
-    constructor(json: object[]) {
-        json.forEach((item) => {
-            this.convert_to_tree_given_path(item as unknown as { user: string; text: string; path: string });
+    constructor({ json, old_json }: { json: Comment[]; old_json?: Comment[] }) {
+        if (old_json) {
+            // DO NOT DEEP MERGE
+            const new_arr = this.convert_to_tree_given_path(json);
+            this.#json = old_json.concat(new_arr);
+        } else {
+            this.#json = this.convert_to_tree_given_path(json);
+        }
+    }
+
+    private convert_to_tree_given_path(data: Comment[]): Comment[] {
+        const tree: Comment[] = [];
+
+        // Create a dictionary to quickly access nodes by their path
+        const node_dictionary: { [path: string]: Comment } = {};
+
+        // First pass: Create nodes and populate the dictionary
+        data.forEach((node: Comment) => {
+            const new_node: Comment = { ...node, child: [] };
+            node_dictionary[node.path] = new_node;
+
+            // If the node is a root-level node, add it to the tree
+            if (!node.path.includes(".")) {
+                tree.push(new_node);
+            }
         });
+
+        // Second pass: Connect child nodes to their parent nodes
+        data.forEach((node: Comment) => {
+            const path_segments = node.path.split(".");
+            const parent_path = path_segments.slice(0, -1).join(".");
+            const parent_node = node_dictionary[parent_path];
+
+            if (parent_node) {
+                parent_node.child.push(node_dictionary[node.path]);
+            }
+        });
+        return tree;
     }
 
-    public convert_to_tree_given_path({ user, text, path }: { user: string; text: string; path: string }) {
-        set(this.#json, path, { path, user, text });
-    }
-
-    public to_tree() {
+    public build() {
         return this.#json;
     }
 }
