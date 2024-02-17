789286659b1d6f201aa7ef298486b8c94cc1c990
tokitouq <tokitousan2005@gmail.com>
2024-02-17 18:35:34 +0530

    Feat: change Landing page

--- src/lib/icons/logos/core_project.svelte
+++ src/lib/icons/logos/core_project.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
+    import { cn } from "$functions/classnames";
+
     const core_mapping: { [key in string]: string } = {
         c: "text-surface-100",
         o: "text-warning-500",
         r: "text-surface-100",
         e: "text-surface-100"
     };
+
+    let klass = "";
+    export {klass as class};
 </script>
 
-<div class="flex select-none gap-[0.25vw] text-[1.5vw] font-bold leading-none">
+<div class={cn(klass, "flex select-none gap-[0.25vw] text-[1.5vw] font-bold leading-none")}>
     <p>
         {#each "core".split("") as letter}
             <span class="inline-flex {core_mapping[letter]}">{letter}</span>
